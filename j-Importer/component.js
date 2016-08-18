4e098d94030da3f8b9219033619759b84ca0b6fc
Peter Širka <petersirka@gmail.com>
2016-08-18 07:19:44 +0200

    Added data-reload attribute.

--- j-Importer/component.js
+++ j-Importer/component.js
@@ -1,19 +1,26 @@
 COMPONENT('importer', function() {
 	var self = this;
 	var imported = false;
+	var reload = self.attr('data-reload');
+
 	self.readonly();
 	self.setter = function(value) {
-
-		if (imported) {
-			self.setter = null;
-			return;
-		}
 
 		if (!self.evaluate(self.attr('data-if')))
 			return;
 
+		if (imported) {
+			if (reload)
+				return EXEC(reload);
+			self.setter = null;
+			return;
+		}
+
 		imported = true;
-		IMPORT(self.attr('data-url'));
-		self.remove();
+		IMPORT(self.attr('data-url'), function() {
+			if (reload)
+				return EXEC(reload);
+			self.remove();
+		});
 	};
 });
--- j-Importer/readme.md
+++ j-Importer/readme.md
@@ -5,6 +5,7 @@ __j-Import__ is a great component for importing HTML templates, scripts or style
 __Attributes__:
 - `data-if` must contain a condition in the form `value === 'myvalue'` (the condition is evaluated with value from `data-component-path`)
 - `data-url` is a URL for importing
+- `data-reload` can contains only path method which is executed when `data-if` is valid
 
 ### Author
 
