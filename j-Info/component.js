6b1db876f77e2d92070795a3ec874cecf078e770
Peter Širka <petersirka@gmail.com>
2016-08-22 21:26:53 +0200

    Added new component.

---
+++ j-Info/component.js
@@ -0,0 +1,85 @@
+COMPONENT('info', function() {
+	var self = this;
+	var $window = $(window);
+	var is = false;
+	var timeout;
+	var container;
+	var arrow;
+	var orient = { left: '' };
+
+	self.singleton();
+	self.readonly();
+	self.blind();
+
+	self.make = function() {
+
+		self.element.addClass('ui-info');
+		self.element.append('<span class="ui-info-arrow fa fa-caret-up"></span><div class="ui-info-body"></div>');
+		container = self.element.find('.ui-info-body');
+		arrow = self.element.find('.ui-info-arrow');
+
+		$(document).on('touchstart mousedown', function(e) {
+			self.hide();
+		});
+	};
+
+	self.show = function(orientation, target, body) {
+
+		if (is) {
+			clearTimeout(timeout);
+			var obj = target instanceof jQuery ? target.get(0) : target;
+			if (self.target === obj)
+				return self.hide(0);
+		}
+
+		target = $(target);
+
+		if (!body)
+			return self.hide(0);
+
+		container.html(body);
+
+		var offset = target.offset();
+
+		switch (orientation) {
+			case 'left':
+				orient.left = '15px';
+				break;
+			case 'right':
+				orient.left = '210px';
+				break;
+			case 'center':
+				orient.left = '107px';
+				break;
+		}
+
+		arrow.css(orient);
+
+		var options = SINGLETON('ui-info');
+		options.left = orientation === 'center' ? Math.ceil((offset.left - self.element.width() / 2) + (target.innerWidth() / 2)) : orientation === 'left' ? offset.left - 8 : (offset.left - self.element.width()) + target.innerWidth();
+		options.top = offset.top + target.innerHeight() + 10;
+		self.element.css(options);
+
+		if (is)
+			return;
+
+		self.element.show();
+
+		setTimeout(function() {
+			self.element.addClass('ui-info-visible');
+		}, 100);
+
+		is = true;
+	};
+
+	self.hide = function(sleep) {
+		if (!is)
+			return;
+		clearTimeout(timeout);
+		timeout = setTimeout(function() {
+			self.element.hide().removeClass('ui-info-visible');
+			self.target = null;
+			is = false;
+		}, sleep ? sleep : 100);
+	};
+});
---
+++ j-Info/readme.md
@@ -0,0 +1,12 @@
+## j-Info
+
+A simple solution for showing additional information in datagrid or for links or etc..
+
+- singleton
+- show info box via `FIND('info').show('left|center|right', element, 'HTML body');`
+- hide info box via `FIND('info').hide()`
+
+### Author
+
+- Peter Širka <petersirka@gmail.com>
+- License: MIT
