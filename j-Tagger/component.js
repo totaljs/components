a1442f9bb9ab9afb2ede0d12cc11424dd01e6e10
Peter Širka <petersirka@gmail.com>
2016-08-24 20:56:12 +0200

    Improved code.

--- j-Tagger/component.js
+++ j-Tagger/component.js
@@ -1,103 +1,104 @@
 /**
  * Tagger
- * @version 1.0.0
+ * @version 1.9.0
  */
 COMPONENT('tagger', function() {
 
-    var self = this;
-    var elements;
+	var self = this;
+	var elements;
 
-    self.readonly();
+	self.readonly();
 
-    self.make = function() {
-        elements = self.find('[data-name]');
-        elements.each(function() {
-            this.$tagger = {};
-            this.$tagger.def = this.innerHTML;
-        });
-    };
+	self.make = function() {
+		elements = self.find('[data-name]');
+		elements.each(function() {
+			this.$tagger = {};
+			this.$tagger.def = this.innerHTML;
+		});
+	};
 
-    self.arrow = function(value) {
-        return FN(value.replace(/\&gt\;/g, '>').replace(/\&lt\;/g, '<').replace(/\&amp\;/g, '&'));
-    };
+	self.arrow = function(value) {
+		return FN(value.replace(/\&gt\;/g, '>').replace(/\&lt\;/g, '<').replace(/\&amp\;/g, '&'));
+	};
 
-    self.setter = function(value) {
+	self.setter = function(value) {
 
-        if (!value) {
-            self.element.addClass('hidden');
-            return;
-        }
+		if (!value) {
+			self.element.addClass('hidden');
+			return;
+		}
 
-        // self.element.toggleClass('transparent', true).removeClass('hidden');
-        elements.each(function() {
+		// self.element.toggleClass('transparent', true).removeClass('hidden');
+		elements.each(function() {
 
-            var name = this.getAttribute('data-name');
-            var format = this.getAttribute('data-format');
-            var type = this.getAttribute('data-type');
-            var visible = this.getAttribute('data-visible');
-            var before = this.getAttribute('data-before');
-            var after = this.getAttribute('data-after');
+			var name = this.getAttribute('data-name');
+			var format = this.getAttribute('data-format');
+			var type = this.getAttribute('data-type');
+			var visible = this.getAttribute('data-visible');
+			var before = this.getAttribute('data-before');
+			var after = this.getAttribute('data-after');
 			var val = name ? GET(name, value) : value;
-            var key;
-            var cache = this.$tagger;
+			var cache = this.$tagger;
+			var key;
 
-            if (format) {
-                key = 'format';
-                if (!cache[key])
-                    format = cache[key] = self.arrow(format);
-                else
-                    format = cache[key];
-            }
+			if (format) {
+				key = 'format';
+				if (cache[key])
+					format = cache[key];
+				else
+					format = cache[key] = self.arrow(format);
+			}
 
-            var typeval = typeof(val);
+			var typeval = typeof(val);
 
-            switch (type) {
-                case 'date':
-                    if (typeval === 'string')
-                        val = val.parseDate();
-                    else if (typeval === 'number')
-                        val = new Date(val);
-                    else
-                        val = '';
-                    break;
+			switch (type) {
+				case 'date':
+					if (typeval === 'string')
+						val = val.parseDate();
+					else if (typeval === 'number')
+						val = new Date(val);
+					else
+						val = '';
+					break;
 
-                case 'number':
-                case 'currency':
-                    if (typeval === 'string')
-                        val = val.parseFloat();
-                    if (typeof(val) !== 'number')
-                        val = '';
-                    break;
-            }
+				case 'number':
+				case 'currency':
+					if (typeval === 'string')
+						val = val.parseFloat();
+					if (typeof(val) !== 'number')
+						val = '';
+					break;
+			}
 
-            if ((val || val === 0) && format)
-                val = format(val);
+			if ((val || val === 0) && format)
+				val = format(val);
 
-            if (visible) {
-                key = 'visible';
-                if (!cache[key])
-                    visible = cache[key] = self.arrow(visible);
-                else
-                    visible = cache[key];
-                var is = visible(val);
-                $(this).toggleClass('hidden', !is);
-                return;
-            }
+			if (visible) {
+				key = 'visible';
+				if (!cache[key])
+					visible = cache[key] = self.arrow(visible);
+				else
+					visible = cache[key];
+				var is = visible(val);
+				$(this).toggleClass('hidden', !is);
+				return;
+			}
 
-            val = val === null || val === undefined ? '' : val.toString();
+			val = val == null ? '' : val.toString();
 
-            if (val && !format)
-                val = Ta.helpers.encode(val);
+			if (val && !format)
+				val = Ta.helpers.encode(val);
 
-            if (val) {
-                if (this.innerHTML !== val)
-                    this.innerHTML = (before ? before : '') + val + (after ? after : '');
-                return;
-            }
+			if (val) {
+				if (this.innerHTML !== val)
+					this.innerHTML = (before ? before : '') + val + (after ? after : '');
+				return;
+			}
 
-            if (this.innerHTML !== cache.def)
-                this.innerHTML = cache.def;
-        });
-        self.element.removeClass('transparent hidden');
-    };
+			if (this.innerHTML !== cache.def)
+				this.innerHTML = cache.def;
+		});
+
+		self.element.removeClass('transparent hidden');
+	};
 });
