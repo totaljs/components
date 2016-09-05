04039f4bdc737d153e36819e07aa97f5decd2763
Peter Širka <petersirka@gmail.com>
2016-09-05 21:48:46 +0200

    Improve code.

--- Smilefy/component.js
+++ Smilefy/component.js
@@ -1,8 +1,7 @@
 function smilefy(text) {
 	var db = { ':-)': 1, ':)': 1, ';)': 8, ':D': 0, '8)': 5, ':((': 7, ':(': 3, ':|': 2, ':P': 6, ':O': 4, ':*': 9, '+1': 10, '1': 11, '\/': 12 };
 	return text.replace(/(\-1|[:;8O\-)DP(|\*]|\+1){1,3}/g, function(match) {
-		var clean = match.replace('-', '');
-		var smile = db[clean];
+		var smile = db[match.replace('-', '')];
 		if (smile == null)
 			return match;
 		return '<span class="smiles smiles-' + smile + '"></span>';
