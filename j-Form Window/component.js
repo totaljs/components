66c7ec8f3281a48cf6fcadae861f324c42852559
Peter Širka <petersirka@gmail.com>
2016-09-12 23:15:02 +0200

    Improve code.

--- j-Form Window/component.js
+++ j-Form Window/component.js
@@ -1,51 +1,115 @@
 COMPONENT('form', function() {
 
-    var self = this;
+	var self = this;
+	var autocenter;
 
-    if (!MAN.$$form) {
-        MAN.$$form = true;
-        $(document).on('click', '.ui-form-button-close', function() {
-            SET($.components.findById($(this).attr('data-id')).path, '');
-        });
-    }
+	if (!MAN.$$form) {
+		window.$$form_level = window.$$form_level || 1;
+		MAN.$$form = true;
+		$(document).on('click', '.ui-form-button-close', function() {
+			SET($.components.findById($(this).attr('data-id')).path, '');
+			window.$$form_level--;
+		});
 
-    var hide = function() {
-        self.set('');
-    };
+		$(window).on('resize', function() {
+			FIND('form', true).forEach(function(component) {
+				!component.element.hasClass('hidden') && component.resize();
+			});
+		});
 
-    self.readonly();
-    self.submit = function(hide) { hide(); };
-    self.cancel = function(hide) { hide(); };
+		$(document).on('click', '.ui-form-container-padding', function(e) {
+			if (!$(e.target).hasClass('ui-form-container-padding'))
+				return;
+			var form = $(this).find('.ui-form');
+			var cls = 'ui-form-animate-click';
+			form.addClass(cls);
+			setTimeout(function() {
+				form.removeClass(cls);
+			}, 300);
+		});
+	}
 
-    self.make = function() {
-        var content = self.element.html();
-        var width = self.attr('data-width') || '800px';
-        var submit = self.attr('data-submit');
+	self.readonly();
+	self.submit = function(hide) { self.hide(); };
+	self.cancel = function(hide) { self.hide(); };
+	self.onHide = function(){};
 
-        self.condition = self.attr('data-if');
-        self.element.empty();
+	var hide = self.hide = function() {
+		self.set('');
+		self.onHide();
+	};
 
-        $(document.body).append('<div id="{0}" class="hidden ui-form-container"><div class="ui-form-container-padding"><div class="ui-form" style="max-width:' + width + '"><div class="ui-form-title"><span class="fa fa-times ui-form-button-close" data-id="{0}"></span>{1}</div>{2}</div></div>'.format(self._id, self.attr('data-title'), content));
+	self.resize = function() {
+		if (!autocenter)
+			return;
+		var ui = self.find('.ui-form');
+		var fh = ui.innerHeight();
+		var wh = $(window).height();
+		var r = (wh / 2) - (fh / 2);
+		if (r > 30)
+			ui.css({ marginTop: (r - 15) + 'px' });
+		else
+			ui.css({ marginTop: '20px' });
+	};
 
-        self.element = $('#' + self._id);
-        self.element.data(COM_ATTR, self);
+	self.make = function() {
+		var width = self.attr('data-width') || '800px';
+		var submit = self.attr('data-submit');
+		var enter = self.attr('data-enter');
+		autocenter = self.attr('data-autocenter') === 'true';
+		self.condition = self.attr('data-if');
 
-        self.element.find('button').on('click', function(e) {
-            switch (this.name) {
-                case 'submit':
-                case 'cancel':
-                    if (!this.disabled)
-                        self[this.name](hide);
-                    break;
-            }
-        });
+		$(document.body).append('<div id="{0}" class="hidden ui-form-container"><div class="ui-form-container-padding"><div class="ui-form" style="max-width:{1}"><div class="ui-form-title"><span class="fa fa-times ui-form-button-close" data-id="{2}"></span>{3}</div>{4}</div></div>'.format(self._id, width, self.id, self.attr('data-title')));
 
-        return true;
-    };
+		self.element.data(COM_ATTR, self);
+		var el = $('#' + self._id);
+		el.find('.ui-form').get(0).appendChild(self.element.get(0));
+		self.element = el;
 
-    self.setter = function(value) {
-        var isHidden = !EVALUATE(self.path, self.condition);
-        self.element.toggleClass('hidden', isHidden);
-        !isHidden && !isMOBILE && self.element.find('input').eq(0).focus();
-    };
+		self.element.on('scroll', function() {
+			EXEC('$calendar.hide');
+		});
+
+		self.element.find('button').on('click', function(e) {
+			window.$$form_level--;
+			switch (this.name) {
+				case 'submit':
+					self.submit(hide);
+					break;
+				case 'cancel':
+					!this.disabled && self[this.name](hide);
+					break;
+			}
+		});
+
+		enter === 'true' && self.element.on('keydown', 'input', function(e) {
+			e.keyCode === 13 && self.element.find('button[name="submit"]').get(0).disabled && self.submit(hide);
+		});
+
+		return true;
+	};
+
+	self.getter = null;
+	self.setter = function(value) {
+
+		var isHidden = !EVALUATE(self.path, self.condition);
+		self.element.toggleClass('hidden', isHidden);
+		EXEC('$calendar.hide');
+
+		if (isHidden) {
+			self.element.find('.ui-form').removeClass('ui-form-animate');
+			return;
+		}
+
+		self.resize();
+		var el = self.element.find('input,select,textarea');
+		el.length > 0 && el.eq(0).focus();
+		window.$$form_level++;
+		self.element.css('z-index', window.$$form_level * 10);
+		self.element.animate({ scrollTop: 0 }, 0, function() {
+			setTimeout(function() {
+				self.element.find('.ui-form').addClass('ui-form-animate');
+			}, 300);
+		});
+	};
 });
