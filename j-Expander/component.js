4f552cb5c15407ff42c6d15667141bb03f6e1d19
Peter Širka <petersirka@gmail.com>
2016-10-16 09:10:08 +0200

    Update j-Expander (now works click on the fade).

--- j-Expander/component.js
+++ j-Expander/component.js
@@ -5,7 +5,7 @@ COMPONENT('expander', function() {
 		self.element.addClass('ui-expander');
 		self.element.wrapInner('<div class="ui-expander-container"></div>');
 		self.append('<div class="ui-expander-fade"></div><div class="ui-expander-button"><span class="fa fa-angle-double-down"></span></div>');
-		self.element.on('click', '.ui-expander-button', function() {
+		self.element.on('click', '.ui-expander-button,.ui-expander-fade', function() {
 			self.element.toggleClass('ui-expander-expanded');
 			self.element.find('.ui-expander-button').find('.fa').toggleClass('fa-angle-double-down fa-angle-double-up');
 		});
