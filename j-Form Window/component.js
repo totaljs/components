4abcc494d310b56143fad9f1240bee9929534e2f
Peter Širka <petersirka@gmail.com>
2016-10-28 14:28:57 +0200

    Fix `noscroll` class in j-Form.

--- j-Form Window/component.js
+++ j-Form Window/component.js
@@ -93,7 +93,7 @@ COMPONENT('form', function() {
 	self.setter = function(value) {
 
 		setTimeout2('noscroll', function() {
-			$('html,body').toggleClass('noscroll', value ? true : false);
+			$('html').toggleClass('noscroll', $('.ui-form-container').not('.hidden').length ? true : false);
 		}, 50);
 
 		var isHidden = !EVALUATE(self.path, self.condition);
