4cc232e2bfc4a2a46056b5f58543d6cdc57296ad
Peter Širka <petersirka@gmail.com>
2016-11-07 19:46:54 +0100

    Fix localStorage in Cookie.

--- j-Cookie/component.js
+++ j-Cookie/component.js
@@ -1,10 +1,16 @@
 COMPONENT('cookie', function() {
 	var self = this;
-    self.singleton();
-    self.readonly();
+	self.singleton();
+	self.readonly();
 	self.make = function() {
 
-      var cookie = localStorage.getItem('cookie');
+		var cookie;
+
+		// private mode
+		try {
+			cookie = localStorage.getItem('cookie');
+		} catch (e) {}
+
 		if (cookie) {
 			self.element.addClass('hidden');
 			return;
@@ -13,6 +19,7 @@ COMPONENT('cookie', function() {
 		self.element.removeClass('hidden').addClass('ui-cookie');
 		self.element.append('<button>' + (self.attr('data-button') || 'OK') + '</button>');
 		self.element.on('click', 'button', function() {
+			// private mode
 			try {
 				localStorage.setItem('cookie', '1');
 			} catch (e) {}
