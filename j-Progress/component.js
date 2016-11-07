1912c77a8c534fe1567c79828d63bf8d728de7f7
Martin Smola <smola.martin@gmail.com>
2016-11-07 16:50:49 +0100

    Updated j-Progress
    Added data-animate attribute for disabling animation of progress

--- j-Progress/component.js
+++ j-Progress/component.js
@@ -1,7 +1,8 @@
 COMPONENT('progress', function() {
 	var self = this;
 	var container;
-    var old;
+	var old;
+	var animate = true;
 
 	self.readonly();
 
@@ -9,14 +10,19 @@ COMPONENT('progress', function() {
 		self.element.addClass('ui-progress');
 		self.append('<div style="width:10%">0%</div>');
 		container = self.find('div');
+		animate = self.attr('data-animate') !== 'false';
 	};
 
 	self.setter = function(value) {
 		if (!value)
 			value = 0;
-        if (old === value)
-            return;
-        old = value;
-		container.animate({ width: (old < 10 ? 10 : old) + '%' }, 100).html(old + '%');
+		if (old === value)
+			return;
+		old = value;
+		if (animate)
+			container.animate({ width: (old < 10 ? 10 : old) + '%' }, 20);
+		else
+			container.css({ width: (old < 10 ? 10 : old) + '%' });
+		container.html(old + '%');
 	};
 });
--- j-Progress/readme.md
+++ j-Progress/readme.md
@@ -3,7 +3,12 @@
 - easy usage
 - works with Bootstrap Grid System
 
+__Attributes__:
+
+- `data-animate="false"` optional(default true) useful when animation of progress is slower then updating of text
+
 ### Author
 
 - Peter Širka <petersirka@gmail.com>
 - License: MIT
+
