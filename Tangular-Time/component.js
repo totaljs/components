aa6124f52957323f2e3f020e3a8076ee776b5fd0
Peter Širka <petersirka@gmail.com>
2016-11-18 09:10:17 +0100

    Add a new component (helper): Tangular-Time

---
+++ Tangular-Time/component.js
@@ -0,0 +1,25 @@
+Tangular.register('time', function(value) {
+	var diff = Date.now() - (value instanceof Date ? value : value.parseDate()).getTime();
+
+	var minutes = ((diff / 1000) / 60) >> 0;
+	if (minutes < 60) {
+		if (minutes < 3)
+			return 'now';
+		return minutes + ' minutes ago';
+	}
+
+	var hours = (minutes / 60) >> 0;
+	if (hours < 24)
+		return hours + ' ' + Tangular.helpers.pluralize(hours, 'hours', 'hour', 'hours', 'hours') + ' ago';
+
+	var days = (hours / 24) >> 0;
+	if (days < 30)
+		return days + ' ' + Tangular.helpers.pluralize(days, 'days', 'day', 'days', 'days') + ' ago';
+
+	var months = (days / 29) >> 0;
+	if (months < 12)
+		return months + ' ' + Tangular.helpers.pluralize(months, 'months', 'month', 'months', 'months') + ' ago';
+
+	var years = (month / 12) >> 0;
+	return years + ' ' + Tangular.helpers.pluralize(years, 'years', 'year', 'years', 'years') + ' ago';
+});
---
+++ Tangular-Time/readme.md
@@ -0,0 +1,24 @@
+# Tangular-Time
+
+A simple Tangular template engine __helper__ for rendering `humanable` date and time.
+
+__Outputs__:
+
+- `now`
+- `5 minutes ago`
+- `1 month ago`,
+- `2 years ago`
+
+__Usage__:
+
+```html
+<h1>{{ name }}</h1>
+<div>{{ created | time }}</div>
+```
+
+
+or
+
+```javascript
+console.log(Tangular.helpers.time(new Date()));
+```
