b217c55eb52079506d7076207930feb42ae3506b
Peter Širka <petersirka@gmail.com>
2016-12-11 11:48:06 +0100

    Add a new component j-reCAPTCHA.

---
+++ j-reCAPTCHA/component.js
@@ -0,0 +1,26 @@
+COMPONENT('recaptcha', function() {
+
+	var self = this;
+	var isRequired = self.attr('data-required') === 'true';
+
+	self.getter = null;
+
+	self.validate = function(value) {
+		return !isRequired || (value && value.length > 300);
+	};
+
+	!isRequired && self.noValid();
+
+	self.make = function() {
+		WAIT(function() {
+			return window.grecaptcha ? true : false;
+		}, function(err, again) {
+			if (err)
+				return again(100);
+			grecaptcha.render(self.element.get(0), { sitekey: self.attr('data-key'), theme: self.attr('data-theme') || 'light', callback: function(response) {
+				self.set(response);
+				self.change(true);
+			}});
+		});;
+	};
+});
---
+++ j-reCAPTCHA/readme.md
@@ -0,0 +1,17 @@
+# j-reCAPTCHA
+
+- Works only with [jComponent](http://jcomponent.org)
+
+__Attributes__:
+
+- `data-key="YOUR_KEY"` (__required__)
+- `data-required="true"` (optional) the field must have some value, default __false__
+- `data-theme="light"` (optional) possible values `light` (default) or `dark`
+
+__Methods__:
+- `component.required(value)` - can enable/disable `required` with validation
+
+### Author
+
+- Peter Širka <petersirka@gmail.com>
+- License: MIT
