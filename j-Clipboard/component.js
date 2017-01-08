2e0b4c4a8a55c4fb94e8615fab786e2f0ade9426
Peter Širka <petersirka@gmail.com>
2017-01-08 11:15:54 +0100

    Add a new component j-Clipboard.

---
+++ j-Clipboard/component.js
@@ -0,0 +1,25 @@
+COMPONENT('clipboard', function() {
+	var self = this;
+	var container;
+
+	self.singleton();
+	self.readonly();
+
+	self.copy = function(value) {
+		container.val(value);
+		container.focus();
+  		document.execCommand('selectAll', false, null);
+  		document.execCommand('copy');
+  		container.blur();
+	};
+
+	self.make = function() {
+		var id = 'clipboard' + self.id;
+		$(document.body).append('<textarea id="{0}" class="ui-clipboard"></textarea>'.format(id));
+		container = $('#' + id);
+	};
+
+	self.setter = function(value) {
+		value && self.copy(value);
+	};
+});
---
+++ j-Clipboard/readme.md
@@ -0,0 +1,17 @@
+## j-Clipboard
+
+- Works only with [jComponent](http://jcomponent.org)
+- __Download__ [jComponent with Tangular (jcta.min.js)](https://github.com/petersirka/jComponent)
+- Works with Bootstrap
+- It works as singleton
+
+__Attributes__:
+- `data-jc-path="some.path"` (optional) copies value into the clipboard automatically
+
+__Methods__:
+- `SETTER('clipboard', 'copy', 'TEXT TO CLIPBOARD')` or `FIND('clipboard').copy('TEXT TO CLIPBOARD')`
+
+### Author
+
+- Peter Širka <petersirka@gmail.com>
+- License: MIT
