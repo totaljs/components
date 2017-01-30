4096e41ead1374e3ad1c0b3e8b3cf9c86ed18058
Peter Širka <petersirka@gmail.com>
2017-01-30 15:14:54 +0100

    New component j-Modal.

---
+++ j-Modal/component.js
@@ -0,0 +1,63 @@
+COMPONENT('modal', function() {
+	var self = this;
+	var reload = self.attr('data-reload');
+
+	if (!MAN.$$modal) {
+		window.$$modal_level = window.$$modal_level || 1;
+		MAN.$$modal = true;
+		$(document).on('click', '.ui-modal-close', function() {
+			SET($(this).attr('data-path'), '');
+			window.$$modal_level--;
+		});
+	}
+
+	self.readonly();
+	self.make = function() {
+		self.condition = self.attr('data-if');
+		$(document.body).append('<div id="{0}" class="hidden ui-modal-container"><a href="javascript:void(0)" class="ui-modal-close" data-path="{2}"><i class="fa fa-times"></i>{1}</a><div class="ui-modal-body"></div></div>'.format(self._id, self.attr('data-button') || 'Close window', self.path));
+		var el = $('#' + self._id);
+		el.find('.ui-modal-body').get(0).appendChild(self.element.get(0));
+		self.element.removeClass('hidden');
+		self.element = el;
+		self.element.on('scroll', function() {
+			WORKFLOW('reflow')(self.name);
+		});
+
+		self.attr('data-esc') !== 'false' && $(window).on('keydown', function(e) {
+			e.keyCode === 27 && self.get() && self.set('');
+		});
+	};
+
+	self.getter = null;
+	self.setter = function(value) {
+
+		setTimeout2(self.id + '.scroll', function() {
+			$('html').toggleClass('ui-modal-noscroll', $('.ui-modal-container').not('.hidden').length ? true : false);
+		}, 50);
+
+		var isHidden = !EVALUATE(self.path, self.condition);
+		if (self.element.hasClass('hidden') && isHidden)
+			return;
+
+		WORKFLOW('reflow')(self.name);
+
+		if (isHidden) {
+			self.element.removeClass('ui-modal-visible');
+			setTimeout2(self.id, function() {
+				self.element.addClass('ui-remove');
+			}, 50);
+			return;
+		}
+
+		window.$$modal_level++;
+		self.element.removeClass('hidden');
+
+		setTimeout2(self.id, function() {
+			self.element.addClass('ui-modal-visible');
+		}, 50);
+
+		reload && EXEC(reload, self);
+		self.element.css('z-index', window.$$modal_level * 5);
+		self.find('.ui-modal-body').animate({ scrollTop: 0 }, 0, NOOP, 300);
+	};
+});
---
+++ j-Modal/readme.md
@@ -0,0 +1,17 @@
+## j-Modal Window
+
+- Works only with [jComponent](http://jcomponent.org)
+- __Download__ [jComponent with Tangular (jcta.min.js)](https://github.com/petersirka/jComponent)
+- Works with Bootstrap
+
+__Attributes__:
+
+- `data-button="Close window"` a label for close button
+- `data-esc="false"` can disable ESC shortcut
+- `data-if="value === 'user'"` a condition for showing modal window
+- `data-reload="METHOD_NAME"` is executed when the modal window is visible
+
+### Author
+
+- Peter Širka <petersirka@gmail.com>
+- License: MIT
