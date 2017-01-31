7aba42b76a4a2237b11e3534ae262503c17f0e3f
Peter Širka <petersirka@gmail.com>
2017-01-31 18:50:57 +0100

    Improve component.

--- j-Form Window/component.js
+++ j-Form Window/component.js
@@ -7,7 +7,7 @@ COMPONENT('form', function() {
 		window.$$form_level = window.$$form_level || 1;
 		MAN.$$form = true;
 		$(document).on('click', '.ui-form-button-close', function() {
-			SET(FIND('#' + $(this).attr('data-id')).path, '');
+			SET($(this).attr('data-path'), '');
 			window.$$form_level--;
 		});
 
@@ -60,11 +60,11 @@ COMPONENT('form', function() {
 		autocenter = self.attr('data-autocenter') === 'true';
 		self.condition = self.attr('data-if');
 
-		$(document.body).append('<div id="{0}" class="hidden ui-form-container"><div class="ui-form-container-padding"><div class="ui-form" style="max-width:{1}"><div class="ui-form-title"><span class="fa fa-times ui-form-button-close" data-id="{2}"></span>{3}</div>{4}</div></div>'.format(self._id, width, self.id, self.attr('data-title')));
+		$(document.body).append('<div id="{0}" class="hidden ui-form-container"><div class="ui-form-container-padding"><div class="ui-form" style="max-width:{1}"><div class="ui-form-title"><span class="fa fa-times ui-form-button-close" data-path="{2}"></span>{3}</div>{4}</div></div>'.format(self._id, width, self.path, self.attr('data-title')));
 
 		var el = $('#' + self._id);
 		el.find('.ui-form').get(0).appendChild(self.element.get(0));
-		self.element.removeClass('hidden');
+		self.classes('-hidden');
 		self.element = el;
 
 		self.element.on('scroll', function() {
@@ -86,11 +86,8 @@ COMPONENT('form', function() {
 		enter === 'true' && self.element.on('keydown', 'input', function(e) {
 			e.keyCode === 13 && !self.element.find('button[name="submit"]').get(0).disabled && self.submit(hide);
 		});
-
-		return true;
 	};
 
-	self.getter = null;
 	self.setter = function(value) {
 
 		setTimeout2('noscroll', function() {
@@ -102,19 +99,28 @@ COMPONENT('form', function() {
 		WORKFLOW('reflow')(self.name);
 
 		if (isHidden) {
+			self.release(true);
 			self.element.find('.ui-form').removeClass('ui-form-animate');
 			return;
 		}
 
 		self.resize();
+		self.release(false);
+
 		var el = self.element.find('input,select,textarea');
-		el.length > 0 && el.eq(0).focus();
+		el.length && el.eq(0).focus();
+
 		window.$$form_level++;
-		self.element.css('z-index', window.$$form_level * 5);
-		self.element.animate({ scrollTop: 0 }, 0, function() {
-			setTimeout(function() {
-				self.element.find('.ui-form').addClass('ui-form-animate');
-			}, 300);
-		});
+		self.element.css('z-index', window.$$form_level * 10);
+		self.element.scrollTop(0);
+
+		setTimeout(function() {
+			self.element.find('.ui-form').addClass('ui-form-animate');
+		}, 300);
+
+		// Fixes a problem with freezing of scrolling in Chrome
+		setTimeout2(self.id, function() {
+			self.element.css('z-index', (window.$$form_level * 10) + 1);
+		}, 1000);
 	};
 });
