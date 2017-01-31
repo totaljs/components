7701d84abc008a42155f80ce1c68b18718012fbb
Peter Širka <petersirka@gmail.com>
2017-01-31 20:05:22 +0100

    Rename argument name.

--- Smilefy/component.js
+++ Smilefy/component.js
@@ -1,6 +1,6 @@
-function smilefy(text) {
+function smilefy(str) {
 	var db = { ':-)': 1, ':)': 1, ';)': 8, ':D': 0, '8)': 5, ':((': 7, ':(': 3, ':|': 2, ':P': 6, ':O': 4, ':*': 9, '+1': 10, '1': 11, '\/': 12 };
-	return text.replace(/(\-1|[:;8O\-)DP(|\*]|\+1){1,3}/g, function(match) {
+	return str.replace(/(\-1|[:;8O\-)DP(|\*]|\+1){1,3}/g, function(match) {
 		var smile = db[match.replace('-', '')];
 		return smile === undefined ? match : '<i class="smiles smiles-' + smile + '"></i>';
 	});
