ed1c26106d4b5e6231199b667f7480c1080cb914
Peter Širka <petersirka@gmail.com>
2017-02-07 09:23:58 +0100

    Update component.

--- j-Expander/component.js
+++ j-Expander/component.js
@@ -1,13 +1,24 @@
 COMPONENT('expander', function() {
 	var self = this;
 	self.readonly();
+
+	self.toggle = function(v) {
+
+		if (v === undefined)
+			v = !self.element.hasClass('ui-expander-expanded');
+
+		self.element.toggleClass('ui-expander-expanded', v);
+		var fa = self.find('.ui-expander-button').find('.fa');
+		fa.toggleClass('fa-angle-double-down', !v);
+		fa.toggleClass('fa-angle-double-up', v);
+	};
+
 	self.make = function() {
-		self.classes('ui-expander');
+		self.classes('ui-expander' + (self.attr('data-expand') === 'true' ? ' ui-expander-expanded' : ''));
 		self.element.wrapInner('<div class="ui-expander-container"></div>');
 		self.append('<div class="ui-expander-fade"></div><div class="ui-expander-button"><span class="fa fa-angle-double-down"></span></div>');
-		self.event('click', '.ui-expander-button,.ui-expander-fade', function() {
-			self.toggle('ui-expander-expanded');
-			self.find('.ui-expander-button').find('.fa').toggleClass('fa-angle-double-down fa-angle-double-up');
+		self.event('click', '.ui-expander-button', function() {
+			self.toggle();
 		});
 	};
 });
