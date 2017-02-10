51792320793e14d73203d03103d3daf7164ee0d8
Peter Širka <petersirka@gmail.com>
2017-02-10 14:47:42 +0100

    Improve j-Search.

--- j-Search/component.js
+++ j-Search/component.js
@@ -28,13 +28,13 @@ COMPONENT('search', function() {
 				return;
 			}
 
-			var search = value.toLowerCase().replace(/y/gi, 'i');
+			var search = value.toSearch();
 			var hide = [];
 			var show = [];
 
 			elements.toArray().waitFor(function(item, next) {
 				var el = $(item);
-				var val = (el.attr(options_attribute) || '').toLowerCase().replace(/y/gi, 'i');
+				var val = (el.attr(options_attribute) || '').toSearch();
 				if (val.indexOf(search) === -1)
 					hide.push(el);
 				else
