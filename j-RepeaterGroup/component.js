80e2e26c1f4f50914cdfb02a4f1c8ae0c889a822
Peter Širka <petersirka@gmail.com>
2017-02-27 14:03:11 +0100

    Add a new component.

---
+++ j-RepeaterGroup/component.js
@@ -0,0 +1,85 @@
+COMPONENT('repeater-group', function() {
+
+	var self = this;
+	var html;
+	var template_group;
+	var group;
+
+	self.readonly();
+
+	self.released = function(is) {
+		if (is) {
+			html = self.html();
+			self.empty();
+		} else
+			html && self.html(html);
+	};
+
+	self.make = function() {
+		group = self.attr('data-group');
+		self.element.find('script').each(function(index) {
+			var element = $(this);
+			var html = element.html();
+			element.remove();
+			if (index)
+				template_group = Tangular.compile(html);
+			else
+				self.template = Tangular.compile(html);
+		});
+	};
+
+	self.setter = function(value) {
+
+		if (!value || !value.length) {
+			self.empty();
+			return;
+		}
+
+		if (NOTMODIFIED(self.id, value))
+			return;
+
+		html = '';
+		var length = value.length;
+		var groups = {};
+
+		for (var i = 0; i < length; i++) {
+			var name = value[i][group];
+			if (!name)
+				name = '0';
+
+			if (groups[name])
+				groups[name].push(value[i]);
+			else
+				groups[name] = [value[i]];
+		}
+
+		var index = 0;
+		var indexgroup = 0;
+		var builder = '';
+		var keys = Object.keys(groups);
+
+		keys.quicksort();
+		keys.forEach(function(key) {
+			var arr = groups[key];
+			var tmp = '';
+
+			for (var i = 0, length = arr.length; i < length; i++) {
+				var item = arr[i];
+				item.index = index++;
+				tmp += self.template(item).replace(/\$index/g, index.toString()).replace(/\$/g, self.path + '[' + index + ']');
+			}
+
+			if (key !== '0') {
+				var options = {};
+				options[group] = key;
+				options.length = arr.length;
+				options.index = indexgroup++;
+				options.body = tmp;
+				builder += template_group(options);
+			}
+
+		});
+
+		self.empty().append(builder);
+	};
+});
---
+++ j-RepeaterGroup/readme.md
@@ -0,0 +1,14 @@
+## j-RepeaterGroup
+
+- easy usage
+- automatically groups data
+- learn by example
+
+__Attributes__:
+- `data-group="PROPERTY"` has to contain a property which will be be groupped
+
+
+### Author
+
+- Peter Širka <petersirka@gmail.com>
+- License: MIT
