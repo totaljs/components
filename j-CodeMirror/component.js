5bc2d030003a1f8077c32af7bda38cb8603b01d1
Peter Širka <petersirka@gmail.com>
2017-08-02 09:27:14 +0200

    Update component.

---
+++ j-CodeMirror/component.js
@@ -0,0 +1,105 @@
+COMPONENT('codemirror', 'linenumbers:false', function(self, config) {
+
+	var skipA = false;
+	var skipB = false;
+	var editor = null;
+
+	self.getter = null;
+
+	self.reload = function() {
+		editor.refresh();
+	};
+
+	self.validate = function(value) {
+		return config.disabled || !config.required ? true : value && value.length > 0;
+	};
+
+	self.configure = function(key, value, init) {
+		if (init)
+			return;
+
+		switch (key) {
+			case 'disabled':
+				self.tclass('ui-disabled', value);
+				editor.readOnly = value;
+				break;
+			case 'required':
+				self.find('.ui-codemirror-label').tclass('ui-codemirror-label-required', value);
+				break;
+		}
+
+	};
+
+	self.make = function() {
+		var content = config.label || self.html();
+		self.html('<div class="ui-codemirror-label' + (config.required ? ' ui-codemirror-label-required' : '') + '">' + (config.icon ? '<i class="fa fa-' + config.icon + '"></i> ' : '') + content + ':</div><div class="ui-codemirror"></div>');
+		var container = self.find('.ui-codemirror');
+		editor = CodeMirror(container.get(0), { lineNumbers: config.linenumbers, mode: config.type || 'htmlmixed', indentUnit: 4 });
+		config.height !== 'auto' && editor.setSize('100%', (config.height || 200) + 'px');
+
+		if (config.disabled) {
+			self.aclass('ui-disabled');
+			editor.readOnly = true;
+		}
+
+		editor.on('change', function(a, b) {
+
+			if (config.disabled)
+				return;
+
+			if (skipB && b.origin !== 'paste') {
+				skipB = false;
+				return;
+			}
+
+			setTimeout2(self.id, function() {
+				skipA = true;
+				self.reset(true);
+				self.dirty(false);
+				self.set(editor.getValue());
+			}, 200);
+		});
+
+		skipB = true;
+	};
+
+	self.setter = function(value) {
+
+		if (skipA === true) {
+			skipA = false;
+			return;
+		}
+
+		skipB = true;
+		editor.setValue(value || '');
+		editor.refresh();
+		skipB = true;
+
+		CodeMirror.commands['selectAll'](editor);
+		skipB = true;
+		editor.setValue(editor.getValue());
+		skipB = true;
+
+		setTimeout(function() {
+			editor.refresh();
+		}, 200);
+
+		setTimeout(function() {
+			editor.refresh();
+		}, 1000);
+
+		setTimeout(function() {
+			editor.refresh();
+		}, 2000);
+	};
+
+	self.state = function(type) {
+		if (!type)
+			return;
+		var invalid = self.isInvalid();
+		if (invalid === self.$oldstate)
+			return;
+		self.$oldstate = invalid;
+		self.find('.ui-codemirror').tclass('ui-codemirror-invalid', invalid);
+	};
+});
---
+++ j-CodeMirror/readme.md
@@ -0,0 +1,27 @@
+## j-CodeMirror
+
+- Works only with [jComponent](http://jcomponent.org)
+- __Download__ [jComponent with Tangular (jcta.min.js)](https://github.com/petersirka/jComponent)
+- Works with Bootstrap
+
+__Configuration__:
+
+Example: `data-jc-config="required:true;icon:envelope"`
+
+- `type` {String} (optional) type for syntax highlighting (default: `htmlmixed`)
+- `required` {Boolean} (optional) enables "required" (default: `false`)
+- `linernumbers` {Boolean} (optional) shows line numbers (default: `false`)
+- `icon` {String} (optional) icon for label without `fa-` e.g. `home`, `cog`, etc.
+- `height` {Number} (optional) control height (default: `200` px)
+- `label` {String} (optional) label (default is HTML content)
+- `disabled` {Boolean} (optional) disables this component
+
+### Author
+
+- Peter Širka <petersirka@gmail.com>
+- License: MIT
+
+### Author
+
+- Peter Širka <petersirka@gmail.com>
+- License: MIT
