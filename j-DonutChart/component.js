8b1e9dc94353fd079d686345d11901b209c50d0a
Peter Širka <petersirka@gmail.com>
2017-08-10 21:16:31 +0200

    Add a new component.

---
+++ j-DonutChart/component.js
@@ -0,0 +1,131 @@
+COMPONENT('donutchart', 'format:{{ value | format(0) }};tooltip:true', function(self, config) {
+
+	var svg, g, selected, tooltip;
+	var strokew = 0;
+	var animate = true;
+	var W = $(window);
+
+	self.readonly();
+	self.make = function() {
+		self.aclass('ui-donutchart');
+		self.append('<div class="ui-donutchart-tooltip"></div><svg></svg>');
+		svg = self.find('svg');
+		g = svg.asvg('g').attr('class', 'pieces');
+		tooltip = self.find('.ui-donutchart-tooltip');
+
+		W.on('resize', self.resize);
+
+		self.element.on('mouseenter touchstart', '.piece', function() {
+			config.tooltip && self.select(+this.getAttribute('data-index'));
+		});
+	};
+
+	self.select = function(index) {
+		var item = self.get()[index];
+		if (item === selected)
+			return;
+		self.find('.selected').rclass('selected').css('stroke-width', strokew);
+		selected = item;
+		var w = self.element.width();
+		var el = self.find('.piece' + (index + 1));
+		tooltip.css('font-size', w / 15);
+		tooltip.html('<b>' + item.name + '</b><br />' + Tangular.render(config.format, item));
+		el.css('stroke-width', strokew.inc('-15%')).aclass('selected');
+	};
+
+	self.destroy = function() {
+		W.off('resize', self.resize);
+	};
+
+	self.resize = function() {
+		setTimeout2('resize.' + self.id, function() {
+			animate = false;
+			self.refresh();
+		}, 100);
+	};
+
+	function arcradius(centerX, centerY, radius, degrees) {
+		var radians = (degrees - 90) * Math.PI / 180.0;
+		return { x: centerX + (radius * Math.cos(radians)), y: centerY + (radius * Math.sin(radians)) };
+	}
+
+	function arc(x, y, radius, startAngle, endAngle){
+		var start = arcradius(x, y, radius, endAngle);
+		var end = arcradius(x, y, radius, startAngle);
+		var largeArcFlag = endAngle - startAngle <= 180 ? 0 : 1;
+		var d = ['M', start.x, start.y, 'A', radius, radius, 0, largeArcFlag, 0, end.x, end.y].join(' ');
+		return d;
+	}
+
+	self.setter = function(value) {
+
+		if (!value) {
+			g.empty();
+			gcircle.empty();
+			return;
+		}
+
+		var sum = null;
+		var len = value.length;
+
+		var groups = [];
+		var items = [];
+
+		for (var i = 0, length = value.length; i < length; i++) {
+			var item = value[i];
+			sum = (sum ? sum + item.value : item.value);
+			groups.push(item.name);
+		}
+
+		var count = 0;
+		var beg = 0;
+		var end = 0;
+
+		for (var i = 0, length = value.length; i < length; i++) {
+			var item = value[i];
+			var p = ((item.value / sum) * 100).floor(2);
+
+			count += p;
+
+			if (i === length - 1 && count < 100)
+				p = p + (100 - count);
+
+			end = beg + ((360 / 100) * p);
+			items.push({ name: item.name, percentage: p, beg: beg, end: end });
+			beg = end;
+		}
+
+		var size = self.element.width();
+		var half = size / 2;
+		var midpoint = size / 2.4;
+
+		strokew = size / 6 >> 0;
+
+		svg.attr('width', size);
+		svg.attr('height', size);
+		g.empty();
+
+		var pieces = [];
+
+		for (var i = 0, length = items.length; i < length; i++) {
+			var item = items[i];
+			pieces.push(g.asvg('path').attr('data-index', i).attr('data-beg', item.beg).attr('data-end', item.end).attr('stroke-width', strokew).attr('class', 'piece piece' + (i + 1)).attr('d', arc(half, half, midpoint, item.beg, animate ? item.beg : item.end)));
+		}
+
+		animate && pieces.waitFor(function(item, next) {
+			var beg = +item.attrd('beg');
+			var end = +item.attrd('end');
+			var diff = end - beg;
+
+			item.animate({ end: diff }, { duration: 180, step: function(fx) {
+				item.attr('d', arc(half, half, midpoint, beg, beg + fx));
+			}, complete: function() {
+				next();
+			}});
+		});
+
+		selected = null;
+		config.tooltip && self.select(0);
+		animate = true;
+	};
+});
---
+++ j-DonutChart/readme.md
@@ -0,0 +1,11 @@
+## j-DonutChart
+
+__Configuration__:
+
+- `tooltip` {Boolean} enables tooltip (default: `true`)
+- `format` {String} a template (Tangular) for rendering tooltip (default: `{{ value | format(0) }}`)
+
+### Author
+
+- Peter Širka <petersirka@gmail.com>
+- License: MIT
