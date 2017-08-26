139c250760e74d943ab74e199074c08f666793b0
Peter Širka <petersirka@gmail.com>
2017-08-26 22:38:15 +0200

    New grid.

--- j-Grid/component.js
+++ j-Grid/component.js
@@ -1,16 +1,84 @@
-COMPONENT('grid', 'pagination:true;head:true;pluralizepages:# pages,# page,# pages,# pages;pluralizeitems:# items,# item,# items,# items', function(self, config) {
+COMPONENT('grid', 'filter:true;external:false;filterlabel:Filtering values ...;boolean:true|on|yes;pluralizepages:# pages,# page,# pages,# pages;pluralizeitems:# items,# item,# items,# items;pagination:false', function(self, config) {
 
-	var tbody, thead, container, head, pagination = null;
-	var options = { columns: [], items: [], head: '' };
-	var ppages, pitems, cache = null;
+	var tbody, thead, tbodyhead, container, pagination;
+	var options = { columns: {}, items: [], indexer: 0, filter: {} };
+	var isFilter = false;
+	var ppages, pitems, cache;
 
+	self.template = Tangular.compile('<td data-index="{{ index }}"{{ if $.class }} class="{{ $.class }}"{{ fi }}><div class="wrap{{ if align }} {{ align }}{{ fi }}"{{ if background }} style="background-color:{{ background }}"{{ fi }}>{{ value | raw }}</div></td>');
+	self.options = options;
 	self.readonly();
 
-	self.configure = function(key, value, init) {
+	self.make = function() {
+
+		var meta = self.find('script').html();
+		self.aclass('ui-grid-container');
+		self.html('<div class="ui-grid"><table class="ui-grid-header"><thead></thead></table><div class="ui-grid-scroller"><table class="ui-grid-data"><thead></thead><tbody></tbody></table></div></div>' + (config.pagination ? '<div class="ui-grid-footer"><div class="ui-grid-meta"></div><div class="ui-grid-pagination"><button class="ui-grid-button" name="first"><i class="fa fa-angle-double-left"></i></button><button class="ui-grid-button" name="prev"><i class="fa fa-angle-left"></i></button><div class="page"><input type="text" maxlength="5" class="ui-grid-input" /></div><button class="ui-grid-button" name="next"><i class="fa fa-angle-right"></i></button><button class="ui-grid-button" name="last"><i class="fa fa-angle-double-right"></i></button></div><div class="ui-grid-pages"></div></div></div>' : ''));
+
+		var body = self.find('.ui-grid-data');
+		tbody = $(body.find('tbody').get(0));
+		tbodyhead = $(body.find('thead').get(0));
+		thead = $(self.find('.ui-grid-header').find('thead').get(0));
+		container = $(self.find('.ui-grid-scroller').get(0));
+		pagination = config.pagination ? VIRTUALIZE(self.find('.ui-grid-footer'), { page: 'input', first: 'button[name="first"]', last: 'button[name="last"]', prev: 'button[name="prev"]', next: 'button[name="next"]', meta: '.meta', pages: '.pages' }) : null;
+		self.meta(meta);
+
+		self.event('click', '.ui-grid-columnsort', function() {
+			var obj = {};
+			obj.columns = options.columns;
+			obj.column = options.columns[+$(this).attr('data-index')];
+			self.sort(obj);
+		});
+
+		self.event('change', '.ui-grid-filter', function() {
+			var el = $(this).parent();
+			if (this.value)
+				options.filter[this.name] = this.value;
+			else
+				delete options.filter[this.name];
+			el.tclass('ui-grid-selected', this.value ? true : false);
+			self.filter();
+		});
+
+		self.event('click', '.ui-grid-button', function() {
+			switch (this.name) {
+				case 'first':
+					cache.page = 1;
+					self.operation('pagination');
+					break;
+				case 'last':
+					cache.page = cache.pages;
+					self.operation('pagination');
+					break;
+				case 'prev':
+					cache.page -= 1;
+					self.operation('pagination');
+					break;
+				case 'next':
+					cache.page += 1;
+					self.operation('pagination');
+					break;
+			}
+		});
+
+		self.event('change', '.ui-grid-input', function() {
+			var page = (+this.value) >> 0;
+			if (isNaN(page) || page < 0 || page > cache.pages || page === cache.page)
+				return;
+			cache.page = page;
+			self.operation('pagination');
+		});
+
+		self.on('resize', self.resize);
+	};
+
+	self.meta = function(html) {
+		options.columns = new Function('return ' + html.trim())();
+		self.rebuild(true);
+	};
+
+	self.configure = function(key, value) {
 		switch (key) {
-			case 'head':
-				!init && head.tclass('hidden', value === false);
-				break;
 			case 'pluralizepages':
 				ppages = value.split(',').trim();
 				break;
@@ -20,100 +88,61 @@ COMPONENT('grid', 'pagination:true;head:true;pluralizepages:# pages,# page,# pag
 		}
 	};
 
-	self.make = function() {
+	self.rebuild = function(init) {
 
-		self.aclass('ui-grid-container');
+		var data = ['<tr class="ui-grid-empty">'];
+		var header = ['<tr>'];
+		var filter = ['<tr>'];
 
-		var reg = /<td.*?>/g;
-		var meta = self.find('script').html();
-		var cols = meta.match(reg);
-		var index;
+		var size = 0;
+		var columns = options.columns;
 
-		for (var i = 0, length = cols.length; i < length; i++) {
-			var item = cols[i];
-			var col = {};
-			index = item.indexOf('name="');
-			col.name = (index === -1 ? '' : item.substring(index + 6, item.indexOf('"', index + 6))).trim();
-			index = item.indexOf('text="');
-			col.text = (index === -1 ? '' : item.substring(index + 6, item.indexOf('"', index + 6))).trim();
-			index = item.indexOf('width="');
-			col.width = (index === -1 ? '' : item.substring(index + 7, item.indexOf('"', index + 7))).trim();
-			index = item.indexOf('align="');
-			col.align = (index === -1 ? '' : item.substring(index + 7, item.indexOf('"', index + 7))).trim();
-			index = item.indexOf('title="');
-			col.title = (index === -1 ? '' : item.substring(index + 7, item.indexOf('"', index + 7))).trim();
-			index = item.indexOf('icon="');
-			col.icon = (index === -1 ? '' : item.substring(index + 6, item.indexOf('"', index + 6))).trim();
-			index = item.indexOf('bg="');
-			col.bg = (index === -1 ? '' : item.substring(index + 4, item.indexOf('"', index + 4))).trim();
-			col.xs = item.indexOf('hidden-xs') === -1;
-			col.sm = item.indexOf('hidden-sm') === -1;
-			col.md = item.indexOf('hidden-md') === -1;
-			col.lg = item.indexOf('hidden-lg') === -1;
-			col.class = ((col.xs ? '' : ' hidden-xs') + (col.sm ? '' : ' hidden-sm') + (col.md ? '' : ' hidden-md') + (col.lg ? '' : ' hidden-lg')).trim();
-			options.columns.push(col);
+		for (var i = 0, length = columns.length; i < length; i++) {
+			var col = columns[i];
+			size += col.size || 1;
+			col.sorting = null;
+			if (typeof(col.render) === 'string')
+				col.render = FN(col.render);
 		}
 
-		meta = meta.replace(reg, function(text) {
-			return text.replace(/(name|width|title|align|bg)=\".*?\"/g, '').replace(/\s{2,}/g, ' ').replace(/\s\>/g, '>');
-		});
+		for (var i = 0, length = columns.length; i < length; i++) {
+			var col = columns[i];
+			var width = (((col.size || 1) / size) * 100).floor(2);
+			data.push('<td style="width:{0}%" data-index="{1}" class="{2}"></td>'.format(width, i, col.class ? ' ' + col.class : ''));
+			header.push('<th class="ui-grid-columnname{3}{5}" style="width:{0}%;text-align:center" data-index="{1}" title="{2}" data-name="{4}"><div class="wrap"><i class="fa{3}"></i>{2}</div></th>'.format(width, i, col.text || col.name, col.class ? ' ' + col.class : '', col.name, col.sort === false ? '' : ' ui-grid-columnsort'));
+			if (col.filter === false)
+				filter.push('<th class="ui-grid-columnfilterempty ui-grid-columnfilter{1}" style="width:{0}%">&nbsp;</th>'.format(width, col.class ? ' ' + col.class : ''));
+			else
+				filter.push('<th class="ui-grid-columnfilter{4}" style="width:{0}%"><input type="text" placeholder="{3}" name="{2}" autocomplete="off" class="ui-grid-filter" /></th>'.format(width, i, col.name, col.filter || config.filterlabel, col.class ? ' ' + col.class : ''));
+		}
 
-		index = meta.indexOf('<tr');
-		var tr = meta.substring(index, meta.indexOf('>') + 1);
+		tbodyhead.html(data.join('') + '</tr>');
+		thead.html(header.join('') + '</tr>' + (config.filter ? (filter.join('') + '</tr>') : ''));
+		!init && self.refresh();
+		isFilter = false;
+		options.filter = {};
+	};
 
-		if (tr.indexOf('class') === -1)
-			meta = meta.replace(tr, '<tr class="grid-row"' + tr.substring(3));
-		else
-			meta = meta.replace(tr, tr.replace('class="', 'class="grid-row '));
+	self.fill = function(count) {
 
-		self.template = Tangular.compile(meta);
-		self.html('<div class="ui-grid"><div class="head"><table><thead></thead></table></div><div class="ui-grid-scroller"><table class="data"><tbody></tbody></table></div><div class="footer"><div class="meta"></div><div class="pagination"><button class="ui-grid-button" name="first"><i class="fa fa-angle-double-left"></i></button><button class="ui-grid-button" name="prev"><i class="fa fa-angle-left"></i></button><div class="page"><input type="text" maxlength="5" class="ui-grid-input" /></div><button class="ui-grid-button" name="next"><i class="fa fa-angle-right"></i></button><button class="ui-grid-button" name="last"><i class="fa fa-angle-double-right"></i></button></div><div class="pages"></div></div></div>');
+		if (config.autosize === false)
+			return;
 
-		tbody = $(self.find('tbody').get(0));
-		thead = $(self.find('thead').get(0));
-		container = $(self.find('.ui-grid-scroller').get(0));
-		head = self.find('.head');
-		pagination = VIRTUALIZE(self.find('.footer'), { page: 'input', first: 'button[name="first"]', last: 'button[name="last"]', prev: 'button[name="prev"]', next: 'button[name="next"]', meta: '.meta', pages: '.pages' });
+		tbody.find('.emptyfill').remove();
+		var builder = ['<tr class="emptyfill">'];
 
-		config.head === false && head.aclass('hidden');
+		var cols = options.columns;
+		for (var i = 0, length = cols.length; i < length; i++) {
+			if (!cols[i].hidden)
+				builder.push('<td{0}>'.format(cols[i].class ? (' class="' + cols[i].class + '"') : '') + (i ? '' : '<div class="wrap">&nbsp;</div>') + '</td>');
+		}
 
-		self.event('click', '.ui-grid-button', function() {
-			switch (this.name) {
-				case 'first':
-					EXEC(config.pageclick, 1);
-					break;
-				case 'last':
-					EXEC(config.pageclick, cache.pages);
-					break;
-				case 'prev':
-					var page = cache.page - 1;
-					EXEC(config.pageclick, page > 0 ? page : cache.pages);
-					break;
-				case 'next':
-					var page = cache.page + 1;
-					EXEC(config.pageclick, page > cache.pages ? 1 : page);
-					break;
-			}
-		});
-
-		self.event('change', '.ui-grid-input', function() {
-			var page = (+this.value) >> 0;
-			if (isNaN(page) || page < 0 || page > cache.pages || page === cache.page)
-				return;
-			EXEC(config.pageclick, page);
-		});
-
-		self.event('click', 'th', function() {
-			if (!config.columnclick)
-				return;
-			var obj = {};
-			obj.columns = options.columns;
-			obj.column = options.columns[+$(this).attr('data-index')];
-			EXEC(config.columnclick, obj);
-		});
-
-		self.on('resize', self.resize);
-		self.resize();
+		builder.push('</tr>');
+		builder = builder.join('');
+		var buffer = [];
+		for (var i = 0; i < count; i++)
+			buffer.push(builder);
+		tbody.append(buffer.join(''));
 	};
 
 	self.resize = function() {
@@ -123,20 +152,198 @@ COMPONENT('grid', 'pagination:true;head:true;pluralizepages:# pages,# page,# pag
 
 		var value = options.items;
 		var parent = self.parent();
-		var height = parent.height() - (config.padding || 0) - (config.offset ? self.element.offset().top : 0) - (config.head ? 40 : 0) - (config.pagination ? 55 : 0);
+		var height = parent.height() - (config.padding || 0);
 
-		container.height(height);
-		var count = (height / (config.rowheight || 28)) >> 0;
-		container.tclass('noscroll', count > value.length);
-		!cache && config.init && EXEC(config.init, count, height);
+		container.height(height - (config.pagination ? 124 : 74));
+
+		var count = (height / 28) >> 0;
+		if (count > value.length) {
+			self.fill((count + 1) - value.length);
+			container.aclass('ui-grid-noscroll');
+		} else
+			container.rclass('ui-grid-noscroll');
+	};
+
+	self.filter = function() {
+		isFilter = Object.keys(options.filter).length > 0;
+		!config.external && self.refresh();
+		self.operation('filter');
+	};
+
+	self.operation = function(type) {
+		config.exec && EXEC(config.exec, type, isFilter ? options.filter : null, options.lastsort ? options.lastsort : null, cache.page);
+	};
+
+	self.sort = function(data) {
+
+		options.lastsortelement && options.lastsortelement.rclass('fa-caret-down fa-caret-up');
+
+		if (data.column.sorting === 'desc') {
+			options.lastsortelement.find('.fa').rclass('fa-caret-down fa-caret-up');
+			options.lastsortelement = null;
+			options.lastsort = null;
+			data.column.sorting = null;
+
+			if (config.external)
+				self.operation('sort');
+			else
+				self.refresh();
+
+		} else if (data.column) {
+			data.column.sorting = data.column.sorting === 'asc' ? 'desc' : 'asc';
+			options.lastsortelement = thead.find('th[data-name="{0}"]'.format(data.column.name)).find('.fa').tclass('fa-caret-down', data.column.sorting === 'asc').tclass('fa-caret-up', data.column.sorting === 'desc');
+			options.lastsort = data.column;
+
+			var name = data.column.name;
+			var sort = data.column.sorting;
+
+			!config.external && options.lastsort && options.items.quicksort(name, sort === 'asc');
+			self.operation('sort');
+			self.redraw();
+		}
+	};
+
+	self.can = function(row) {
+
+		var keys = Object.keys(options.filter);
+		var cache = {};
+
+		for (var i = 0; i < keys.length; i++) {
+
+			var column = keys[i];
+			var val = row[column];
+			var filter = options.filter[column];
+			var type = typeof(val);
+			var val2 = cache[column];
+
+			if (type === 'number') {
+
+				if (val2 == null)
+					val2 = cache[column] = self.parseNumber(filter);
+
+				if (val2.length === 1 && val !== val2[0])
+					return false;
+
+				if (val < val2[0] || val > val2[1])
+					return false;
+
+			} else if (type === 'string') {
+
+				if (val2 == null) {
+					val2 = cache[column] = filter.split(/-|,/).trim();
+					for (var j = 0; j < val2.length; j++)
+						val2[j] = val2[j].toSearch();
+				}
+
+				var is = false;
+				var s = val.toSearch();
+
+				for (var j = 0; j < val2.length; j++) {
+					if (s.indexOf(val2[j]) !== -1) {
+						is = true;
+						break;
+					}
+				}
+
+				if (!is)
+					return false;
+
+			} else if (type === 'boolean') {
+				if (val2 == null)
+					val2 = cache[column] = config.boolean.indexOf(filter.replace(/\s/g, '')) !== -1;
+				if (val2 !== val)
+					return false;
+			} else if (val instanceof Date) {
+
+				val.setHours(0);
+				val.setMinutes(0);
+
+				if (val2 == null) {
+					val2 = cache[column] = filter.replace(/\s/g, '').split('-').trim();
+					for (var j = 0; j < val2.length; j++)
+						val2[j] = self.parseDate(val2[j]);
+				}
+
+				if (val2.length === 1 && val.format('yyyyMMdd') !== val2[0].format('yyyyMMdd'))
+					return false;
+				if (val < val2[0] || val > val2[1])
+					return false;
+			} else
+				return false;
+		}
+
+		return true;
+	};
+
+	self.parseDate = function(val) {
+		var index = val.indexOf('.');
+		if (index === -1) {
+			if ((/[a-z]+/).test(val))
+				return DATETIME.add(val);
+			if (val.length === 4)
+				return new Date(+val, 1, 1);
+		} else if (val.indexOf('.', index + 1) === -1) {
+			var a = val.split('.');
+			return new Date(DATETIME.getFullYear(), +a[1] - 1, +a[0]);
+		}
+		return val.parseDate();
+	};
+
+	self.parseNumber = function(val) {
+		var arr = [];
+		var num = val.replace(/\s/g, '').split('-');
+		var minus = false;
+
+		for (var i = 0, length = num.length; i < length; i++) {
+			var n = num[i];
+			if (n === '') {
+				minus = true;
+				continue;
+			}
+			arr.push(minus ? (+n) * -1 : +n);
+		}
+		return arr;
+	};
+
+	self.reset = function() {
+		options.filter = {};
+		isFilter = false;
+		thead.find('input').val('');
+		thead.find('.ui-grid-selected').rclass('ui-grid-selected');
+		options.lastsortelement && options.lastsortelement.rclass('fa-caret-down fa-caret-up');
+		options.lastsortelement = null;
+		if (options.lastsort)
+			options.lastsort.sorting = null;
+		options.lastsort = null;
+	};
+
+	self.redraw = function() {
+
+		var items = options.items;
+		var columns = options.columns;
+		var builder = [];
+		var m = {};
+
+		for (var i = 0, length = items.length; i < length; i++) {
+			builder.push('<tr class="grid-row">');
+			for (var j = 0, jl = columns.length; j < jl; j++) {
+				var column = columns[j];
+				var val = items[i][column.name];
+				m.value = column.render ? column.render(val, column) : val == null ? '' : (column.format ? val.format(column.format) : val);
+				m.index = j;
+				m.align = column.align;
+				m.background = column.background;
+				builder.push(self.template(m, column));
+			}
+			builder.push('</tr>');
+		}
+
+		tbody.html(builder.join(''));
+		container.rclass('noscroll');
+		self.resize();
 	};
 
 	self.setter = function(value) {
-
-		if (!value) {
-			self.aclass('hidden');
-			return;
-		}
 
 		// value.items
 		// value.limit
@@ -144,51 +351,36 @@ COMPONENT('grid', 'pagination:true;head:true;pluralizepages:# pages,# page,# pag
 		// value.pages
 		// value.count
 
+		if (!value) {
+			tbody.empty();
+			self.resize();
+			return;
+		}
+
 		cache = value;
 
-		pagination.prev.prop('disabled', value.page === 1);
-		pagination.first.prop('disabled', value.page === 1);
-		pagination.next.prop('disabled', value.page >= value.pages);
-		pagination.last.prop('disabled', value.page === value.pages);
-
-		pagination.page.val(value.page);
-
-		pagination.meta.html(value.count.pluralize.apply(value.count, pitems));
-		pagination.pages.html(value.pages.pluralize.apply(value.pages, ppages));
-
-		var columns = options.columns;
-		var builder = [];
-		var header = [];
-
-		builder.push('<tr class="empty">');
-		for (var i = 0, length = columns.length; i < length; i++) {
-			var column = columns[i];
-			var width = column.width ? ('width:' + column.width + ';') : '';
-			builder.push('<td style="{0}" data-index="{1}"{2}></td>'.format(width, i, column.class ? (' class="' + column.class + '"') : ''));
-			header.push('<th style="{0}{4}{6}" data-index="{1}" title="{5}"{7}><div class="wrap">{3}{2}</div></th>'.format(width, i, column.text, (column.sort ? ('<i class="fa fa-sort-{0}"></i>'.format(column.sort)) : '') + (column.icon ? '<i class="fa fa-{0}"></i>'.format(column.icon) : ''), column.align ? ('text-align:' + column.align + ';') : '', column.title || column.text, column.bg ? ('background-color:' + column.bg) : '', column.class ? (' class="' + column.class + '"') : ''));
+		if (config.pagination) {
+			pagination.prev.prop('disabled', value.page === 1);
+			pagination.first.prop('disabled', value.page === 1);
+			pagination.next.prop('disabled', value.page >= value.pages);
+			pagination.last.prop('disabled', value.page === value.pages);
+			pagination.page.val(value.page);
+			pagination.meta.html(value.count.pluralize.apply(value.count, pitems));
+			pagination.pages.html(value.pages.pluralize.apply(value.pages, ppages));
 		}
 
-		builder.push('</tr>');
-		options.items = value.items;
-
-		var model = {};
-
-		for (var i = 0, length = value.items.length; i < length; i++) {
-			model.index = i;
-			builder.push(self.template(value.items[i], model));
+		if (config.external) {
+			options.items = value.items;
+		} else {
+			options.items = [];
+			for (var i = 0, length = value.items.length; i < length; i++) {
+				if (isFilter && !self.can(value.items[i]))
+					continue;
+				options.items.push(value.items[i]);
+			}
+			options.lastsort && options.items.quicksort(options.lastsort.name, options.lastsort.sorting === 'asc');
 		}
 
-		var tmp = header.join('');
-
-		if (tmp !== options.head) {
-			options.head = tmp;
-			thead.empty().append('<tr>{0}</tr>'.format(options.head));
-		}
-
-		tbody.empty().append(builder.join(''));
-		container.rclass('noscroll');
-		self.rclass('hidden');
-
-		setTimeout(self.resize, 500);
+		self.redraw();
 	};
 });
--- j-Grid/readme.md
+++ j-Grid/readme.md
@@ -17,37 +17,30 @@ __Data-source__ needs to contain:
 
 __Configuration__:
 
-- `pagination` {Boolean} shows pagination panel (optional, default: `true`)
-- `head` {Boolean} shows a head (optional, default: `true`)
+- `filter` {Boolean} shows a filter (optional, default: `true`)
+- `filterlabel` {String} a default placeholder for all filters (optional)
+- `external` {Boolean} is an external data-source (dynamic data loading) (optinal, default: `false`)
 - `pluralizepages` {String} pluralization for pages (optional, default: `# pages,# page,# pages,# pages`)
 - `pluralizeitems` {String} pluralization for items (optional, default: `# items,# item,# items,# items`)
-- `pageclick` {String} a link to `function(page)`, it's evaluated if the pagination is changed
-- `columnclick` {String} a link to `function({ columns: [], column: {} })`, it's evaluated if on the column is clicked
-- `init` {String} a link to `function(count, height)`, it's evaluated if the grid is initialized
-- `rowheight` {Number} row height (optional, default: `28`)
+- `exec` {String} a link to `function(type, filter, sort, page)`, it's evaluated if the grid performs some operations
 - `padding` {Number} height padding (optional, default: `0`)
-- `offset` {Boolean} enables Y offset of parent element (optional, default `false`)
 - `autosize` {Boolean} enables full-height auto-size (optional, default `true`)
+- `boolean` {String} a values for filtering of boolean values (optional, default: `true|on|yes`)
+- `pagination` {Boolean} shows pagination panel (optional, default: `false`)
 
-__Column attributes in the template__:
+__Column properties__:
+{ name: 'name', text: 'Name', size: 2, render: 'n => n.toUpperCase()', filter: 'Search in name' },
 
-- `name="price"` {String} a property name for e.g. sorting (required)
-- `text="Price"` {String} a column name for column header (optional, default: `property name`)
-- `width="150"` {Number} width in pixels (optional)
-- `align="center"` {String} can contain `left` (default), `right` or `center` for column header
-- `title="Tooltip"` {String} can contain a tooltip text for column header (optional)
-- `icon="home"` {String} font-awesome icon without `fa-` for column header (optional)
-- `bg="#F0F0F0` {String} a background color for column header (optional)
-
-__Disabling column borders__
-
-- just add a class `grid-noborder`
-
-```html
-<div data-jc="grid" class="grid-noborder" ...>
-    ...
-</div>
-```
+- `name` {String} a name of field in the object
+- `text` {String} a column label
+- `size` {Number} a column width: `1` small, `2` medium, `3` large, `4` very large (optional, default `1`)
+- `filter` {String} a placeholder for the filter (optional)
+- `format` {String/Number} for formatting values (`string` for date, `number` for numbers)
+- `background` {String} a background color (optional)
+- `align` {String} can be `center` or `right` (optional, default: `left`)
+- `render` {String/Function} a custom rendering, `string` is evaluated as an arrow function
+- `sort` {Boolean} enables sorting (optional, default: `true`)
+- `class` {String} additional classes for the column, e.g. `hidden-xs`
 
 ### Author
 
