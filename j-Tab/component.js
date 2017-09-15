b0d12593cf5417586586ada7981213c75d27b9f4
Peter Širka <petersirka@gmail.com>
2017-09-15 10:46:18 +0200

    Improve j-Tab.

--- j-Tab/component.js
+++ j-Tab/component.js
@@ -1,18 +1,19 @@
-COMPONENT('tabmenu', function(self) {
-	var old = null;
+COMPONENT('tabmenu', 'class:selected', function(self, config) {
+	var old, oldtab;
+
 	self.readonly();
 	self.make = function() {
 		self.event('click', 'li', function() {
 			var el = $(this);
-			!el.hasClass('selected') && self.set(el.attr('data-value'));
+			!el.hclass(config.class) && self.set(el.attr('data-value'));
 		});
 	};
 
 	self.setter = function(value) {
 		if (old === value)
 			return;
-		self.find('.selected').rclass('selected');
-		self.find('li[data-value="' + value + '"]').aclass('selected');
+		oldtab && oldtab.rclass(config.class);
+		oldtab = self.find('li[data-value="' + value + '"]').aclass(config.class);
 		old = value;
 	};
 });
