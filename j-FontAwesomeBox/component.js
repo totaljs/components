a6b0b0773c4d9057973936a1349448b8f1ec780a
Peter Širka <petersirka@gmail.com>
2017-10-18 09:19:40 +0200

    Fix height.

--- j-FontAwesomeBox/component.js
+++ j-FontAwesomeBox/component.js
@@ -14,7 +14,7 @@ COMPONENT('fontawesomebox', 'height:300;fa:false', function(self, config) {
 	self.make = function() {
 		self.aclass('ui-fontawesomebox');
 		self.css('height', config.height + 'px');
-		self.append('<div class="ui-fontawesomebox-search"><span><i class="fa fa-search clearsearch"></i></span><div><input type="text" maxlength="50" placeholder="{0}" /></div></div><div></div><div class="ui-fontawesomebox-icons"><ul style="height:{1}px"></ul></div>'.format(config.search, config.height));
+		self.append('<div class="ui-fontawesomebox-search"><span><i class="fa fa-search clearsearch"></i></span><div><input type="text" maxlength="50" placeholder="{0}" /></div></div><div></div><div class="ui-fontawesomebox-icons"><ul style="height:{1}px"></ul></div>'.format(config.search, config.height - 40));
 		container = $(self.find('.ui-fontawesomebox-icons').find('ul').get(0));
 		input = self.find('input');
 		icon = self.find('.ui-fontawesomebox-search').find('.fa');
