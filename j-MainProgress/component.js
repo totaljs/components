d633ce76d1b8924a7b1f95f042bec5f279472edb
Peter Širka <petersirka@gmail.com>
2017-10-31 10:59:30 +0100

    Add: j-MainProgress.

---
+++ j-MainProgress/component.js
@@ -0,0 +1,28 @@
+COMPONENT('mainprogress', function(self) {
+
+	var old = null;
+
+	self.singleton();
+	self.readonly();
+
+	self.make = function() {
+		self.aclass('ui-mainprogress hidden');
+	};
+
+	self.setter = function(value) {
+		!value && (value = 0);
+
+		if (old === value)
+			return;
+
+		if (value > 100)
+			value = 100;
+		else if (value < 0)
+			value = 0;
+
+		old = value >> 0;
+
+		self.element.stop().animate({ width: old + '%' }, 80).show();
+		self.tclass('hidden', old === 0 || old === 100);
+	};
+});
---
+++ j-MainProgress/readme.md
@@ -0,0 +1,13 @@
+## j-MainProgress
+
+This component is singleton.
+
+__Configuration__:
+
+- `animate` {Boolean} enables animation (default: `true`)
+
+### Author
+
+- Peter Širka <petersirka@gmail.com>
+- License: MIT
+
