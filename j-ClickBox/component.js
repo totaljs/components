42dcaf4aeaf10a80f0f46eb66e17df980aeb4f6f
Peter Širka <petersirka@gmail.com>
2017-11-10 08:59:43 +0100

    Fix new fix.

--- j-ClickBox/component.js
+++ j-ClickBox/component.js
@@ -1,16 +1,11 @@
 COMPONENT('clickbox', function(self, config) {
 
-	var visible = false;
-
 	self.readonly();
 	self.blind();
 
 	self.init = function() {
 		$(document).on('click', function() {
-			if (visible) {
-				$('.ui-clickbox-visible').rclass('ui-clickbox-visible');
-				visible = false;
-			}
+			$('.ui-clickbox-visible').rclass('ui-clickbox-visible');
 		});
 	};
 
@@ -25,11 +20,9 @@ COMPONENT('clickbox', function(self, config) {
 			var cls = 'ui-clickbox-visible';
 			if (self.hclass(cls)) {
 				self.rclass(cls);
-				visible = false;
 				return;
 			}
 
-			visible = true;
 			setTimeout(function() {
 				self.aclass(cls);
 			}, 50);
