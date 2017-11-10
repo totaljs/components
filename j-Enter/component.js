3421e117becea7f9858f7d59e4f72d7984e3a7d5
Peter Širka <petersirka@gmail.com>
2017-11-10 08:54:58 +0100

    Improve configuration.

--- j-Enter/component.js
+++ j-Enter/component.js
@@ -1,8 +1,9 @@
-COMPONENT('enter', function(self) {
+COMPONENT('enter', 'validate:true', function(self) {
 	self.readonly();
 	self.make = function() {
 		self.event('keydown', 'input', function(e) {
-			e.which === 13 && MAIN.can(self.path) && EXEC(self.attrd('enter'));
+			if (e.which === 13 && (!config.validate || MAIN.can(self.path)))
+					EXEC(self.attrd('enter'));
 		});
 	};
 });
