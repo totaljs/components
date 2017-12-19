1e34e7c77c81fbd9ae5054598128aadba99b75bc
Peter Širka <petersirka@gmail.com>
2017-12-19 08:50:12 +0100

    Add versioning.

--- cdn.js
+++ cdn.js
@@ -3,22 +3,29 @@ require('total.js');
 
 Fs.readdir(process.cwd(), function(err, dir) {
 	var path = '0cdn/';
-	U.ls(path, function(files) {
-		files.wait((item, next) => Fs.unlink(item, next), function() {
-			dir.wait(function(item, next) {
-				if (item.substring(0, 2) !== 'j-')
-					return next();
+	//	U.ls(path, function(files) {
+	//		files.wait((item, next) => Fs.unlink(item, next), function() {
+	dir.wait(function(item, next) {
 
-				var builder = [];
-				Fs.readFile(item + '/component.css', function(err, css) {
-					css && builder.push('<style>{0}</style>'.format(U.minifyStyle('/*auto*/\n' + css.toString('utf8'))));
-					Fs.readFile(item + '/component.js', function(err, js) {
-						js && builder.push('<script>{0}</script>'.format(U.minifyScript(js.toString('utf8'))));
-						Fs.writeFile(path + item.toLowerCase() + '.html', builder.join('\n'), next);
+		if (item.substring(0, 2) !== 'j-')
+			return next();
+
+		var builder = [];
+		Fs.readFile(item + '/component.json', function(err, meta) {
+			meta = meta.toString('utf8').parseJSON();
+			Fs.readFile(item + '/component.css', function(err, css) {
+				css && builder.push('<style>{0}</style>'.format(U.minifyStyle('/*auto*/\n' + css.toString('utf8'))));
+				Fs.readFile(item + '/component.js', function(err, js) {
+					js && builder.push('<script>{0}</script>'.format(U.minifyScript(js.toString('utf8'))));
+					var name = item.toLowerCase();
+					Fs.writeFile(path + name + '.html', builder.join('\n'), function() {
+						Fs.writeFile(path + name + '@{0}.html'.format(meta.version), builder.join('\n').replace('COMPONENT(\'' + name.substring(2), 'COMPONENT(\'' + name.substring(2) + '@' + meta.version), next);
 						console.log('Processing:', item);
 					});
 				});
-			}, () => console.log('100% done, you need to upload all files "{0}*.html" on CDN.'.format(path)));
+			});
 		});
-	});
+	}, () => console.log('100% done, you need to upload all files "{0}*.html" on CDN.'.format(path)));
+	//		});
+	//	});
 });
