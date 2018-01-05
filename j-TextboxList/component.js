60a03ecb1307244b7006c0f52103d7c9f65fb232
Pedro Maia Costa <info@pmcdigital.pt>
2018-01-05 18:42:57 +0000

    textboxlist required and type validation

--- j-TextboxList/component.js
+++ j-TextboxList/component.js
@@ -1,178 +1,222 @@
-COMPONENT('textboxlist', 'maxlength:100;error:You reach the maximum limit', function(self, config) {
+COMPONENT('textboxlist', 'maxlength:100;required:false;error:You reach the maximum limit', function (self, config) {
 
-	var container, content;
-	var empty = {};
-	var skip = false;
-	var cempty = 'empty';
-	var helper = null;
+    var container, content;
+    var empty = {};
+    var skip = false;
+    var cempty = 'empty';
+    var helper = null;
 
-	self.readonly();
-	self.template = Tangular.compile('<div class="ui-textboxlist-item"><div><i class="fa fa-times"></i></div><div><input type="text" maxlength="{{ max }}" placeholder="{{ placeholder }}"{{ if disabled}} disabled="disabled"{{ fi }} value="{{ value }}" /></div></div>');
+    // PMC: removed self.readonly();
+    self.template = Tangular.compile('<div class="ui-textboxlist-item"><div><i class="fa fa-times"></i></div><div><input type="text" maxlength="{{ max }}" placeholder="{{ placeholder }}"{{ if disabled}} disabled="disabled"{{ fi }} value="{{ value }}" /></div></div>');
 
-	self.configure = function(key, value, init, prev) {
-		if (init)
-			return;
+    self.configure = function (key, value, init, prev) {
+        if (init)
+            return;
 
-		var redraw = false;
-		switch (key) {
-			case 'disabled':
-				self.tclass('ui-required', value);
-				self.find('input').prop('disabled', true);
-				empty.disabled = value;
-				break;
-			case 'maxlength':
-				empty.max = value;
-				self.find('input').prop(key, value);
-				break;
-			case 'placeholder':
-				empty.placeholder = value;
-				self.find('input').prop(key, value);
-				break;
-			case 'label':
-				redraw = true;
-				break;
-			case 'icon':
-				if (value && prev)
-					self.find('i').rclass().aclass(value);
-				else
-					redraw = true;
-				break;
-		}
+        var redraw = false;
+        switch (key) {
+            case 'disabled':
+                self.tclass('ui-required', value);
+                self.find('input').prop('disabled', true);
+                empty.disabled = value;
+                break;
+            case 'maxlength':
+                empty.max = value;
+                self.find('input').prop(key, value);
+                break;
+            case 'placeholder':
+                empty.placeholder = value;
+                self.find('input').prop(key, value);
+                break;
+            case 'label':
+                redraw = true;
+                break;
+            case 'icon':
+                if (value && prev)
+                    self.find('i').rclass().aclass(value);
+                else
+                    redraw = true;
+                break;
+        }
 
-		if (redraw) {
-			skip = false;
-			self.redraw();
-			self.refresh();
-		}
-	};
+        if (redraw) {
+            skip = false;
+            self.redraw();
+            self.refresh();
+        }
+    };
 
-	self.redraw = function() {
+    self.redraw = function () {
 
-		var icon = '';
-		var html = config.label || content;
+        var icon = '';
+        var html = config.label || content;
 
-		if (config.icon)
-			icon = '<i class="fa fa-{0}"></i>'.format(config.icon);
+        if (config.icon)
+            icon = '<i class="fa fa-{0}"></i>'.format(config.icon);
 
-		empty.value = '';
-		self.html((html ? '<div class="ui-textboxlist-label">{1}{0}:</div>'.format(html, icon) : '') + '<div class="ui-textboxlist-items"></div>' + self.template(empty).replace('-item"', '-item ui-textboxlist-base"'));
-		container = self.find('.ui-textboxlist-items');
-	};
+        empty.value = '';
 
-	self.make = function() {
+        // PMC: added required label class
+        self.html((html ? '<div class="ui-textboxlist-label{2}">{1}{0}:</div>'.format(html, icon, config.required ? ' ui-textboxlist-label-required' : '') : '') + '<div class="ui-textboxlist-items"></div>' + self.template(empty).replace('-item"', '-item ui-textboxlist-base"'));
+        container = self.find('.ui-textboxlist-items');
+    };
 
-		empty.max = config.max;
-		empty.placeholder = config.placeholder;
-		empty.value = '';
-		empty.disabled = config.disabled;
+    self.make = function () {
 
-		if (config.disabled)
-			self.aclass('ui-disabled');
+        empty.max = config.max;
+        empty.placeholder = config.placeholder;
+        empty.value = '';
+        empty.disabled = config.disabled;
 
-		content = self.html();
-		self.aclass('ui-textboxlist');
-		self.redraw();
+        if (config.disabled)
+            self.aclass('ui-disabled');
 
-		self.event('click', '.fa-times', function() {
+        content = self.html();
+        self.aclass('ui-textboxlist');
+        self.redraw();
 
-			if (config.disabled)
-				return;
+        self.event('click', '.fa-times', function () {
 
-			var el = $(this);
-			var parent = el.closest('.ui-textboxlist-item');
-			var value = parent.find('input').val();
-			var arr = self.get();
+            if (config.disabled)
+                return;
 
-			helper != null && helper.remove();
-			helper = null;
+            var el = $(this);
+            var parent = el.closest('.ui-textboxlist-item');
+            var value = parent.find('input').val();
+            var arr = self.get();
 
-			parent.remove();
+            helper != null && helper.remove();
+            helper = null;
 
-			var index = arr.indexOf(value);
-			if (index === -1)
-				return;
+            parent.remove();
 
-			arr.splice(index, 1);
+            var index = arr.indexOf(value);
+            if (index === -1)
+                return;
 
-			self.tclass(cempty, arr.length === 0);
+            arr.splice(index, 1);
 
-			skip = true;
-			self.set(self.path, arr, 2);
-			self.change(true);
-		});
+            self.tclass(cempty, arr.length === 0);
+            // PMC: toggle required class when removing items
+            self.tclass('required', config.required && arr.length === 0);
 
-		self.event('change keypress', 'input', function(e) {
+            skip = true;
+            self.set(self.path, arr, 2);
+            self.change(true);
+        });
 
-			if (config.disabled || (e.type !== 'change' && e.which !== 13))
-				return;
+        // PMC: added blur event for base input auto submit
+        self.event('change keypress blur', 'input', function (e) {
 
-			var el = $(this);
+            if ((e.type === 'keypress' && e.which !== 13) || config.disabled)
+                return;
 
-			var value = this.value.trim();
-			if (!value)
-				return;
+            var el = $(this);
 
-			var arr = [];
-			var base = el.closest('.ui-textboxlist-base');
-			var len = base.length > 0;
+            var value = this.value.trim();
+            if (!value)
+                return;
 
-			if (len && e.type === 'change')
-				return;
+            var arr = [];
+            var base = el.closest('.ui-textboxlist-base');
+            var len = base.length > 0;
 
-			var raw = self.get();
+            if (len && e.type === 'change')
+                return;
 
-			if (config.limit && raw.length >= config.limit) {
+            var raw = self.get();
 
-				if (helper != null)
-					return;
+            if (config.limit && raw.length >= config.limit) {
 
-				base.after('<div class="ui-textboxlist-helper"><i class="fa fa-warning" aria-hidden="true"></i> {0}</div>'.format(config.error));
-				helper = container.closest('.ui-textboxlist').find('.ui-textboxlist-helper');
-				return;
-			}
+                if (helper != null)
+                    return;
 
-			if (len) {
+                base.after('<div class="ui-textboxlist-helper"><i class="fa fa-warning" aria-hidden="true"></i> {0}</div>'.format(config.error));
+                helper = container.closest('.ui-textboxlist').find('.ui-textboxlist-helper');
+                return;
+            }
 
-				if (!raw || raw.indexOf(value) === -1)
-					self.push(self.path, value, 2);
+            if (len) {
 
-				this.value = '';
-				self.change(true);
-				return;
-			}
+                if (!raw || raw.indexOf(value) === -1)
+                    self.push(self.path, value, 2);
 
-			container.find('input').each(function() {
-				arr.push(this.value.trim());
-			});
+                this.value = '';
+                self.change(true);
+                return;
+            }
 
-			skip = true;
-			self.set(self.path, arr, 2);
-			self.change(true);
-		});
-	};
+            container.find('input').each(function () {
+                arr.push(this.value.trim());
+            });
 
-	self.setter = function(value) {
+            skip = true;
+            self.set(self.path, arr, 2);
+            self.change(true);
+        });
+    };
 
-		if (skip) {
-			skip = false;
-			return;
-		}
+    self.setter = function (value) {
 
-		if (!value || !value.length) {
-			self.aclass(cempty);
-			container.empty();
-			return;
-		}
+        if (skip) {
+            skip = false;
+            return;
+        }
 
-		self.rclass(cempty);
+        if (!value || !value.length) {
+            self.aclass(cempty);
 
-		var builder = [];
+            // PMC: add required class if required
+            config.required && self.aclass('required');
+            container.empty();
+            return;
+        }
 
-		value.forEach(function(item) {
-			empty.value = item;
-			builder.push(self.template(empty));
-		});
+        self.rclass(cempty);
 
-		container.empty().append(builder.join(''));
-	};
+        // PMC: remove required class
+        self.rclass('required');
+
+        var builder = [];
+        value.forEach(function (item) {
+            empty.value = item;
+            builder.push(self.template(empty));
+        });
+
+        container.empty().append(builder.join(''));
+    };
+
+    // PMC: added validate method for each item
+    self.validate = function (value) {
+        var valid = !config.required;
+        var items = container.children();
+        if (!value || !value.length || !items || !items.length) {
+            return valid;
+        }
+        value.forEach(function (item, i) {
+            !item && (item = '');
+            switch (config.type) {
+                case 'email':
+                    valid = item.isEmail();
+                    break;
+                case 'url':
+                    valid = item.isURL();
+                    break;
+                case 'currency':
+                case 'number':
+                    valid = item > 0;
+                    break;
+                case 'date':
+                    valid = item instanceof Date && !isNaN(item.getTime());
+                    // TODO: date string format validation
+                    break;
+                default:
+                    valid = item.length > 0;
+                    break;
+            }
+            items.eq(i).tclass('invalid', !valid);
+        });
+        return valid;
+    };
+
 });
