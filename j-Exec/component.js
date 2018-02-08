5b4e2c7848ab1776e6a5ee8dca688d22e28d0daf
Peter Širka <petersirka@gmail.com>
2018-02-08 09:55:56 +0100

    Improved `exec` component.

--- j-Exec/component.js
+++ j-Exec/component.js
@@ -4,10 +4,27 @@ COMPONENT('exec', function(self, config) {
 	self.make = function() {
 		self.event('click', config.selector || '.exec', function() {
 			var el = $(this);
-			var attr = el.attr('data-exec');
-			var path = el.attr('data-path');
+
+			var attr = el.attrd('exec');
+			var path = el.attrd('path');
+			var href = el.attrd('href');
+
 			attr && EXEC(attr, el);
-			path && SET(path, new Function('return ' + el.attr('data-value'))());
+			href && NAV.redirect(href);
+
+			if (path) {
+				var val = el.attrd('value');
+				if (val == null) {
+					var a = new Function('return ' + el.attrd('value-a'))();
+					var b = new Function('return ' + el.attrd('value-b'))();
+					var v = GET(path);
+					if (v === a)
+						SET(path, b, true);
+					else
+						SET(path, a, true);
+				} else
+					SET(path, new Function('return ' + val)(), true);
+			}
 		});
 	};
 });
--- j-Exec/readme.md
+++ j-Exec/readme.md
@@ -8,8 +8,9 @@ __Configuration__:
 __Elements__:
 
 1. Elements can contain `data-exec` attribut with a method name which will be execute when the user click on the element.
-
 2. Elements can contain these attributes `data-path="some.path` and `data-value="'STRING'"` or `data-value="340"` (number) or `data-value="{}"` (object). Clicking on the element sets `data-value` to `data-path`.
+3. Toggling value with these attributes `data-path="some.path` and `data-value-a="1"` (same functionality as `data-value`) and `data-value-b="2"` (same functionality as `data-value`)
+4. Elements can contain `data-href` attribute and the component performs `REDIRECT()`.
 
 ### Author
 
