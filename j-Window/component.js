3cf20a8f30095005c177dd218dc2e11f7d5687b5
Peter Širka <petersirka@gmail.com>
2018-02-23 20:33:17 +0100

    Added new component `j-Window`.

---
+++ j-Window/component.js
@@ -0,0 +1,127 @@
+COMPONENT('window', function(self, config) {
+
+	var W = window;
+	var header = null;
+
+	if (!W.$$window) {
+
+		W.$$window_level = W.$$window_level || 1;
+		W.$$window = true;
+
+		$(document).on('click', '.ui-window-button-close', function() {
+			SET($(this).attr('data-path'), '');
+		});
+
+		$(window).on('resize', function() {
+			SETTER('window', 'resize');
+		});
+	}
+
+	self.readonly();
+
+	self.hide = function() {
+		self.set('');
+	};
+
+	self.resize = function() {
+		var el = self.element.find('.ui-window-body');
+		el.height($(window).height() - self.find('.ui-window-header').height());
+	};
+
+	self.make = function() {
+
+		var icon;
+
+		if (config.icon)
+			icon = '<i class="fa fa-{0}"></i>'.format(config.icon);
+		else
+			icon = '<i></i>';
+
+		$(document.body).append('<div id="{0}" class="hidden ui-window-container"><div class="ui-window"><div class="ui-window-title"><button class="ui-window-button-close{5}" data-path="{2}"><i class="fa fa-times"></i></button>{4}<span>{3}</span></div><div class="ui-window-header"></div><div class="ui-window-body"></div></div>'.format(self._id, config.width || 800, self.path, config.title, icon, config.closebutton == false ? ' hidden' : ''));
+
+		var el = $('#' + self._id);
+		el.find('.ui-window-body').get(0).appendChild(self.element.get(0));
+		self.rclass('hidden');
+		self.replace(el);
+
+		header = self.virtualize({ title: '.ui-window-title > span', icon: '.ui-window-title > i' });
+
+		self.find('button').on('click', function() {
+			switch (this.name) {
+				case 'cancel':
+					self.hide();
+					break;
+			}
+		});
+	};
+
+	self.configure = function(key, value, init, prev) {
+		if (init)
+			return;
+		switch (key) {
+			case 'icon':
+				header.icon.rclass(header.icon.attr('class'));
+				value && header.icon.aclass('fa fa-' + value);
+				break;
+			case 'title':
+				header.title.html(value);
+				break;
+			case 'closebutton':
+				self.find('.ui-window-button-close').tclass(value !== true);
+				break;
+		}
+	};
+
+	self.setter = function(value) {
+
+		setTimeout2('noscroll', function() {
+			$('html').tclass('noscroll', !!$('.ui-window-container').not('.hidden').length);
+		}, 50);
+
+		var isHidden = value !== config.if;
+
+		if (self.hclass('hidden') === isHidden)
+			return;
+
+		setTimeout2('windowreflow', function() {
+			EMIT('reflow', self.name);
+		}, 10);
+
+		if (isHidden) {
+			self.aclass('hidden');
+			self.release(true);
+			self.find('.ui-window').rclass('ui-window-animate');
+			W.$$window_level--;
+			return;
+		}
+
+		if (W.$$window_level < 1)
+			W.$$window_level = 1;
+
+		W.$$window_level++;
+
+		self.css('z-index', W.$$window_level * 10);
+		self.element.scrollTop(0);
+		self.rclass('hidden');
+		self.release(false);
+		self.resize();
+
+		config.reload && EXEC(config.reload, self);
+		config.default && DEFAULT(config.default, true);
+
+		if (!isMOBILE && config.autofocus) {
+			var el = self.find(config.autofocus === true ? 'input[type="text"],select,textarea' : config.autofocus);
+			el.length && el.eq(0).focus();
+		}
+
+		setTimeout(function() {
+			self.element.scrollTop(0);
+			self.find('.ui-window').aclass('ui-window-animate');
+		}, 300);
+
+		// Fixes a problem with freezing of scrolling in Chrome
+		setTimeout2(self.id, function() {
+			self.css('z-index', (W.$$window_level * 10) + 1);
+		}, 1000);
+	};
+});
---
+++ j-Window/readme.md
@@ -0,0 +1,23 @@
+## j-Window
+
+- Works only with `+v11.1.0` [jComponent](http://jcomponent.org)
+- __Download__ [jComponent with Tangular (jcta.min.js)](https://github.com/petersirka/jComponent)
+- Works with Bootstrap
+
+__Configuration__:
+
+Example: `data-jc-config="title:Window title;if:user;icon:home"`
+
+- `title` {String} window title
+- `if` {String} condition for showing of the window, it's compared with the value within of `data-jc-path`
+- `icon` {String} Font-Awesome icon without `fa-`
+- `reload` {String} link to a global function and it's executed if the form is showing
+- `enter` {Boolean} (optional) captures `enter` key automatically and performs submit (default: `false`)
+- `autofocus` {Boolean/String} (optional) can focus an input. `String` === `jQuery selector` for the input
+- `default` {String} (optional) a short alias for `DEFAULT(default, true)`
+- `closebutton` {Boolean} (optional) can hide `x` button (default: `false`)
+
+### Author
+
+- Peter Širka <petersirka@gmail.com>
+- License: MIT
