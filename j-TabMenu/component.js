e5fcbf751be260bde9bfee26d39f133fe7a47d54
Peter Širka <petersirka@gmail.com>
2018-04-13 18:28:28 +0200

    Renamed component.

--- j-TabMenu/component.js
+++ j-TabMenu/component.js
@@ -1,11 +1,11 @@
-COMPONENT('tabmenu', 'class:selected', function(self, config) {
+COMPONENT('tabmenu', 'class:selected;selector:li', function(self, config) {
 	var old, oldtab;
 
 	self.readonly();
 	self.make = function() {
 		self.event('click', 'li', function() {
 			var el = $(this);
-			!el.hclass(config.class) && self.set(el.attr('data-value'));
+			!el.hclass(config.class) && self.set(el.attrd('value'));
 		});
 	};
 
@@ -13,7 +13,7 @@ COMPONENT('tabmenu', 'class:selected', function(self, config) {
 		if (old === value)
 			return;
 		oldtab && oldtab.rclass(config.class);
-		oldtab = self.find('li[data-value="' + value + '"]').aclass(config.class);
+		oldtab = self.find(config.selector + '[data-value="' + value + '"]').aclass(config.class);
 		old = value;
 	};
 });
