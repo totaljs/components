34d7312c06f28b6696f6eb86bd62a3524b72ef25
Peter Širka <petersirka@gmail.com>
2018-05-01 10:05:52 +0200

    Fixe component.

--- j-MarkdownPreview/component.js
+++ j-MarkdownPreview/component.js
@@ -1,22 +1,22 @@
 COMPONENT('markdownpreview', function(self) {
 
-	marked.setOptions({ gfm: true, breaks: true, sanitize: true, tables: true });
-
-	var M = function(text) {
-		return marked(text).replace(/<img/g, '<img class="img-responsive img-rounded"').replace(/<table/g, '<table class="table table-bordered"');
-	};
-
-	window.Markdown = M;
-
 	self.bindvisible();
 	self.readonly();
 
 	self.make = function() {
+		marked.setOptions({ gfm: true, breaks: true, sanitize: true, tables: true });
+		self.M = function(text) {
+			return marked(text).replace(/<img/g, '<img class="img-responsive img-rounded"').replace(/<table/g, '<table class="table table-bordered"');
+		};
 		self.aclass('ui-markdown');
+
 	};
 
 	self.setter = function(value) {
-		self.html(value ? M(value) : '');
+		self.html(value ? self.M(value) : '');
+		self.find('pre code').each(function(i, block) {
+			hljs.highlightBlock(block);
+		});
 	};
 
-}, ['//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/github.min.css', '//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js', '//cdnjs.cloudflare.com/ajax/libs/marked/0.3.19/marked.min.js']);
+}, ['https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/github.min.css', 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/marked/0.3.19/marked.min.js']);
