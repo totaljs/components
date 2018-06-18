3eff2a26ac18ae63ce311882c56119e0f68215e0
Peter Širka <petersirka@gmail.com>
2018-06-19 00:04:31 +0200

    Improved code.

--- j-JSONTree/component.js
+++ j-JSONTree/component.js
@@ -1,4 +1,4 @@
-COMPONENT('jsontree', function(self){
+COMPONENT('jsontree', function(self, config) {
 
 	var item = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABkAAAAZCAYAAADE6YVjAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAOxAAADsQBlSsOGwAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAABoSURBVEiJ7dSxDYAwDETRO4vBsk22QBFTJNuExXKp6CgMKBKFf23rdUcAhLNaayG5Ayg558P7Z97DLwUSSCA/QbY3TyRTa809R48QM4MkSEoA0hJkjAGSINklnUuQK0k9VjiQQAK5bwLU3xq8lh5dEwAAAABJRU5ErkJggg==';
 	var itemlast = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABkAAAAZCAYAAADE6YVjAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAOxAAADsQBlSsOGwAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAABnSURBVEiJ7ZSxDYAwDAQvyHvBNhkjyhgeJ5swSAqaFKmCMQ1CPum7f11jOQEJI6pagALUnHO17jZr8Q0hCUlIPiIR5+5Q1WVhfjteyT6ywifpvTcRacA5YiLx4At7+c91hSQkIbnnAlzgEDui0adaAAAAAElFTkSuQmCC';
@@ -6,21 +6,13 @@ COMPONENT('jsontree', function(self){
 	var spacerempty = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABkAAAAZCAYAAADE6YVjAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAOxAAADsQBlSsOGwAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAAZSURBVEiJ7cEBDQAAAMKg909tDwcUAAA3BgndAAF85o4iAAAAAElFTkSuQmCC';
 	var $tree, $textarea;
 
-	self.onClick = NOOP;
-
-	self.configure = function(key, value) {
-		switch (key) {
-			case 'click':
-				self.onClick = window[value];
-				break;
-		}
-	};
+	self.readonly();
 
 	self.make = function(){
 		var title = self.html();
 		self.empty();
 		self.aclass('jsontree');
-		self.append('<div class="jsontree-label"><i class="fa fa-code-fork fa-rotate-90"></i>&nbsp;{0}<button class="button button-small">Clear</button></div>'.format(title || 'JSON tree'));
+		self.append('<div class="jsontree-label"><i class="fa fa-code-fork fa-rotate-90"></i>&nbsp;{0}<button>Clear</button></div>'.format(title || 'JSON tree'));
 		self.append('<div class="jsontree-textarea"><div data-jc="textarea" data-jc-noscope="true" data-jc-path="{0}" data-jc-config="height:400"></div></div>'.format(self.path));
 		self.append('<div class="jsontree-tree hidden"></div>');
 		$textarea = self.find('.jsontree-textarea');
@@ -28,7 +20,7 @@ COMPONENT('jsontree', function(self){
 
 		self.event('click', 'span', function(){
 			var path = $(this).parent().attrd('path');
-			self.onClick(path);
+			config.click && EXEC(config.click, path, self);
 		});
 
 		self.event('click', 'button', function(){
