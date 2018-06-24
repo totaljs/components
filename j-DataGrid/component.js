a9405ed65ce207ab84cc7aa2bd80d618c7863f82
Peter Širka <petersirka@gmail.com>
2018-06-24 20:43:31 +0200

    Fixed hidden bugs.

--- j-DataGrid/component.js
+++ j-DataGrid/component.js
@@ -767,6 +767,10 @@ COMPONENT('datagrid', 'checkbox:true;colwidth:150;rowheight:24;filterlabel:Filte
 
 		// Get data
 		var obj = self.get();
+
+		if (obj == null)
+			return;
+
 		var items = obj instanceof Array ? obj : obj.items;
 		var output = [];
 
@@ -887,7 +891,7 @@ COMPONENT('datagrid', 'checkbox:true;colwidth:150;rowheight:24;filterlabel:Filte
 
 		opt.cluster = new Cluster(vbody, config.rowheight, 80);
 		opt.cluster.scroll = self.scrolling;
-		opt.cluster.update(opt.render);
+		opt.render && opt.cluster.update(opt.render);
 	};
 
 	self.scrolling = function() {
@@ -1084,5 +1088,132 @@ COMPONENT('datagrid', 'checkbox:true;colwidth:150;rowheight:24;filterlabel:Filte
 			arr.push(+n);
 		}
 		return arr;
+	};
+});
+
+COMPONENT('textarea', function(self, config) {
+
+	var input, container, content = null;
+
+	self.validate = function(value) {
+		if (config.disabled || !config.required)
+			return true;
+		if (value == null)
+			value = '';
+		else
+			value = value.toString();
+		return value.length > 0;
+	};
+
+	self.configure = function(key, value, init) {
+		if (init)
+			return;
+
+		var redraw = false;
+
+		switch (key) {
+			case 'readonly':
+				self.find('textarea').prop('readonly', value);
+				break;
+			case 'disabled':
+				self.tclass('ui-disabled', value);
+				self.find('textarea').prop('disabled', value);
+				break;
+			case 'required':
+				self.noValid(!value);
+				!value && self.state(1, 1);
+				self.find('.ui-textarea-label').tclass('ui-textarea-label-required', value);
+				break;
+			case 'placeholder':
+				input.prop('placeholder', value || '');
+				break;
+			case 'maxlength':
+				input.prop('maxlength', value || 1000);
+				break;
+			case 'label':
+				redraw = true;
+				break;
+			case 'autofocus':
+				input.focus();
+				break;
+			case 'monospace':
+				self.tclass('ui-textarea-monospace', value);
+				break;
+			case 'icon':
+				redraw = true;
+				break;
+			case 'format':
+				self.format = value;
+				self.refresh();
+				break;
+		}
+
+		redraw && setTimeout2('redraw' + self.id, function() {
+			self.redraw();
+			self.refresh();
+		}, 100);
+	};
+
+	self.redraw = function() {
+
+		var attrs = [];
+		var builder = [];
+
+		self.tclass('ui-disabled', config.disabled === true);
+		self.tclass('ui-textarea-monospace', config.monospace === true);
+
+		config.placeholder && attrs.attr('placeholder', config.placeholder);
+		config.maxlength && attrs.attr('maxlength', config.maxlength);
+		config.error && attrs.attr('error');
+		attrs.attr('data-jc-bind', '');
+		config.height && attrs.attr('style', 'height:{0}px'.format(config.height));
+		config.autofocus === 'true' && attrs.attr('autofocus');
+		config.disabled && attrs.attr('disabled');
+		config.readonly && attrs.attr('readonly');
+		builder.push('<textarea {0}></textarea>'.format(attrs.join(' ')));
+
+		var label = config.label || content;
+
+		if (!label.length) {
+			config.error && builder.push('<div class="ui-textarea-helper"><i class="fa fa-warning" aria-hidden="true"></i> {0}</div>'.format(config.error));
+			self.aclass('ui-textarea ui-textarea-container');
+			self.html(builder.join(''));
+			input = self.find('textarea');
+			container = self.element;
+			return;
+		}
+
+		var html = builder.join('');
+
+		builder = [];
+		builder.push('<div class="ui-textarea-label{0}">'.format(config.required ? ' ui-textarea-label-required' : ''));
+		config.icon && builder.push('<i class="fa fa-{0}"></i>'.format(config.icon));
+		builder.push(label);
+		builder.push(':</div><div class="ui-textarea">{0}</div>'.format(html));
+		config.error && builder.push('<div class="ui-textarea-helper"><i class="fa fa-warning" aria-hidden="true"></i> {0}</div>'.format(config.error));
+
+		self.html(builder.join(''));
+		self.rclass('ui-textarea');
+		self.aclass('ui-textarea-container');
+		input = self.find('textarea');
+		container = self.find('.ui-textarea');
+	};
+
+	self.make = function() {
+		content = self.html();
+		self.type = config.type;
+		self.format = config.format;
+		self.redraw();
+	};
+
+	self.state = function(type) {
+		if (!type)
+			return;
+		var invalid = config.required ? self.isInvalid() : false;
+		if (invalid === self.$oldstate)
+			return;
+		self.$oldstate = invalid;
+		container.tclass('ui-textarea-invalid', invalid);
+		config.error && self.find('.ui-textarea-helper').tclass('ui-textarea-helper-show', invalid);
 	};
 });
