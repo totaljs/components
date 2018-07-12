60388de66897a85191aa364cc481f2f1026112f5
Peter Širka <petersirka@gmail.com>
2018-07-12 14:25:17 +0200

    Added new component.

--- j-MarkdownEditor/component.js
+++ j-MarkdownEditor/component.js
@@ -1,20 +1,20 @@
 COMPONENT('markdowneditor', function (self) {
 
-    var input, editor;
+	var input, editor;
 
-    self.setter = function (value, path, type) {
-        if (type !== 3)
-            editor.value(value || '');
-    };
+	self.make = function () {
+		self.html('<textarea></textarea>');
 
-    self.make = function () {
-        self.html('<textarea></textarea>');
+		input = self.find('textarea')[0];
+		editor = new SimpleMDE({ element: input });
+		editor.codemirror.on('change', function () {
+			self.set(self.path, editor.value(), 3);
+		});
+	};
 
-        input = self.find('textarea')[0];
-        editor = new SimpleMDE({ element: input });
-        editor.codemirror.on('change', function () {
-            self.set(self.path, editor.value(), 3);
-        });
-    };
+	self.setter = function (value, path, type) {
+		if (type !== 3)
+			editor.value(value || '');
+	};
 
 }, ['https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js', 'https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css']);
