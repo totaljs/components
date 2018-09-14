75d94e2080de76731e2fced9acf7889d0fb6a47d
Peter Širka <petersirka@gmail.com>
2018-09-14 11:42:28 +0200

    Improved UI.

--- j-Panel/component.js
+++ j-Panel/component.js
@@ -7,15 +7,18 @@ COMPONENT('panel', 'width:350;icon:circle-o', function(self, config) {
 		W.$$panel_level = W.$$panel_level || 1;
 		W.$$panel = true;
 
-		$(document).on('click', '.ui-panel-button-close,.ui-panel-container', function(e) {
+		$(document).on('click touchend', '.ui-panel-button-close,.ui-panel-container', function(e) {
 			var target = $(e.target);
 			var curr = $(this);
 			var main = target.hclass('ui-panel-container');
 			if (curr.hclass('ui-panel-button-close') || main) {
 				var parent = target.closest('.ui-panel-container');
 				var com = parent.component();
-				if (!main || com.config.bgclose)
+				if (!main || com.config.bgclose) {
 					com.hide();
+					e.preventDefault();
+					e.stopPropagation();
+				}
 			}
 		});
 
@@ -32,7 +35,7 @@ COMPONENT('panel', 'width:350;icon:circle-o', function(self, config) {
 
 	self.resize = function() {
 		var el = self.element.find('.ui-panel-body');
-		el.height(WH - self.find('.ui-panel-header').height());
+		el.height(WH - (isMOBILE ? 0 : self.find('.ui-panel-header').height()));
 	};
 
 	self.icon = function(value) {
@@ -41,7 +44,7 @@ COMPONENT('panel', 'width:350;icon:circle-o', function(self, config) {
 	};
 
 	self.make = function() {
-		$(document.body).append('<div id="{0}" class="hidden ui-panel-container{3}"><div class="ui-panel" style="width:{1}px"><div data-bind="@config__change .ui-panel-icon:@icon__html span:value.title" class="ui-panel-title"><button class="ui-panel-button-close{2}"><i class="fa fa-times"></i></button><i class="ui-panel-icon"></i><span></span></div><div class="ui-panel-header"></div><div class="ui-panel-body"></div></div>'.format(self.ID, config.width, config.closebutton == false ? ' hidden' : '', config.bg ? '' : ' ui-panel-inline'));
+		$(document.body).append('<div id="{0}" class="hidden ui-panel-container{3}"><div class="ui-panel" style="max-width:{1}px"><div data-bind="@config__change .ui-panel-icon:@icon__html span:value.title" class="ui-panel-title"><button class="ui-panel-button-close{2}"><i class="fa fa-times"></i></button><i class="ui-panel-icon"></i><span></span></div><div class="ui-panel-header"></div><div class="ui-panel-body"></div></div>'.format(self.ID, config.width, config.closebutton == false ? ' hidden' : '', config.bg ? '' : ' ui-panel-inline'));
 		var el = $('#' + self.ID);
 		el.find('.ui-panel-body')[0].appendChild(self.dom);
 		self.rclass('hidden');
@@ -99,7 +102,7 @@ COMPONENT('panel', 'width:350;icon:circle-o', function(self, config) {
 		container.scrollTop(0);
 		self.rclass('hidden');
 		self.release(false);
-		self.resize();
+		setTimeout(self.resize, 100);
 
 		config.reload && EXEC(config.reload, self);
 		config.default && DEFAULT(config.default, true);
