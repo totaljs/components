6d461831588f81bed9542370e11768ffa7ddf768
Peter Širka <petersirka@gmail.com>
2018-11-09 10:02:11 +0100

    Improved `j-Panel` configuration.

--- j-Panel/component.js
+++ j-Panel/component.js
@@ -65,6 +65,9 @@ COMPONENT('panel', 'width:350;icon:circle-o;zindex:12', function(self, config) {
 
 	self.configure = function(key, value, init) {
 		switch (key) {
+			case 'bg':
+				self.find('.ui-panel-container').tclass('ui-panel-inline', !value);
+				break;
 			case 'closebutton':
 				!init && self.find('.ui-panel-button-close').tclass(value !== true);
 				break;
