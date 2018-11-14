00ee765254341370fca29c27a1bb4ffb2cf05229
Peter Širka <petersirka@gmail.com>
2018-11-14 20:09:41 +0100

    Improved TabMenu.

--- j-TabMenu/component.js
+++ j-TabMenu/component.js
@@ -9,6 +9,28 @@ COMPONENT('tabmenu', 'class:selected;selector:li', function(self, config) {
 			var el = $(this);
 			!el.hclass(config.class) && self.set(el.attrd('value'));
 		});
+		var scr = self.find('script');
+		if (scr.length) {
+			self.template = Tangular.compile(scr.html());
+			scr.remove();
+		}
+	};
+
+	self.configure = function(key, value) {
+		switch (key) {
+			case 'datasource':
+				self.datasource(value, function(path, value) {
+					if (value instanceof Array) {
+						var builder = [];
+						for (var i = 0; i < value.length; i++)
+							builder.push(self.template(value[i]));
+						old = null;
+						self.html(builder.join(''));
+						self.refresh();
+					}
+				}, true);
+				break;
+		}
 	};
 
 	self.setter = function(value) {
--- j-TabMenu/readme.md
+++ j-TabMenu/readme.md
@@ -2,6 +2,14 @@
 
 - easy usage
 
+__Configuration__:
+
+Example: `data-jc-config="class:selected;selector:a"`
+
+- `class` {String} (optional) is a class name for selected element (default: `selected`)
+- `selector` {String} (optional) is a jQuery selector (default: `li`)
+- __NEW__: `datasource` {String} (optional) path to a data-source, content must be Tangular template
+
 ### Author
 
 - Peter Širka <petersirka@gmail.com>
