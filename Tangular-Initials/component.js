89920b508d40b2d825ef085d2299819b768de73d
Peter Širka <petersirka@gmail.com>
2018-11-24 15:45:42 +0100

    Improved colors.

--- Tangular-Initials/component.js
+++ Tangular-Initials/component.js
@@ -4,5 +4,8 @@ Thelpers.initials = function(value) {
 	var index = value.indexOf('.');
 	var arr = value.substring(index + 1).replace(/\s{2,}/g, ' ').trim().split(' ');
 	var initials = (arr[0].substring(0, 1) + (arr[1] || '').substring(0, 1));
-	return '<span class="initials" style="background-color:{1}" title="{2}">{0}</span>'.format(initials, TTIC[value.length % TTIC.length], value);
+	var sum = 0;
+	for (var i = 0; i < value.length; i++)
+		sum += value.charCodeAt(i);
+	return '<span class="initials" style="background-color:{1}" title="{2}">{0}</span>'.format(initials, TTIC[sum % value.length], value);
 };
