c16a1e2cf2ceb36fd97516ab1b826f8790a4c82c
Peter Širka <petersirka@gmail.com>
2018-12-23 21:28:31 +0100

    Improved CSS.

--- j-ViewBox/component.js
+++ j-ViewBox/component.js
@@ -5,32 +5,43 @@ COMPONENT('viewbox', 'margin:0;scroll:true', function(self, config) {
 	self.readonly();
 
 	self.init = function() {
-		OP.on('resize', function() {
+		$(window).on('resize', function() {
 			SETTER('viewbox', 'resize');
 		});
 	};
 
-	self.configure = function(key, value) {
+	self.configure = function(key, value, init) {
 		switch (key) {
 			case 'disabled':
 				eld.tclass('hidden', !value);
 				break;
-			case 'scroll':
-				self.tclass('ui-viewbox-scroll', !!value);
+			case 'minheight':
+				!init && self.resize();
 				break;
 		}
 	};
 
 	self.make = function() {
+		config.scroll && MAIN.version > 17 && self.element.wrapInner('<div class="ui-viewbox-body"></div>');
 		self.element.prepend('<div class="ui-viewbox-disabled hidden"></div>');
 		eld = self.find('> .ui-viewbox-disabled').eq(0);
 		self.aclass('ui-viewbox ui-viewbox-hidden');
+		if (config.scroll) {
+			if (MAIN.version > 17)
+				window.SCROLLBAR(self.find('.ui-viewbox-body'), { parent: self.element });
+			else
+				self.aclass('ui-viewbox-scroll');
+		}
 		self.resize();
 	};
 
 	self.resize = function() {
 		var el = config.selector ? self.element.closest(config.selector) : self.parent();
 		var h = ((el.height() / 100) * config.height) - config.margin;
+
+		if (config.minheight && h < config.minheight)
+			h = config.minheight;
+
 		eld.css({ height: h, width: self.element.width() });
 		self.css('height', h);
 		self.element.SETTER('*', 'resize');
