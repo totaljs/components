272496fb3219373c0ab60cb63cfa7578471800ac
Peter Širka <petersirka@gmail.com>
2019-02-10 21:14:21 +0100

    Fixed `dirkey` and `dirsource`.

--- j-Input/component.js
+++ j-Input/component.js
@@ -1,4 +1,4 @@
-COMPONENT('input', 'maxlength:200;key:name;value:id;increment:1;after:\\:', function(self, config) {
+COMPONENT('input', 'maxlength:200;dirkey:name;dirvalue:id;increment:1;after:\\:', function(self, config) {
 
 	var cls = 'ui-input';
 	var cls2 = '.' + cls;
