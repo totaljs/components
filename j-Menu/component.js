1370268ab6f809baf97546789fac3a7f0a46b450
Peter Širka <petersirka@gmail.com>
2019-02-12 11:15:24 +0100

    Fixed code.

--- j-Menu/component.js
+++ j-Menu/component.js
@@ -4,18 +4,19 @@ COMPONENT('menu', function(self) {
 	self.readonly();
 	self.nocompile && self.nocompile();
 
-	var ul;
+	var cls = 'ui-menu';
 	var is = false;
 	var events = {};
+	var ul;
 
 	self.make = function() {
-		self.aclass('ui-menu hidden');
+		self.aclass(cls + ' hidden');
 		self.append('<ul></ul>');
 		ul = self.find('ul');
 
 		self.event('touchstart mousedown', 'li', function(e) {
 			var el = $(this);
-			if (el.hclass('ui-menu-divider')) {
+			if (el.hclass(cls + '-divider')) {
 				e.preventDefault();
 				e.stopPropagation();
 			} else {
@@ -33,7 +34,7 @@ COMPONENT('menu', function(self) {
 		self.on('scroll', events.hide);
 		self.on('resize', events.hide);
 
-		events.click = function() {
+		events.click = function(e) {
 			if (is && (!self.target || (self.target !== e.target && !self.target.contains(e.target))))
 				self.hide();
 		};
@@ -86,7 +87,7 @@ COMPONENT('menu', function(self) {
 
 		for (var i = 0; i < opt.items.length; i++) {
 			var item = opt.items[i];
-			builder.push(typeof(item) == 'string' ? '<li class="ui-menu-divider">{0}</li>'.format(item === '-' ? '<hr />' : ('<span>' + item + '</span>')) : '<li{2}>{0}{1}</li>'.format(item.icon ? '<i class="fa fa-{0}"></i>'.format(item.icon) : '', item.name, item.icon ? '' : ' class="ui-menu-nofa"'));
+			builder.push(typeof(item) == 'string' ? '<li class="{1}-divider">{0}</li>'.format(item === '-' ? '<hr />' : ('<span>' + item + '</span>'), cls) : '<li{2}>{0}{1}</li>'.format(item.icon ? '<i class="fa fa-{0}"></i>'.format(item.icon) : '', item.name, item.icon ? '' : (' class="' + cls + '-nofa"')));
 		}
 
 		var css = {};
@@ -98,7 +99,7 @@ COMPONENT('menu', function(self) {
 
 		if (!is) {
 			self.rclass('hidden');
-			self.aclass('ui-menu-visible', 100);
+			self.aclass(cls + '-visible', 100);
 			is = true;
 			if (!events.is)
 				self.bindevents();
@@ -110,14 +111,14 @@ COMPONENT('menu', function(self) {
 
 		if (opt.element) {
 			switch (opt.align) {
-				case 'left':
-					css.left = offset.left;
-					break;
 				case 'center':
 					css.left = Math.ceil((offset.left - w / 2) + (target.innerWidth() / 2));
 					break;
 				case 'right':
 					css.left = (offset.left - w) + target.innerWidth();
+					break;
+				default:
+					css.left = offset.left;
 					break;
 			}
 		} else {
@@ -139,7 +140,7 @@ COMPONENT('menu', function(self) {
 		self.target = null;
 		self.opt = null;
 		self.aclass('hidden');
-		self.rclass('ui-menu-visible');
+		self.rclass(cls + '-visible');
 	};
 
 });
