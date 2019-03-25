45a7bdb0e6098713e9fd47e33ac56aa63efa04e3
Denis Granec <denis@granec.cz>
2019-03-25 13:20:50 +0100

    Update j-ColorPicker

--- j-ColorPicker/component.js
+++ j-ColorPicker/component.js
@@ -1,76 +1,121 @@
-COMPONENT('colorpicker', function(self) {
+COMPONENT('colorpicker', function(self, config) {
 
-	var container = null;
-	var template = '<li data-index="{0}"><i class="fa fa-circle" style="color:#{1}"></i></li>';
+	var cls = 'ui-colorpicker';
+	var cls2 = '.' + cls;
 	var is = false;
-	var opener = {};
-	var css = {};
-	var colors = 'ff0000,ff4000,ff8000,ffbf00,ffff00,bfff00,80ff00,40ff00,00ff00,00ff40,00ff80,00ffbf,00ffff,00bfff,0080ff,0040ff,0000ff,4000ff,8000ff,bf00ff,ff00ff,ff00bf,ff0080,ff0040,ff0000,F0F0F0,D0D0D0,AAB2BD,505050,000000'.split(',');
+	var events = {};
+	var colors = [['e73323', 'ec8632', 'fffd54', '7bfa4c', '7cfbfd', '041ef5', 'e73cf7', '73197b', '91683c', 'ffffff', '808080', '000000'],['ffffff', 'e8e8e8', 'd1d1d1', 'b9b9b9', 'a2a2a2', '8b8b8b', '747474', '5d5d5d', '464646', '2e2e2e', '171717', '000000'],['5c0e07', '5e350f', '66651c', '41641a', '2d6419', '2d6438', '2d6465', '133363', '000662', '2d0962', '5c1262', '5c0f32', '8a1a11', '8e501b', '99982f', '62962b', '47962a', '479654', '479798', '214d94', '010e93', '451393', '8a2094', '8a1c4c', 'b9261a', 'bd6b27', 'cccb41', '83c83c', '61c83b', '61c871', '62c9ca', '2e67c5', '0216c4', '5c1dc4', 'b92ec5', 'b92865', 'e73323', 'ec8632', 'fffd54', 'a4fb4e', '7bfa4c', '7bfa8d', '7cfbfd', '3b80f7', '041ef5', '7327f5', 'e73cf7', 'e7357f', 'e8483f', 'ef9d4b', 'fffe61', 'b4fb5c', '83fa5a', '83faa2', '83fbfd', '5599f8', '343cf5', '8c42f6', 'e84ff7', 'e84a97', 'ea706b', 'f2b573', 'fffe7e', 'c5fc7c', '96fa7a', '96fbb9', '96fcfd', '7bb2f9', '666af6', 'a76ef7', 'eb73f8', 'ea71b0', 'f6cecd', 'fae6cf', 'fffed1', 'ebfed1', 'd7fdd0', 'd7fde7', 'd8fefe', 'd1e5fd', 'cccdfb', 'e1cefb', 'f6cffc', 'f6cee4']];
 
 	self.singleton();
 	self.readonly();
 	self.blind();
-	self.nocompile && self.nocompile();
+	self.nocompile();
 
 	self.make = function() {
-		self.aclass('ui-colorpicker hidden');
-		self.append('<ul></ul>');
-		container = self.find('ul');
 
-		self.event('click', 'li', function() {
-			var color = '#' + colors[+this.getAttribute('data-index')];
-			opener.callback && opener.callback(color, opener.target);
+		var html = '';
+		for (var i = 0; i < colors.length; i++) {
+			for (var j = 0; j < colors[i].length; j++) {
+				html += '<span style="background-color: #{0}"></span>'.format(colors[i][j]);
+			}
+			if (i !== colors.length - 1)
+				html += '<br/><br/>';
+		}
+
+		self.html('<div class="{0}"><div class="{0}-body">{1}</div></div>'.format(cls, html));
+		self.aclass(cls + '-container hidden');
+
+		self.event('click', cls2 + '-body span', function() {
+			self.opt.callback($(this).attr('style').replace('background-color:', ''));
 			self.hide();
-			EMIT('colorpicker', color);
 		});
 
-		self.on('reflow', self.$hide);
-		self.on('resize', self.$hide);
+		events.click = function(e) {
+			var el = e.target;
+			var parent = self.dom;
+			do {
+				if (el == parent)
+					return;
+				el = el.parentNode;
+			} while (el);
+			self.hide();
+		};
 	};
 
-	self.$hide = function() {
-		is && self.hide();
-	};
-
-	self.render = function() {
-		var builder = [];
-		for (var i = 0, length = colors.length; i < length; i++)
-			builder.push(template.format(i, colors[i]));
-		container.empty().html(builder.join(''));
-	};
-
-	self.hide = function() {
-		if (is) {
-			self.aclass('hidden');
-			is = false;
+	self.bindevents = function() {
+		if (!events.is) {
+			events.is = true;
+			$(document).on('click', events.click);
 		}
 	};
 
-	self.show = function(x, y, target, callback) {
+	self.unbindevents = function() {
+		if (events.is) {
+			events.is = false;
+			$(document).off('click', events.click);
+		}
+	};
 
-		if (is && opener.x === x && opener.y === y) {
-			opener.x = null;
-			opener.y = null;
+	self.show = function(opt) {
+
+		var tmp = opt.element ? opt.element instanceof jQuery ? opt.element[0] : opt.element.element ? opt.element.dom : opt.element : null;
+
+		if (is && tmp && self.target === tmp) {
 			self.hide();
 			return;
 		}
 
-		if (typeof(target) === 'function') {
-			callback = target;
-			target = null;
+		self.target = tmp;
+		self.opt = opt;
+		var css = {};
+
+		if (is) {
+			css.left = 0;
+			css.top = 0;
+			self.element.css(css);
+		} else
+			self.rclass('hidden');
+
+		var target = $(opt.element);
+		var w = self.element.width();
+		var offset = target.offset();
+
+		if (opt.element) {
+			switch (opt.align) {
+				case 'center':
+					css.left = Math.ceil((offset.left - w / 2) + (target.innerWidth() / 2));
+					break;
+				case 'right':
+					css.left = (offset.left - w) + target.innerWidth();
+					break;
+				default:
+					css.left = offset.left;
+					break;
+			}
+
+			css.top = opt.position === 'bottom' ? (offset.top - self.element.height() - 10) : (offset.top + target.innerHeight() + 10);
+
+		} else {
+			css.left = opt.x;
+			css.top = opt.y;
 		}
 
-		opener.callback = callback;
-		opener.target = target;
-		opener.x = x;
-		opener.y = y;
+		if (opt.offsetX)
+			css.left += opt.offsetX;
 
-		!is && self.render();
+		if (opt.offsetY)
+			css.top += opt.offsetY;
+
 		is = true;
+		self.element.css(css);
+		setTimeout(self.bindevents, 50);
+	};
 
-		css.left = x;
-		css.top = y;
-
-		self.element.css(css).rclass('hidden');
+	self.hide = function() {
+		is = false;
+		self.target = null;
+		self.opt = null;
+		self.unbindevents();
+		self.aclass('hidden');
 	};
 });
--- j-ColorPicker/readme.md
+++ j-ColorPicker/readme.md
@@ -1,12 +1,32 @@
 ## j-ColorPicker
 
-A simple `ColorPicker` component. This component is __singleton__.
+- Component is __singleton__
+- Simple color picker
 
-__Methods__:
-- `component.show(x, y, [target], [callback(color, target)]` and it works as `toggle` when `x` and `y` are same as previous call
-- `component.hide()`
+## Usage
+
+```javascript
+var opt = {};
+
+// opt.offsetX {Number}    : adds X offset (default: 0)
+// opt.offsetY {Number}    : adds Y offset (default: 0)
+// opt.align {String}      : align `left` (default), `center` or `right`
+// opt.position {String}   : position `top` (default) or `bottom`
+
+opt.element = YOUR_ELEMENT;
+// or if you want to use a fixed position:
+// opt.x {Number}          : `x` position
+// opt.y {Number}          : `y` position
+
+opt.callback = function(color) {
+	console.log(color); // callback parameter has string with HEX color (like #bada55)
+};
+
+SETTER('colorpicker', 'show', opt);
+```
+
 
 ### Author
 
-- Peter Širka <petersirka@gmail.com>
+- Denis Granec <denis@granec.cz>
 - License: MIT
