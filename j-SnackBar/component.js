dc91f8b61b777707b384a56d90dd368d35ca7d79
Peter Širka <petersirka@gmail.com>
2019-05-25 00:11:08 +0200

    Improved code.

--- j-SnackBar/component.js
+++ j-SnackBar/component.js
@@ -1,5 +1,7 @@
 COMPONENT('snackbar', 'timeout:4000;button:OK', function(self, config) {
 
+	var cls = 'ui-snackbar';
+	var cls2 = '.' + cls;
 	var show = true;
 	var callback;
 	var delay;
@@ -9,9 +11,9 @@ COMPONENT('snackbar', 'timeout:4000;button:OK', function(self, config) {
 	self.nocompile && self.nocompile();
 
 	self.make = function() {
-		self.aclass('ui-snackbar hidden');
-		self.append('<div><span class="ui-snackbar-dismiss"></span><span class="ui-snackbar-icon"></span><div class="ui-snackbar-body"></div></div>');
-		self.event('click', '.ui-snackbar-dismiss', function() {
+		self.aclass(cls + ' hidden');
+		self.append('<div><span class="{0}-dismiss"></span><span class="{0}-icon"></span><div class="{0}-body"></div></div>'.format(cls));
+		self.event('click', cls2 + '-dismiss', function() {
 			self.hide();
 			callback && callback();
 		});
@@ -19,7 +21,7 @@ COMPONENT('snackbar', 'timeout:4000;button:OK', function(self, config) {
 
 	self.hide = function() {
 		clearTimeout2(self.ID);
-		self.rclass('ui-snackbar-visible');
+		self.rclass(cls + '-visible');
 		if (delay) {
 			clearTimeout(delay);
 			self.aclass('hidden');
@@ -54,14 +56,14 @@ COMPONENT('snackbar', 'timeout:4000;button:OK', function(self, config) {
 
 		callback = close;
 
-		self.find('.ui-snackbar-icon').html('<i class="fa {0}"></i>'.format(icon || 'fa-info-circle'));
-		self.find('.ui-snackbar-body').html(message).attr('title', message);
-		self.find('.ui-snackbar-dismiss').html(button || config.button);
+		self.find(cls2 + '-icon').html('<i class="fa {0}"></i>'.format(icon || 'fa-info-circle'));
+		self.find(cls2 + '-body').html(message).attr('title', message);
+		self.find(cls2 + '-dismiss').html(button || config.button);
 
 		if (show) {
 			self.rclass('hidden');
 			setTimeout(function() {
-				self.aclass('ui-snackbar-visible');
+				self.aclass(cls + '-visible');
 			}, 50);
 		}
 
