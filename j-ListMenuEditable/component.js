e4da1e8c5caab61db4664a27ff6168218efbe7df
Denis Granec <denis@granec.cz>
2019-06-13 12:38:36 +0200

    Updated j-ListMenuEditable

--- j-ListMenuEditable/component.js
+++ j-ListMenuEditable/component.js
@@ -1,24 +1,42 @@
-COMPONENT('listmenueditable', 'iconremove:times;defaulticon:pencil-alt;addicon:plus-square;placeholder:Write text and press ENTER', function(self, config) {
+COMPONENT('listmenueditable', 'iconremove:times;defaulticon:pencil-alt;addicon:plus-square;placeholder:Write text and press ENTER;class:selected', function(self, config) {
 
 	var cls = 'ui-listmenueditable';
 	var cls2 = '.' + cls;
+	var selected;
 	var container;
-	var deftemplate = '';
-	var template = '<div class="' + cls + '-item {{ if selected }}selected{{ fi }} {{ classname }}" data-index="{{ index }}" {{ if value }} data-value="{{ value }}" {{ fi }}>{{ if icon }}<span class="' + cls + '-icon"><i class="fa fa-{{ icon }}"></i></span>{{ fi }}<div class="' + cls + '-text"><span>{{ name | encode | ui_listmenueditable_helper }}</span></div></div>';
+	var last;
+	var template = '<div class="' + cls + '-item {{ if selected }}selected{{ fi }} {{ classname }}" data-index="{{ index }}" {{ if value }} data-value="{{ value }}" {{ fi }}>{{ if editable }}<span class="' + cls + '-icon"><i class="fa fa-{{ icon }}"></i></span>{{ fi }}<div class="' + cls + '-text"><span>{{ name | encode | ui_listmenueditable_helper }}</span></div></div>';
 	var templateinput = '<div class="' + cls + '-item" {{ if index != null }}data-index="{{ index }}"{{ fi }}><span class="' + cls + '-deleteicon"><i class="fa fa-' + config.iconremove + '"></i></span><div class="' + cls + '-text"><input placeholder="' + config.placeholder + '" {{ if index != null }}value="{{ text }}"{{ fi }} type="text" /></div></div>';
+	var layout;
 
 	Thelpers.ui_listmenueditable_helper = function(val) {
-		var t = this;
-		return t.template ? t.template.indexOf('{{') === -1 ? t.template : Tangular.render(t.template, this) : val;
+		return layout ? layout.indexOf('{{') === -1 ? layout : Tangular.render(layout, this) : val;
 	};
 
 	self.template = Tangular.compile(template);
 	self.templateinput = Tangular.compile(templateinput);
 
 	self.make = function() {
+
+		var scr = self.find('> script');
+		layout = scr.length ? scr.html() : null;
+
 		self.aclass(cls);
-		config.title && self.html('<div class="' + cls + '-header"><span class="' + cls + '-icon"><i class="fas fa-' + config.addicon + '"></i></span><div class="' + cls + '-text"><span>Title</span></div></div>');
+		config.title && self.html('<div class="{0}-header"><span class="{0}-icon"><i class="fas fa-{1}"></i></span><div class="{0}-text"><span>{2}</span></div></div>'.format(cls, config.addicon, config.title));
 		self.append('<div class="' + cls + '-container"></div>');
+
+		self.event('click', cls2 + '-item' + ' ' + cls2 + '-text', function() {
+			if ($(this).find('input').length)
+				return;
+
+			var parent = $(this).closest(cls2 + '-item');
+			var index = parent.attrd('index');
+			var items = self.get();
+			container.find('.' + config.class).rclass(config.class);
+			parent.aclass(config.class);
+			selected = index;
+			config.click && EXEC(config.click, parent, items[index], index);
+		});
 
 		self.event('click', cls2 + '-header ' + cls2 + '-icon', function() {
 			config.addclick && EXEC(config.addclick);
@@ -40,8 +58,6 @@ COMPONENT('listmenueditable', 'iconremove:times;defaulticon:pencil-alt;addicon:p
 					obj[key] = value;
 					if (config.defaulticon.length)
 						obj.icon = config.defaulticon;
-					if (deftemplate.length)
-						obj.template = deftemplate;
 					cur.push(obj);
 				} else {
 					cur[index][key] = value;
@@ -58,10 +74,11 @@ COMPONENT('listmenueditable', 'iconremove:times;defaulticon:pencil-alt;addicon:p
 		self.event('focusout', 'input', function() {
 			var parent = $(this).closest(cls2 + '-item');
 			var cur = self.get();
-			var index = parent.attrd('index');
-			if (index == null)
-				parent.remove();
-			else {
+			var index = parseInt(parent.attrd('index'));
+			if (index === last) {
+				cur.splice(index, 1);
+				self.set(cur);
+			} else {
 				var itemtemplate = self.itemtemplate();
 				parent.replaceWith(itemtemplate(cur[index]));
 			}
@@ -71,22 +88,26 @@ COMPONENT('listmenueditable', 'iconremove:times;defaulticon:pencil-alt;addicon:p
 	self.edit = function(index) {
 		if (index == null)
 			return;
+
 		var el = container.find(cls2 + '-item[data-index="' + index + '"]');
 		var text = self.get()[index];
 		var key = config.key || 'name';
-		el.replaceWith(self.templateinput({ text: text[key], index: index }));
+		el.replaceWith(self.templateinput({ text: text[key] || '', index: index }));
 
 		setTimeout2(self.id + 'focus', function() {
 			container.find('input').focus();
+			var input = container.find('input');
+			var value = input.val();
+			input.val('').val(value);
 		}, 80);
 	};
 
-	self.add = function(templa) {
-		deftemplate = templa || '';
-		container.append(self.templateinput(EMPTYOBJECT));
-		setTimeout2(self.id + 'focus', function() {
-			container.find('input').focus();
-		}, 80);
+	self.add = function(data) {
+		if (data == null)
+			data = {};
+		self.push(data);
+		var index = last = self.get().length - 1;
+		self.edit(index);
 	};
 
 	self.itemtemplate = function() {
@@ -101,9 +122,12 @@ COMPONENT('listmenueditable', 'iconremove:times;defaulticon:pencil-alt;addicon:p
 		var builder = [];
 		var itemtemplate = self.itemtemplate();
 
-		for (var i = 0, length = value.length; i < length; i++) {
+		for (var i = 0; i < value.length; i++) {
 			var item = value[i];
 			item.index = i;
+			item.icon = config.defaulticon;
+			if (selected != null && selected == i)
+				item.selected = true;
 			builder.push(itemtemplate(item));
 		}
 
--- j-ListMenuEditable/readme.md
+++ j-ListMenuEditable/readme.md
@@ -6,30 +6,35 @@ Component for editable list menu
 
 __Configuration__:
 
-- `iconremove` {String} optional, change remove icon. It's fontawesome icon without `fa-` part (default: `times`)
-- `addicon` {String} optional, will change `add` icon (default: `plus-square`)
-- `defaulticon` {String} optional, change "edit" icon. You can disable this icon with value `0`. ONLY FOR NEW ITEMS (default: `pencil-alt`)
+- `iconremove` {String} optional, change `remove` button icon. It's fontawesome icon without `fa-` part (default: `times`)
+- `defaulticon` {String} optional, change `edit` button icon (default: `pencil-alt`)
+- `addicon` {String} optional, will change `add` button icon (default: `plus-square`)
 - `placeholder` {String} optional, a placeholder for input (default: `Write text and press ENTER`)
-- `title` {String} optional, will add title on top of list menu
+- `class` {String} CSS class for selected item (default: `selected`)
+- `title` {String} optional, will add title on top of list menu (important if you want use `addicon` and `addclick`)
 - `key` {String} optional, a default `key` for `text` value (default: `name`)
-- `addclick` {String} optional, a path to function() executes when user click on `add` icon
-- `editclick` {String} optional, a path to function(element, index)
+- `click` {String} optional, action after click on item in menu. Path to function(element, index)
+- `addclick` {String} optional, action after click on `add `button. Path to function() executes when user click on `add` icon
+- `editclick` {String} optional, action after click on `edit` button. Path to function(element, index)
 
 __Methods__:
 
-- `component.add()`
-- `component.edit(element, index)`
+- `component.add(object)` - This function will push object into path (can be null)
+- `component.edit(index)` - Function will enable edit on element
 
 __Good to know__:
 
-Path __must be__ array of objects.
-
-__Formatting via Tangular__:
-
-```javascript
-var opt = {};
-opt.items = [];
-opt.push({ name: 'Total.js', template: '<b>{{ name }}</b>' });
+Path __must be__ array of objects. After click on item in menu
+Supports dynamic evaluation of the content of `<script type="text/html">`. The example below contains a script with HTML and the component evaluates the content when the component is creating (only once).
+```
+<div data---="listmenueditable__tags">
+	<script type="text/html">
+		{{ if mycolor }}
+			<i class="fas fa-circle" style="color: {{ mycolor }}"></i>
+		{{ fi }}
+		{{ name }}
+	</script>
+</div>
 ```
 
 __Adding of custom class__:
@@ -40,20 +45,12 @@ opt.items = [];
 opt.push({ name: 'Total.js', classname: 'your_class_name' });
 ```
 
-__Setting selected__:
+__Toggle editable__:
 
 ```javascript
 var opt = {};
 opt.items = [];
-opt.push({ name: 'Total.js', selected: true });
-```
-
-__Adding edit icon__:
-
-```javascript
-var opt = {};
-opt.items = [];
-opt.push({ name: 'Total.js', icon: 'pencil-alt' });
+opt.push({ name: 'Total.js', editable: true });
 ```
 
 ### Author
