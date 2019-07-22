3ca74d5d16247c6dbfadd5aba5982df48f2aa313
Peter Širka <petersirka@gmail.com>
2019-07-22 14:00:29 +0200

    Fixed resizing.

--- j-ImageViewer/component.js
+++ j-ImageViewer/component.js
@@ -126,7 +126,7 @@ COMPONENT('imageviewer', 'selector:.img-viewer;container:body', function(self, c
 			var ratio;
 
 			var mw = WW - 10;
-			var mh = WH - (isMOBILE ? 45 : 100);
+			var mh = WH - 55;
 
 			if (img.width > img.height)
 				ratio = mw / (img.width / 100);
