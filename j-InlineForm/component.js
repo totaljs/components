5bb6205f55167279311cf69d49613467ba08f8ac
Peter Širka <petersirka@gmail.com>
2019-09-03 09:27:59 +0200

    Fixed code.

--- j-InlineForm/component.js
+++ j-InlineForm/component.js
@@ -50,11 +50,11 @@ COMPONENT('inlineform', 'icon:circle-o', function(self, config) {
 		$(document.body).append('<div id="{0}" class="hidden {3}-container" style="max-width:{1}"><div class="{3}"><div class="{3}-title" data-bind="@config__html span:value.title__change .ui-inlineform-icon:@icon"><button name="cancel" class="{3}-close"><i class="fa fa-times"></i></button><i class="{3}-icon"></i><span></span></div></div></div>'.format(self.ID, (config.width || dw) + 'px', self.path, cls));
 
 		var el = $('#' + self.ID);
-
 		var scr = self.find('> script');
 		self.template = scr.length ? scr.html() : '';
 
 		el.find(cls2)[0].appendChild(self.dom);
+		self.aclass(cls + '-body');
 		self.rclass('hidden');
 		self.replace(el);
 		self.event('click', 'button[name]', function() {
@@ -100,7 +100,7 @@ COMPONENT('inlineform', 'icon:circle-o', function(self, config) {
 
 		if (self.template) {
 			is = self.template.COMPILABLE();
-			self.find('div[data-jc-replaced]').aclass(cls + '-body').html(self.template);
+			self.find('div[data-jc-replaced]').html(self.template);
 			self.template = null;
 			is && COMPILE();
 			setTimeout(self.show, 250, opt);
