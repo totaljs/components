04b62f33b2e6531d8b8b346abeeffab7fd307cce
Peter Širka <petersirka@gmail.com>
2019-09-11 10:17:44 +0200

    Added new component: `j-FaIconsButton`

---
+++ j-FaIconsButton/component.js
@@ -0,0 +1,41 @@
+COMPONENT('faiconsbutton', 'default:#FFFFFF;align:left;position:top', function(self, config) {
+
+	var cls = 'ui-faiconsbutton';
+	var icon;
+
+	self.nocompile();
+
+	self.make = function() {
+		self.aclass(cls);
+		self.append('<span class="{0}-arrow"><i class="fa fa-angle-down"></i></span><div class="{0}-icon"></div>'.format(cls));
+		icon = self.find('.' + cls + '-icon');
+
+		self.event('click', function() {
+			if (config.disabled)
+				return;
+			var opt = {};
+			opt.align = config.align;
+			opt.position = config.position;
+			opt.offsetX = config.offsetX;
+			opt.offsetY = config.offsetY;
+			opt.element = self.element;
+			opt.callback = function(icon) {
+				self.set(icon);
+				self.change(true);
+			};
+			SETTER('faicons', 'show', opt);
+		});
+	};
+
+	self.configure = function(key, value) {
+		switch (key) {
+			case 'disabled':
+				self.tclass('ui-disabled', !!value);
+				break;
+		}
+	};
+
+	self.setter = function(value) {
+		icon.html(value ? '<i class="{0}"></i>'.format(value) : '');
+	};
+});
