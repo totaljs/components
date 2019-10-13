7a6c931f5839f0d4683127e0736acf99d61e0de5
Denis Granec <denis@granec.cz>
2019-10-13 23:21:50 +0200

    Added j-Tour

---
+++ j-Tour/component.js
@@ -0,0 +1,150 @@
+COMPONENT('tour', 'skiptext:Skip;nexttext:Next;backtext:Back;endtext:End;escape:1', function(self, config) {
+
+	var cls = 'ui-tour';
+	var cls2 = '.' + cls;
+	var data, lastindex;
+	var $background, $container, $info, $last;
+
+	self.singleton();
+	self.nocompile();
+
+	self.configure = function(name, value, init) {
+		switch(name) {
+			case 'datasource':
+				self.datasource(value, self.bind);
+				break;
+		}
+	};
+
+	self.bind = function(path, val) {
+		data = val;
+	};
+
+	self.make = function() {
+		var builder = [];
+		builder.push('<div class="' + cls + '-background"></div>');
+		builder.push('<div class="' + cls + '"></div>');
+		builder.push('<div class="' + cls + '-info"></div>');
+
+		var html = builder.join('');
+
+		self.aclass(cls + '-container');
+		self.html(html);
+		$background = self.find(cls2 + '-background');
+		$container = self.find(cls2);
+		$info = self.find(cls2 + '-info');
+
+		self.event('click', '.close', function() {
+			self.hideintro(2);
+		});
+
+		self.event('click', '.next', function() {
+			lastindex += 1;
+			if (lastindex === data.length) {
+				self.hideintro(1);
+				return;
+			}
+			var item = data[lastindex];
+			self.setintro(item);
+		});
+
+		self.event('click', '.back', function() {
+			lastindex -= 1;
+			var item = data[lastindex];
+			self.setintro(item);
+		});
+
+		self.event('click', cls2 + '-background', function() {
+			config.bghide && self.hideintro(4);
+		});
+
+	};
+
+	self.keydown = function(e) {
+		switch (e.which) {
+			case 27:
+				config.escape && self.hideintro(3);
+				break;
+			case 9:
+			case 39:
+				self.find('.next').click();
+				break;
+			case 37:
+				self.find('.back').click();
+				break;
+		}
+
+	};
+
+	self.setter = function(value, path, type) {
+
+		if (type === 0)
+			return;
+
+		var index = +value;
+		if (value === '' || data == null || data[index] == null) {
+			self.hideintro(0);
+			return;
+		}
+
+		var item = data[index];
+		lastindex = index;
+		self.setintro(item);
+		$(W).on('keydown', self.keydown);
+	};
+
+	self.hideintro = function(type) {
+		$last && $last.rclass(cls + '-element');
+		self.rclass(cls + '-visible');
+		config.hide && EXEC(self.makepath(config.hide), type);
+		$(W).off('keydown', self.keydown);
+	};
+
+	self.setintro = function(item) {
+
+		var builder = [];
+		var css = {};
+
+		var el = $(item.target).eq(0);
+		var position = el.offset();
+
+		css.top = position.top;
+		css.left = position.left;
+		css.width = el.outerWidth();
+		css.height = el.outerHeight();
+		$container.css(css);
+
+		$last && $last.rclass(cls + '-element');
+		$last = el;
+		el.aclass(cls + '-element');
+
+		var counter = '{0}&sol;{1}'.format(lastindex + 1, data.length);
+		var islast = ((data.length - 1) === lastindex);
+		item.title && builder.push('<div class="' + cls + '-info-header"><span>' + counter + '</span><div>' + item.title + '</div></div>');
+		builder.push('<div class="' + cls + '-info-body">' + item.text + '</div>');
+		builder.push('<div class="{0}-info-footer"><button class="close">{1}</button><button class="back"{2}><i class="fas fa-angle-left"></i>{3}</button><button class="next">{4}<i class="fas fa-angle-right {5}"></i></button></div>'.format(cls, config.skiptext, (lastindex === 0 ? ' disabled' : ''), config.backtext, islast ? config.endtext : config.nexttext, islast ? 'hidden' : ''));
+
+		$info.html(builder.join(''));
+
+		var height = $info.innerHeight() + 20;
+		$(W).scrollTop(position.top - height - 100);
+
+		var current = position.top - $(W).scrollTop();
+		var isbottom = (current < height);
+		var viewoffset = isbottom ? ((css.height * -1) - 20) : height;
+
+		$info.rclass(cls + '-info-bottom');
+
+		if (isbottom)
+			$info.aclass(cls + '-info-bottom');
+
+		var offset = item.offsetY || 0;
+		$info.css({
+			top: (position.top - viewoffset) - offset,
+			left: position.left
+		});
+
+		self.aclass(cls + '-visible');
+	};
+
+});
---
+++ j-Tour/readme.md
@@ -0,0 +1,42 @@
+## j-Tour (BETA)
+
+- Supports keyboard arrows
+- Supports `tab` key
+- Supports `esc` key
+
+__IMPORTANT: Put this component on bottom of body element__
+
+__Configuration__:
+
+- `escape` {Boolean} toggle possibility to hide Tour with `esc` key (default: `true`)
+- `skiptext` {String} a text of `skip` button (default: `Skip`)
+- `nexttext` {String} a text of `next` button (default: `Next`)
+- `backtext` {String} a text of `back` button (default: `Back`)
+- `endtext` {String} a text of `end` button (default: `End`)
+- `datasource` {String} path to data-source
+- `bghide` {Number} toggle possibility to hide Tour by clicking on background (default: `false`)
+- `hide` {String} path to method `function(type)` (type - 0: by PATH (e.g. null) 1: end button 2: skip button 3: `esc` key 4: click on bg)
+
+```javascript
+// datasource
+var example = [
+	{
+		target: '.test1',
+		title: 'My title 1',
+		text: 'Message 1'
+	},
+	{
+		target: '.test2',
+		title: 'My title 2',
+		text: 'Message with offset',
+		offsetY: 10
+	}
+];
+```
+
+__PATH is actual tour `page`__
+
+### Author
+
+- Denis Granec <info@totalavengers.com>
+- [License](https://www.totaljs.com/license/)
