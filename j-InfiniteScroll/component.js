b75b63f8d630ae65049b27d519d8c6fd29469e11
Peter Širka <petersirka@gmail.com>
2019-11-29 12:12:13 +0100

    Improved code in `j-InfiniteScroll`.

--- j-InfiniteScroll/component.js
+++ j-InfiniteScroll/component.js
@@ -6,21 +6,24 @@ COMPONENT('infinitescroll', 'margin:0;padding:50;autoscroll:100', function(self,
 	var isloading = true;
 	var css = {};
 	var container;
+	var body;
 
 	self.readonly();
 	self.blind();
 
 	self.add = function(content) {
-		content && self.scrollbar.area.append(content);
+		content && body.append(content);
 
 		if (typeof(content) === 'string' && content.COMPILABLE())
 			COMPILE();
 
 		setTimeout(function() {
 			self.scrollbar.area.css('overflow-y', 'scroll');
-			self.scrollbar.resize();
-			self.scrollbar.area[0].scrollTop += config.autoscroll;
-			isloading = false;
+			setTimeout(function() {
+				self.scrollbar.resize();
+				self.scrollbar.area[0].scrollTop += config.autoscroll;
+				isloading = false;
+			}, 100);
 		}, 200);
 	};
 
@@ -29,17 +32,18 @@ COMPONENT('infinitescroll', 'margin:0;padding:50;autoscroll:100', function(self,
 		self.element.wrapInner('<div class="{0}-container"></div>'.format(cls));
 		container = self.find(cls2 + '-container');
 		self.scrollbar = new SCROLLBAR(container, { visibleY: true, onscroll: self.onscroll });
+		body = self.scrollbar.area.find('.ui-scrollbar-body');
 		self.resize();
 		setTimeout(function() {
 			isloading = false;
-			config.exec && EXEC(config.exec, self.add, self.scrollbar.area, true);
+			config.exec && EXEC(config.exec, self.add, body, true);
 		}, 200);
 	};
 
 	self.onscroll = function(sb) {
 		if (!isloading) {
-			var y = sb.area[0].scrollTop + sb.size.viewHeight;
-			if (y >= (sb.size.scrollHeight - config.padding)) {
+			var y = sb.area[0].scrollTop + sb.size.viewHeight + config.padding;
+			if (y >= sb.size.scrollHeight) {
 				isloading = true;
 				self.scrollbar.area.css('overflow-y', 'hidden');
 				config.exec && EXEC(config.exec, self.add, self.scrollbar.area, false);
