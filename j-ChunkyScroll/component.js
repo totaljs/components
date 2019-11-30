463c5081e4aa0d8afa76c71b62ed42907968c63e
Peter Sirka <petersirka@gmail.com>
2019-11-30 20:29:59 +0100

    Fixed typo.

--- j-ChunkyScroll/component.js
+++ j-ChunkyScroll/component.js
@@ -1,4 +1,4 @@
-COMPONENT('chunkycroll', 'parent:parent;limit:80;margin:0;rowheight:0;scrolltop:1', function(self, config) {
+COMPONENT('chunkyscroll', 'parent:parent;limit:80;margin:0;rowheight:0;scrolltop:1', function(self, config) {
 
 	var cls = 'ui-' + self.name;
 	var cls2 = '.' + cls;
