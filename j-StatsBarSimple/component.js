8605737cff615c4e599a17d066bbe93acdaf0395
Peter Širka <petersirka@gmail.com>
2020-01-08 13:00:32 +0100

    Improved code.

--- j-StatsBarSimple/component.js
+++ j-StatsBarSimple/component.js
@@ -17,11 +17,13 @@ COMPONENT('statsbarsimple', 'tooltip:1;animate:1', function(self, config, cls) {
 				self.template = ta;
 		});
 
+		if (!templatetooltip)
+			templatetooltip = self.template;
 
 		self.aclass(cls);
 		self.append('<div class="{0}-table"></div>'.format(cls));
 
-		templatetooltip && config.tooltip && self.event('mouseenter touchstart', cls2 + '-bar', function() {
+		config.tooltip && self.event('mouseenter touchstart', cls2 + '-bar', function() {
 			var opt = {};
 			opt.element = $(this);
 			var index = +opt.element.attrd('index');
@@ -58,9 +60,9 @@ COMPONENT('statsbarsimple', 'tooltip:1;animate:1', function(self, config, cls) {
 
 		for (var i = 0; i < value.length; i++) {
 			var item = value[i];
-			var p = ((item.value / sum) * 100).floor(1);
+			var p = !item.value && !sum ? 0 : ((item.value / sum) * 100).floor(1);
 			item.percentage = p;
-			builder.push(('<div style="width:' + (config.animate ? '100%' : '{4}') + ';background-color:{3}" class="{0}-bar" data-index="{4}" data-percentage="{2}"><span>{1}</span></div>').format(cls, self.template(item).trim(), p === 0 ? 1 : p, item.color, i));
+			builder.push(('<div style="width:' + (config.animate ? '100%' : '{4}') + ';background-color:{3}" class="{0}-bar" data-index="{4}" data-percentage="{2}"><span>{1}</span></div>').format(cls, self.template(item).trim(), sum === 0 ? (100 / value.length).floor(2) : p === 0 ? 5 : p, item.color, i));
 		}
 
 		container.html(builder.join(''));
