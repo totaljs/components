2a14e3fa7814216c2acfd68066f40d7c833ef543
Peter Širka <petersirka@gmail.com>
2020-02-20 10:00:27 +0100

    Added new component `j-Submit`.

---
+++ j-Submit/component.js
@@ -0,0 +1,200 @@
+COMPONENT('submit', 'delay:100;flags:visible;loading:1;default:1;messages:1;blocked:2000;newbie:n=>!n.id;selector:button[name="submit"];update_modified:1', function(self, config, cls) {
+
+	// config.enter {String} A selector
+	// config.exec {String} A function
+	// config.fail {String} A function
+	// config.done {String} A function
+	// config.prepare {String} A function
+	// config.sending {String} A function
+
+	// config.default {Boolean} true
+	// config.loading {Boolean} true
+	// config.messages {Boolean} true
+
+	// config.url
+	// config.success {String} A message
+	// config.create {String} e.g. POST  /api/users/
+	// config.create_success {String} A message
+	// config.update {String} e.g. PATCH /api/users/{id}/
+	// config.update_success {String} A message
+	// config.newbie {String} A function
+	// config.update_modified {Boolean}
+
+	var path, old, track, enter, elements = null;
+	var flags = null;
+	var tracked = false;
+	var issending = false;
+	var isdisabled = true;
+	var reset = 0;
+	var messages = {};
+
+	self.readonly();
+
+	self.make = function() {
+		elements = self.find(config.selector);
+		path = self.path.replace(/\.\*$/, '');
+
+		if (config.enter) {
+			enter = self.element.closest(config.enter);
+			enter.on('keydown', self.onkeydown);
+		}
+
+		self.event('click', 'button[name]', function() {
+			switch (this.name) {
+				case 'submit':
+					self.submit();
+					break;
+				case 'reset':
+					self.reset();
+					break;
+			}
+		});
+	};
+
+	self.reset = function() {
+		if (!issending)
+			config.default && DEFAULT(self.path);
+	};
+
+	self.submit = function() {
+
+		if (isdisabled || issending || BLOCKED(self.ID, config.blocked))
+			return;
+
+		var modified = MODIFIED(self.path);
+		var data = CLONE(GETR(self.path));
+		var isnewbie = config.newbie && FN(config.newbie)(data);
+		var url = (isnewbie ? config.create : config.update) || config.url;
+
+		if (!url)
+			return;
+
+		if (url.indexOf('{{') !== -1)
+			url = Tangular.render(url, data);
+		else if (url.indexOf('{') !== -1)
+			url = url.arg(data);
+
+		var model = data;
+
+		config.prepare && SEEX(self.makepath(config.prepare), data);
+
+		if (!data)
+			return;
+
+		issending = true;
+		config.loading && SETTER('loading', 'show');
+		config.sending && SEEX(self.makepath(config.sending), true, model);
+
+		if (!isnewbie && url.indexOf('PATCH') !== -1 && config.update_modified) {
+			var obj = {};
+			for (var i = 0; i < modified.length; i++) {
+				var key = modified[i];
+				obj[key] = data[key];
+			}
+			data = obj;
+		}
+
+		AJAX(url + ' REPEAT', data, function(response, err) {
+
+			model.response = data.response = response;
+			config.loading && SETTER('loading', 'hide', 500);
+			config.sending && SEEX(self.makepath(config.sending), false, model);
+			issending = false;
+
+			config.exec && SEEX(self.makepath(config.exec), response);
+			SET(self.path + '.response', response);
+
+			if (response instanceof Array) {
+				config.fail && SEEX(self.makepath(config.fail), response);
+				var msg = [];
+				for (var i = 0; i < response.length; i++)
+					msg.push(response[i].error);
+				config.messages && SETTER('message', 'warning', msg.join('<br />'));
+			} else {
+				self.rclass(cls + '-modified');
+				response.success && config.done && SEEX(self.makepath(config.done), response);
+				if (config.messages) {
+					var msg = (isnewbie ? messages.create_success : messages.update_success) || messages.success;
+					msg && SETTER('message', 'success', msg(model));
+				}
+				config.default && DEFAULT(self.path);
+			}
+		});
+	};
+
+	self.onkeydown = function(e) {
+		if (e.which === 13)
+			setTimeout2(self.ID, self.submit, self.delay + 50);
+	};
+
+	self.destroy = function() {
+		enter && enter.off('keydown', self.onkeydown);
+	};
+
+	self.configure = function(key, value, init) {
+		switch (key) {
+			case 'selector':
+				if (!init)
+					elements = self.find(value);
+				break;
+			case 'flags':
+				if (value) {
+					flags = value.split(',');
+					for (var i = 0; i < flags.length; i++)
+						flags[i] = '@' + flags[i];
+				} else
+					flags = null;
+				break;
+			case 'track':
+				track = value.split(',').trim();
+				break;
+			case 'create_success':
+			case 'update_success':
+			case 'success':
+				messages[key] = Tangular.compile(value);
+				break;
+		}
+	};
+
+	self.setter = function(value, path, type) {
+
+		var is = path === self.path || path.length < self.path.length;
+
+		if (reset !== is) {
+			reset = is;
+			self.tclass(cls + '-modified', !reset);
+		}
+
+		if ((type === 1 || type === 2) && track && track.length) {
+			for (var i = 0; i < track.length; i++) {
+				if (path.indexOf(track[i]) !== -1) {
+					tracked = 1;
+					return;
+				}
+			}
+			if (tracked === 1) {
+				tracked = 2;
+				setTimeout(function() {
+					tracked = 0;
+				}, config.delay * 3);
+			}
+		}
+	};
+
+	self.state = function(type, what) {
+		if (type === 3 || what === 3)
+			tracked = 0;
+		setTimeout2(self.ID, function() {
+			var disabled = tracked ? !VALID(path, flags) : DISABLED(path, flags);
+			if (!disabled && config.if)
+				disabled = !EVALUATE(self.path, config.if);
+			if (disabled !== old) {
+				elements.prop('disabled', disabled);
+				self.tclass(cls + '-ok', !disabled);
+				self.tclass(cls + '-no', disabled);
+				old = disabled;
+			}
+			isdisabled = disabled;
+		}, config.delay);
+	};
+});
---
+++ j-Submit/readme.md
@@ -0,0 +1,44 @@
+## j-Submit
+
+This component is very similar to `j-Validation` but it adds additional features which can simplify creating of SPA.
+
+__Configuration__:
+
+- `if` {String} can contain a condition for evaluation (optional)
+- `selector` {String} jQuery selector for disabling (optional, default: `button[name="submit"]`)
+- `delay` {Number} a delay for validation (optional, default: `100`)
+- `flags` {String} `+v13.0.0` flags for better validation, can contain `visible,hidden,enabled,disabled` components
+	- `enabled` validates all components which are enabled `input`, `textarea` or `select` elements
+	- `disabled` validates all components which are disabled `input`, `textarea` or `select` elements
+	- `hidden` validates all components which are hidden
+	- `visible` validates all components which are visible
+- `track` {String} can contain paths divided by comma which evaluate validation (executors for paths aren't components, but these methods `SET()`, `UPD()`, `INC()`, etc.)
+- `enter` {String} a closest selector for element where will be captured `ENTER` event (default: empty)
+- `exec` {String} a link to the function `function(response)`, executed if the response is processed
+- `fail` {String} a link to the function `function(response)`, executed if the response contains an error
+- `done` {String} a link to the function `function(response)`, executed if the response is successful
+- `prepare` {String} a link to the function `function(data)`, it's used for editing of data before send (keep a reference)
+- `sending` {String} a link to the function `function(issending, data)`, executed when the AJAX begins and ends
+- `default` {Boolean} resets the form if the response is successful (default: `true`)
+- `loading` {Boolean} shows loading via `j-Loading` when the form is sending (default: `true`)
+- `messages` {Boolean} shows messages via `j-Message` when the form is processed, contains a error handling (default: `true`)
+- `url` {String} a default URL address to submit data, must contain method with URL, e.g. `POST /api/users/` (default URL for `create/update`)
+- `create` {String} URL address for creating of item, e.g. `POST /api/users/`
+- `update` {String} URL address for updating of item, e.g. `PUT /api/users/{id}/` or `PATCH /api/users/{id}/`
+- `newbie` {String} Arrow function which determines `create` (is newbie?) or `update` URL, (default: `data => !data.id`)
+- `create_success` {String} A Tangular message, it's displayed if the response of the new item is successful
+- `update_success` {String} A Tangular message, it's displayed if the response of the updated item is successful
+- `success` {String} A Tangular message, it's displayed if the response is successful (default for create/update)
+- `update_modified` {Boolean} copies only modified data if: `update` url contains `PATCH`
+- `blocked` {Number} in milliseconds and it's a simple prevention for double click on submit button (default: `2000`)
+
+__Good to know__:
+
+- component toggles `ui-submit-ok` class if the model is valid
+- component toggles `ui-submit-no` class if the model is invalid
+- component toggles `ui-submit-modified` class if the model is modified
+
+### Author
+
+- Peter Širka <petersirka@gmail.com>
+- [License](https://www.totaljs.com/license/)
