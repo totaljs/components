9ef4884b7a0f305fd23bfeafb69b172c011f8d34
Peter Širka <petersirka@gmail.com>
2020-04-15 14:13:33 +0200

    Added new component `j-FloatingInput`.

---
+++ j-FloatingSearch/component.js
@@ -0,0 +1,53 @@
+COMPONENT('floatingsearch', 'height:45', function(self, config, cls) {
+
+	var is = false;
+	var input;
+
+	self.make = function() {
+
+		self.aclass(cls + ' hidden');
+		self.append('<div class="close"><i class="far fa-times"></i></div><div class="icon"><i class="fa fa-search"></i></div><form action="#" onsubmit="return false" class="input"><input placeholder="{1}" data-jc-bind type="text" name="input{2}" autocomplete="new-password" /></form>'.format(cls, config.placeholder, Date.now()));
+
+		if (config.height !== 45) {
+			self.find('.close,.icon,input').css('line-height', (config.height - 2) + 'px');
+			self.find('.close').css('width', config.height + 'px');
+			self.find('.input').css('margin-right', (config.height + 5) + 'px');
+			self.css('height', config.height + 'px');
+		}
+
+		input = self.find('input').on('keydown', function(e) {
+			if (e.which === 27)
+				self.hide();
+		}).on('input', self.toggleicon)[0];
+
+		self.event('click', '.close', self.hide);
+		self.event('click', '.icon', function() {
+			if (is) {
+				input.value && self.set('', 2);
+				self.toggleicon();
+				input.focus();
+			}
+		});
+	};
+
+	self.toggleicon = function() {
+		var tmp = !!input.value;
+		if (is !== tmp) {
+			is = tmp;
+			self.find('.icon').find('i').tclass('fa-search', !is).tclass('fa-times', is);
+			self.tclass(cls + '-filled', is);
+		}
+	};
+
+	self.hide = function() {
+		input.value && self.set('', 2);
+		self.aclass('hidden');
+	};
+
+	self.show = function() {
+		self.toggleicon();
+		self.rclass('hidden');
+		self.find('input').focus();
+	};
+
+});
---
+++ j-FloatingSearch/readme.md
@@ -0,0 +1,18 @@
+## j-FloatingSearch
+
+A simple floating search input. It uses absolute position and it's targeted above of toolbars because it can overlay the toolbar.
+
+__Configuration__
+
+- `placeholder` {String} A placeholder for input (default: empty)
+- `height` {Number} A height in pixels (default: `45`)
+
+__Methods__:
+
+- `floatingsearch/show` shows the component in the current position
+- `floatingsearch/hide` hides the component
+
+### Author
+
+- Peter Širka <petersirka@gmail.com>
+- [License](https://www.totaljs.com/license/)
