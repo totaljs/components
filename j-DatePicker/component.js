2f87892c9a9f03a909a8266a6185dda5f33c7d4f
Peter Širka <petersirka@gmail.com>
2020-04-28 10:43:49 +0200

    Removed `console.log`.

--- j-DatePicker/component.js
+++ j-DatePicker/component.js
@@ -194,8 +194,6 @@ COMPONENT('datepicker', 'today:Set today;firstday:0', function(self, config, cls
 		var height = 305 + (opt.cancel ? 25 : 0);
 		var s = 250;
 
-		console.log(off.left, opt.align, w, el.width(), el.innerWidth());
-
 		if (opt.element) {
 			switch (opt.align) {
 				case 'center':
