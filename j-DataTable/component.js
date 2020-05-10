e8a8d4523a7d6da3add4d9a0816acec443bab0e1
Peter Sirka <petersirka@gmail.com>
2020-05-10 16:39:05 +0200

    Added a new component `j-DataTable`.

---
+++ j-DataTable/component.js
@@ -0,0 +1,495 @@
+COMPONENT('datatable', 'parent:parent;margin:0;pluralizeitems:# items,# item,# items,# items;pluralizepages:# pages,# page,# pages,# pages;unhighlight:0;colwidth:150', function(self, config, cls) {
+
+	var cls2 = '.' + cls;
+	var container_rows;
+	var container_cols;
+	var container_pages;
+	var template_col = Tangular.compile('<div data-name="{{ name }}" class="{0}-col{{ if sort }} {0}-sortable{{ fi }}{{ if alignheader }} {{ alignheader }}{{ fi }}" style="width:{{ width }}px">{{ if sort }}<i class="fa"></i>{{ fi }}<span>{{ text }}</span></div>'.format(cls));
+	var meta = {};
+	var temp = {};
+
+	self.init = function() {
+		Thelpers.ui_datatable_checkbox = function(val) {
+			return ('<div class="{0}-checkbox' + (val ? ' {0}-checkbox-checked' : '') + '"><i class="fa fa-check"></i></div>').format(cls);
+		};
+
+		var resize = function() {
+			for (var i = 0; i < M.components.length; i++) {
+				var com = M.components[i];
+				if (com.name === 'datatable' && !HIDDEN(com.dom) && com.$ready && !com.$removed)
+					com.resizeforce();
+			}
+		};
+
+		W.DATATABLEMETA = false;
+		W.DATATABLESHIFT = false;
+		$(W).on('keydown', function(e) {
+			W.DATATABLESHIFT = e.shiftKey;
+			W.DATATABLEMETA = e.metaKey;
+		}).on('keyup', function() {
+			W.DATATABLESHIFT = false;
+			W.DATATABLEMETA = false;
+		}).on('resize', function() {
+			setTimeout2(cls, resize, 500);
+		});
+	};
+
+	self.make = function() {
+
+		var scr = self.find('script');
+		var html = scr.html();
+		scr.remove();
+
+		self.aclass(cls);
+		self.append('<div class="{0}-cols-scroller"><div class="{0}-cols"></div></div><div class="{0}-rows-scroller"><div class="{0}-rows"></div></div><div class="{0}-pagination"><div class="{0}-info"></div><div class="{0}-pages"><button name="prev" disabled><i class="fa fa-chevron-left"></i></button><input value="1" type="text" maxlength="4" disabled /><button name="next" disabled><i class="fa fa-chevron-right"></i></button></div></div>'.format(cls));
+
+		config.noborder && self.css('border', '0');
+
+		container_cols = self.find(cls2 + '-cols');
+		container_rows = self.find(cls2 + '-rows');
+		container_pages = self.find(cls2 + '-pagination');
+
+		self.scrollbar = SCROLLBAR(container_rows.parent(), { onscroll: self.onscroll, visibleX: 1, visibleY: 1 });
+
+		self.event('click', cls2 + '-sortable', function() {
+
+			var el = $(this);
+			var name = el.attrd('name');
+			var isa = el.hclass('asc');
+			var isd = el.hclass('desc');
+
+			if (isa && !isd)
+				name += '_desc';
+			else if (!isa && !isd)
+				name += '_asc';
+
+			self.sort(name);
+		});
+
+		container_rows.on('click', cls2 + '-row', function() {
+
+			if (meta.clickskip) {
+				meta.clickskip = false;
+				return;
+			}
+
+			var el = $(this);
+			var index = +el.attrd('index');
+			var row = meta.rows[index];
+
+			if (meta.checked.length) {
+				meta.checked = [];
+				self.redrawchecked();
+			}
+
+			if (config.highlight) {
+				if (config.unhighlight && meta.selected && meta.selected[0] === this) {
+					meta.selected.rclass(cls + '-selected');
+					config.click && EXEC(self.makepath(config.click), null, self);
+					meta.selected = null;
+				} else
+					self.select(row);
+			}
+		});
+
+		container_rows.on('mousedown', cls2 + '-row-col', function(e) {
+
+			var now = Date.now();
+
+			if (meta.click) {
+				if (now - meta.click < 500)
+					e.preventDefault();
+			}
+
+			meta.click = now;
+
+			if (!config.checked)
+				return;
+
+			var elcol = $(this);
+			var elrow = elcol.parent();
+			var oldindex = temp.index;
+
+			temp.index = +elrow.attrd('index');
+
+			if (W.DATATABLEMETA || W.DATATABLESHIFT)
+				e.preventDefault();
+
+			if (W.DATATABLESHIFT && oldindex !== -1) {
+				for (var i = oldindex; i <= temp.index; i++) {
+					var row = meta.rows[i];
+					if (meta.checked.indexOf(row) === -1)
+						meta.checked.push(row);
+				}
+				self.redrawchecked();
+				meta.clickskip = true;
+				return;
+			}
+
+			if (W.DATATABLEMETA) {
+				var row = meta.rows[temp.index];
+				if (row && meta.checked.indexOf(row) === -1) {
+					console.log(row);
+					meta.checked.push(row);
+					self.redrawchecked();
+				}
+				meta.clickskip = true;
+			} else {
+				temp.drag = true;
+				temp.bindevents();
+			}
+		});
+
+		temp.onmove = function(e) {
+
+			e.preventDefault();
+
+			var elrow = $(this);
+			var index = +elrow.attrd('index');
+
+			if (index === meta.currentindex)
+				return;
+
+			if (meta.selected) {
+				meta.selected.rclass(cls + '-selected');
+				meta.selected = null;
+				config.click && EXEC(self.makepath(config.click), null, self);
+			}
+
+			meta.currentindex = index;
+			meta.checked = [];
+
+			for (var i = temp.index; i <= index; i++) {
+				var row = meta.rows[i];
+				if (meta.checked.indexOf(row) === -1)
+					meta.checked.push(row);
+			}
+
+			self.redrawchecked();
+		};
+
+		temp.bindevents = function() {
+			container_rows.on('mouseup', temp.unbindevents);
+			container_rows.on('mousemove', cls2 + '-row', temp.onmove);
+		};
+
+		temp.unbindevents = function() {
+
+			meta.selected && meta.selected.rclass(cls + '-selected');
+			config.click && EXEC(self.makepath(config.click), null, self);
+			meta.selected = null;
+
+			container_rows.off('mouseup', temp.unbindevents);
+			container_rows.off('mousemove', cls2 + '-row', temp.onmove);
+		};
+
+		container_rows.on('dblclick', cls2 + '-row-col', function() {
+
+			var el = $(this);
+			var name = el.attrd('name');
+			var col = meta.cols.findItem('name', name);
+			var elrow = el.parent();
+			var index = +elrow.attrd('index');
+			var row = meta.rows[index];
+
+			if (!col.editable) {
+				config.dblclick && SEEX(self.makepath(config.dblclick), row, self, elrow);
+				return;
+			}
+
+			var opt = {};
+			opt.index = index;
+			opt.row = row;
+			opt.col = col;
+			opt.elcol = el;
+			opt.elrow = elrow;
+			opt.value = row[name];
+			opt.next = function(value) {
+
+				if (!row.CHANGES)
+					row.CHANGES = {};
+
+				row.CHANGES[name] = true;
+				row[name] = value;
+
+				var selected = opt.elrow.hclass(cls + '-selected');
+				var el = self.redrawrow(index);
+				if (el) {
+					el.find(cls2 + '-row-col[data-name="{0}"]'.format(name)).aclass(cls + '-changed');
+					if (meta.changed.indexOf(row) === -1) {
+						meta.changed.push(row);
+						if (selected)
+							meta.selected = el.aclass(cls + '-selected');
+						config.changed && EXEC(self.maketpath(config.changed), meta.changed);
+					}
+				}
+			};
+
+			config.editable && EXEC(self.makepath(config.editable), opt);
+		});
+
+		container_pages.on('click', 'button', function() {
+			var page = 0;
+			switch (this.name) {
+				case 'prev':
+					page = meta.page - 1;
+					break;
+				case 'next':
+					page = meta.page + 1;
+					break;
+			}
+			page && self.page(page);
+		});
+
+		container_pages.on('change', 'input', function() {
+			var page = this.value.parseInt();
+
+			if (page > meta.pages)
+				page = meta.pages;
+
+			if (page < 1)
+				page = 1;
+
+			if (page !== meta.page)
+				self.page(page);
+		});
+
+		html && self.redrawcols(new Function('return ' + html.trim())());
+		self.resizeforce();
+	};
+
+	self.sort = function(name) {
+		// name_asc    : asc
+		// name_desc   : desc
+		// name        : off
+		var arr = name.split('_');
+		container_cols.find('.asc,.desc').rclass('asc desc');
+		arr[1] && container_cols.find('> {0}-col[data-name="{1}"]'.format(cls2, arr[0])).aclass(arr[1]);
+		meta.sort = arr.length ? name : undefined;
+		self.page(meta.page);
+	};
+
+	self.page = function(page) {
+		config.exec && EXEC(self.makepath(config.exec), page, meta.sort);
+	};
+
+	self.resize = function() {
+		setTimeout2(self.ID, self.resizeforce, 500);
+	};
+
+	self.resizeforce = function() {
+		var parent = self.parent(config.parent);
+		var width = parent.width();
+		var height = parent.height() - container_cols.height() - container_pages.height();
+
+		if (meta.ww === width && meta.wh === height) {
+			self.scrollbar.resize();
+			meta.width && container_rows.width(meta.width);
+			return;
+		}
+
+		meta.ww = width;
+		meta.wh = height;
+		self.find(cls2 + '-rows-scroller').css('height', height);
+		meta.width && container_rows.width(meta.width);
+		container_cols.css('min-width', width);
+		self.scrollbar.resize();
+	};
+
+	self.onscroll = function(e) {
+		container_cols[0].parentNode.scrollLeft = e.area[0].scrollLeft;
+	};
+
+	self.renderrow = function(index, row) {
+
+		var div = document.createElement('DIV');
+		var tmp = [];
+		var ccls = '';
+
+		if (meta.rowclass)
+			ccls = meta.rowclass(row).trim();
+
+		div.setAttribute('class', cls + '-row' + (index % 2 ? (' ' + cls + '-row-odd') : '') + (ccls ? (' ' + ccls) : ''));
+		div.setAttribute('data-index', index);
+
+		for (var i = 0; i < meta.cols.length; i++) {
+			var col = meta.cols[i];
+			var value = row[col.name];
+
+			if (col.template)
+				value = col.template(row);
+
+			ccls = '';
+
+			if (row.CHANGES && row.CHANGES[col.name])
+				ccls += ' ' + cls + '-changed';
+
+			if (col.align)
+				ccls += ' ' + col.align;
+
+			tmp.push('<div class="{0}-row-col{4}" data-name="{1}" style="width:{2}px">{3}</div>'.format(cls, col.name, col.width, value, ccls));
+		}
+
+		div.innerHTML = tmp.join('');
+		return div;
+	};
+
+	self.replacerow = function(index, row) {
+		var el = container_rows.find(cls2 + '-row[data-index="{0}"]'.format(index));
+		if (el.length) {
+			self.get().rows[index] = row;
+			container_rows[0].replaceChild(self.renderrow(index, row), el[0]);
+		}
+	};
+
+	self.redrawchecked = function() {
+		container_rows.find(cls2 + '-checked').rclass(cls + '-checked');
+		var rows = container_rows.find(cls2 + '-row');
+		for (var i = 0; i < rows.length; i++) {
+			var el = rows[i];
+			var index = +el.getAttribute('data-index');
+			var row = meta.rows[index];
+			if (meta.checked.indexOf(row) !== -1)
+				$(el).aclass(cls + '-checked');
+		}
+		config.checked && SEEX(self.makepath(config.checked), meta.checked, self);
+	};
+
+	self.redrawrow = function(index) {
+
+		if (typeof(index) === 'object')
+			index = meta.rows.indexOf(index);
+
+		if (index === -1)
+			return;
+
+		var el = container_rows.find(cls2 + '-row[data-index="{0}"]'.format(index));
+		if (el.length) {
+			var row = self.renderrow(index, meta.rows[index]);
+			container_rows[0].replaceChild(row, el[0]);
+			row = $(row);
+
+			if (meta.checked.indexOf(meta.rows[index]) !== -1)
+				row.aclass(cls + '-checked');
+
+			return row;
+		}
+	};
+
+	self.clear = function() {
+		self.refresh();
+	};
+
+	self.rebind = self.redrawcols = function(cols) {
+
+		var key = 'datatable' + HASH(cols);
+		var builder = [];
+
+		meta.id = key;
+		meta.width = 0;
+		meta.cols = [];
+
+		for (var i = 0; i < cols.length; i++) {
+			var col = cols[i];
+
+			if (typeof(col) === 'string') {
+				meta.rowclass = Tangular.compile(col);
+				continue;
+			}
+
+			meta.cols.push(col);
+			meta.width += col.width || config.colwidth;
+
+			if (!col.template && col.type) {
+				switch (col.type.toLowerCase()) {
+					case 'number':
+						col.template = '{{ {0} | format({1}) }}'.format(col.name, col.format || '0');
+						if (!col.align)
+							col.align = 'right';
+						if (!col.alignheader)
+							col.alignheader = 'center';
+						break;
+					case 'currency':
+						col.template = '{{ {0} | currency({1}) }}'.format(col.name, col.currency || col.format);
+						if (!col.align)
+							col.align = 'right';
+						if (!col.alignheader)
+							col.alignheader = 'center';
+						break;
+					case 'boolean':
+						col.template = '{{ {0} | ui_datatable_checkbox }}'.format(col.name);
+						if (!col.align)
+							col.align = 'center';
+						if (!col.alignheader)
+							col.alignheader = 'center';
+						break;
+					case 'date':
+						col.template = '{{ {0} | format{1} }}'.format(col.name, col.format ? ('(\'' + col.format + '\')') : '');
+						if (!col.align)
+							col.align = 'center';
+						if (!col.alignheader)
+							col.alignheader = 'center';
+						break;
+				}
+				if (col.template)
+					col.template = Tangular.compile(col.template);
+			}
+
+			builder.push(template_col(col));
+		}
+
+		var sw = self.scrollbar.pathy.width();
+		container_cols.css('width', meta.width + sw).html(builder.join(''));
+	};
+
+	self.select = function(row) {
+		var index = meta.rows.indexOf(row);
+		if (index !== -1) {
+			var el = container_rows.find(cls2 + '-row[data-index="{0}"]'.format(index));
+			meta.selected && meta.selected.rclass(cls + '-selected');
+			meta.selected = el.aclass(cls + '-selected');
+			config.click && SEEX(self.makepath(config.click), row, self, el);
+		}
+	};
+
+	self.setter = function(value, path, type) {
+
+		value.cols && self.redrawcols(value.cols);
+
+		var rows = value.rows || value.items;
+		meta.rows = rows;
+
+		meta.page = value.page || 1;
+		meta.pages = value.pages || 1;
+		meta.count = value.count;
+		meta.checked = [];
+		meta.changed = [];
+		temp.index = -1;
+
+		var dom = container_rows[0];
+		dom.innerHTML = '';
+
+		for (var i = 0; i < rows.length; i++)
+			dom.appendChild(self.renderrow(i, rows[i]));
+
+		self.resizeforce();
+
+		if (type !== 'noscroll')
+			self.scrollbar.scrollTop(0);
+
+		var pages = value.pages ? (value.pages.pluralize(config.pluralizepages) + ' / ') : '';
+		container_pages.find(cls2 + '-info').html(pages + (value.count || rows.length).pluralize(config.pluralizeitems));
+		var buttons = container_pages.find('button');
+		buttons[0].disabled = meta.page <= 1;
+		buttons[1].disabled = meta.page >= meta.pages;
+		container_pages.find('input').val(meta.page)[0].disabled = meta.pages === 1;
+
+		if (config.autoselect && rows && rows.length) {
+			setTimeout(function(rows) {
+				var index = meta.selected ? +meta.selected.attrd('index') : 0;
+				self.select(rows[index]);
+			}, 1, rows);
+		}
+	};
+
+});
---
+++ j-DataTable/readme.md
@@ -0,0 +1,85 @@
+## j-DataTable
+
+__BETA__: This component is targeted for SPA and server-side listing.
+
+- please try to understand the functionality
+- supports pagination
+- needed `bootstrap` grid system
+- supports sorting
+- supports vertical/horizontal scrolling
+- responsive
+- supports __dark mode__
+
+__Data-source__ needs to contain:
+
+```javascript
+{
+	cols: [{ name: 'id', ... }], // optional, can be defined inline in HTML
+    items: [{ name: 'Row 1' }, { name: 'Row 2' }, ...] // items
+    page: 1,    // current pages
+    pages: 30,  // count of pages
+    limit: 5,   // items limit per page
+    count: 150  // count of items in DB
+}
+```
+
+__Configuration__:
+
+- `pluralizepages` {String} pluralization for pages (optional, default: `# pages,# page,# pages,# pages`)
+- `pluralizeitems` {String} pluralization for items (optional, default: `# items,# item,# items,# items`)
+- `height` {Number/String} height of grid, supported values: `auto` (default), `parent` or `NUMBER` as height or `selector` for jQuery.closest()
+- `margin` {Number} a top margin for height (optional, default: `0`)
+- `click` {String} a link to `function(row, grid, row_el)`, it's executed if the user clicks on a row
+- `highlight` {Boolean} each selected row (after `click`) will be highlighted (default: `false`)
+- `unhighlight` {Boolean} enables `undo highlighting` of selected row (default: `true`)
+- `checked` {String} `path` to a variable or path to `function(rows, grid)` is executed if the user selected/checked some rows
+- `autoselect` {Boolean} enables auto-select of first row in grid, it performs `EXEC(config.click)`, default: `false`
+- `exec` {String} a link to `function(page, sort)` for server-side operations only (it disables client-side sorting & filtering), supported types: `refresh` or `page`
+- `changed` {String} `path` to a variable or path to `function(rows, grid)` is executed if the user changed some rows
+- `dblclick` {String} a link to `function(row, grid, row_el)` method, it's executed if the user double-clicks on a row
+- `noborder` {Boolean} can disable a border around the grid (default: `false`)
+- `colwidth` {Number} a default width for columns (default: `150`)
+- `editable` {String a link to `function(opt)` when the user performs editing
+
+__Column properties__:
+
+- `name` {String} a name of field in the row object
+- `text` {String} a column label, text with `.fa fa-home` will render FontAwesome icon
+- `width` {Number} a column width (optional, default `config.colwidth`)
+- `align` {String} can be `center` or `right` (optional, default: `left`)
+- `template` {String} can be a Tangular template and the model is the entire object of row
+- `sort` {Boolean} enables sorting (optional, default: `false`)
+- `format` {String/Number} can be used for date and numbers (count of decimals) field (optional), e.g. `dd.MM.yyyy`
+- `currency` {String} a currency formatter, just add a currency
+- `class` {String} optional, a custom column class name
+- `empty` {String/Boolean} optional, can rewrite empty value with the value defined in empty field (`true` value uses `---` as a default value)
+- `type` {String} optional, can contain a data-type (`boolean`, `date` or `number`) for the field
+- `editable` {Boolean} enables editing
+
+__Methods__:
+
+- `component.redrawrow(row_object/index)` redraws row or __NEW__: replaces+redraws `row_object` with a new `new_object_row`
+- `component.clear()` clears all changes
+- `component.select(row)` selects `row` must be the same object as in data-source
+- `component.rebind(columnsdeclaration)` the method rebinds the schema
+
+__Good to know__:
+
+- `SET('link.to.data', data, 'noscroll')` --> `noscroll` type disables reseting of scrolling
+
+__How to extend a class of row?__
+
+- look to the `{{ if active` line, it extends a class of row by adding `dg-active-class` if the `active` will be valid
+
+```javascript
+[
+	'{{ if active }} dg-active-class{{ fi }}',
+	{ name: 'name', text: 'Name', width: 200 },
+	// other columns ...
+]
+```
+
+### Author
+
+- Peter Širka <petersirka@gmail.com>
+- [License](https://www.totaljs.com/license/)
