be056a060a643330474879442fcfac8191ad8843
Peter Širka <petersirka@gmail.com>
2020-07-13 12:42:27 +0200

    Fixed orietantation in `j-PictureUploadExpert`.

--- j-PictureUploadExpert/component.js
+++ j-PictureUploadExpert/component.js
@@ -291,20 +291,15 @@ COMPONENT('pictureuploadexpert', 'width:200;height:100;background:#FFFFFF;qualit
 			var canvas = document.createElement('canvas');
 			var ctx = canvas.getContext('2d');
 
-			// set proper canvas dimensions before transform & export
-			if (4 < srcOrientation && srcOrientation < 9) {
-				canvas.width = height;
-				canvas.height = width;
-			} else {
-				canvas.width = width;
-				canvas.height = height;
-			}
+			canvas.width = width;
+			canvas.height = height;
+
 			switch (srcOrientation) {
 				case 2: ctx.transform(-1, 0, 0, 1, width, 0); break;
 				case 3: ctx.transform(-1, 0, 0, -1, width, height); break;
 				case 4: ctx.transform(1, 0, 0, -1, 0, height); break;
 				case 5: ctx.transform(0, 1, 1, 0, 0, 0); break;
-				case 6: ctx.transform(0, 1, -1, 0, height, 0); break;
+				case 6: ctx.transform(-1, 0, 0, 1, width, 0); break;
 				case 7: ctx.transform(0, -1, -1, 0, height, width); break;
 				case 8: ctx.transform(0, -1, 1, 0, 0, width); break;
 			}
