4ef260986e15c242375f24c20315086a7ba3b472
Peter Širka <petersirka@gmail.com>
2020-09-25 09:17:37 +0200

    Added `iconoptions` to `j-Tree`.

--- j-Tree/component.js
+++ j-Tree/component.js
@@ -1,4 +1,4 @@
-COMPONENT('tree', 'autoreset:false;checkednested:true;reselect:false', function(self, config, cls) {
+COMPONENT('tree', 'autoreset:false;checkednested:true;reselect:false;iconoptions:fa fa-ellipsis-h', function(self, config, cls) {
 
 	var cls2 = '.' + cls;
 	var cache = null;
@@ -15,7 +15,7 @@ COMPONENT('tree', 'autoreset:false;checkednested:true;reselect:false', function(
 	self.make = function() {
 
 		self.aclass(cls);
-		self.template = Tangular.compile(('<div' + (config.dragdrop ? ' draggable="true"' : '') + ' class="{0}-item{{ if children }} {0}-expand{{ fi }}" title="{{ name }}" data-index="{{ $pointer }}">' + (config.checked ? '<div class="{0}-checkbox"><i class="fa fa-check"></i></div><div class="{0}-label">' : '') + '<i class="far {{ if children }}{0}-folder{{ else }}{{ icon | def(\'fa-file-o\') }}{{ fi }}"></i>' + (config.options ? '<span class="{0}-options"><i class="fa fa-ellipsis-h"></i></span>' : '') + '<div class="{0}-item-name{{ if classname }} {{ classname }}{{ fi }}">{{ if html }}{{ html | raw }}{{ else }}{{ name }}{{ fi }}</div></div>' + (config.checked ? '</div>' : '')).format(cls));
+		self.template = Tangular.compile(('<div' + (config.dragdrop ? ' draggable="true"' : '') + ' class="{0}-item{{ if children }} {0}-expand{{ fi }}" title="{{ name }}" data-index="{{ $pointer }}">' + (config.checked ? '<div class="{0}-checkbox"><i class="fa fa-check"></i></div><div class="{0}-label">' : '') + '<i class="far {{ if children }}{0}-folder{{ else }}{{ icon | def(\'fa-file-o\') }}{{ fi }}"></i>' + (config.options ? ('<span class="{0}-options"><i class="' + config.iconoptions + '"></i></span>') : '') + '<div class="{0}-item-name{{ if classname }} {{ classname }}{{ fi }}">{{ if html }}{{ html | raw }}{{ else }}{{ name }}{{ fi }}</div></div>' + (config.checked ? '</div>' : '')).format(cls));
 
 		self.event('click', cls2 + '-checkbox', function(e) {
 			e.stopPropagation();
@@ -319,7 +319,7 @@ COMPONENT('tree', 'autoreset:false;checkednested:true;reselect:false', function(
 		} else
 			config.first !== false && cache.first && setTimeout(self.first, 100);
 
-		if (!isexpand && config.expand)
+		if (!isexpand && config.expanded)
 			self.expand();
 
 		config.checked && EXEC(self.makepath(config.checked), EMPTYARRAY, self);
