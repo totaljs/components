fe5fbfe97f32160bcea74ff6b747a5d7cb2d4eba
Peter Sirka <petersirka@gmail.com>
2020-11-14 19:57:29 +0100

    Added new component `j-Rules`.

---
+++ j-Rules/component.js
@@ -0,0 +1,125 @@
+COMPONENT('rules', 'dirsearch:Search', function(self, config, cls) {
+
+	var schema;
+	var skip = false;
+	var cls2 = '.' + cls;
+	var items;
+
+	self.readonly();
+	self.renders = {};
+
+	self.renders.number = function(item) {
+		return '<div class="{0}-number"><input type="text"{1} value="{2}" /></div>'.format(cls, item.enabled ? '' : ' disabled', item.value);
+	};
+
+	self.renders.string = function(item) {
+		if (item.items) {
+			var m = item.items.findItem('id', item.value);
+			return '<div class="{0}-string"><div class="{0}-list">{2}</div></div>'.format(cls, item.enabled ? '' : ' disabled', m ? m.name : '');
+		} else
+			return '<div class="{0}-string"><input type="text"{1} value="{2}" /></div>'.format(cls, item.enabled ? '' : ' disabled', item.value);
+	};
+
+	self.renders.boolean = function(item) {
+		return '<div class="{0}-boolean"><span class="{0}-checkbox"><i></i></span></div>';
+	};
+
+	self.renders.item = function(item) {
+
+		var type = item.type.toLowerCase();
+		var html = self.renders[type](item);
+		var comparer = ['<', '<=', '==', '!=', '>=', '>'];
+
+		for (var i = 0; i < comparer.length; i++) {
+			var m = comparer[i];
+			var disabled = type !== 'number' && i !== 2 && i !== 3;
+			var selected = item.comparer === m;
+			comparer[i] = '<button data-disabled="{3}" class="{4}" name="{0}"{1}>{2}</button>'.format(m, disabled || !item.enabled ? ' disabled' : '', m === '!=' ? '<>' : m === '==' ? '=' : m.replace('>=', '=>'), disabled, selected ? 'selected' : '');
+		}
+
+		return ('<div class="{0}-item' + (item.enabled ? '' : ' disabled') + '" data-name="' + item.name + '"><div class="{0}-value">' + html + '</div><div class="{0}-comparer">' + comparer.join('') + '</div><div class="{0}-key"><div class="{0}-enabled"><span class="{0}-checkbox' + (item.enabled ? ' checked' : '') + '"><i></i></span></div><div class="{0}-label">' + item.label + (item.note ? ('<div class="help">' + item.note + '</div>') : '') + '</div></div></div>').format(cls);
+	};
+
+	self.forcechange = function() {
+		skip = true;
+		self.change(true);
+		self.update();
+	};
+
+	self.make = function() {
+		self.aclass(cls);
+
+		self.event('click', cls2 + '-enabled', function() {
+			var el = $(this);
+			var is = el.find('span').tclass('checked').hclass('checked');
+			el = el.closest(cls2 + '-item');
+			el.tclass('disabled', !is);
+			el.find('button').prop('disabled', function() {
+				return this.getAttribute('data-disabled') === 'true' ? true : !is;
+			});
+			el.find('input').prop('disabled', !is);
+			items.findItem('name', el.attrd('name')).enabled = is;
+			self.forcechange();
+		});
+
+		self.event('click', cls2 + '-boolean', function() {
+			var el = $(this);
+			var is = el.find('span').tclass('checked').hclass('checked');
+			var id = el.closest(cls + '-item').attrd('name');
+			items.findItem('name', id).value = is;
+			self.forcechange();
+		});
+
+		self.event('click', 'button', function() {
+			var el = $(this);
+			el.parent().find('.selected').rclass('selected');
+			el.aclass('selected');
+			var id = el.closest(cls2 + '-item').attrd('name');
+			items.findItem('name', id).comparer = el.prop('name');
+			self.forcechange();
+		});
+
+		self.event('change', 'input', function() {
+			var el = $(this);
+			var id = el.closest(cls2 + '-item').attrd('name');
+			var item = items.findItem('name', id);
+			if (item.type === 'number')
+				item.value = el.val().parseFloat();
+			else
+				item.value = el.val();
+			self.forcechange();
+		});
+
+		self.event('click', cls2 + '-list', function() {
+			var el = $(this);
+			var opt = {};
+			var item = items.findItem('name', el.closest(cls2 + '-item').attrd('name'));
+			opt.element = el;
+			opt.align = 'right';
+			opt.items = item.items;
+			opt.placeholder = config.dirsearch;
+			opt.callback = function(sel) {
+				el.text(sel.name);
+				self.forcechange();
+			};
+			SETTER('directory/show', opt);
+		});
+
+	};
+
+	self.setter = function(value) {
+
+		if (skip) {
+			skip = false;
+			return;
+		}
+
+		items = value;
+		var builder = [];
+		for (var i = 0; i < value.length; i++)
+			builder.push(self.renders.item(value[i]));
+
+		self.html(builder.join(''));
+	};
+
+});
---
+++ j-Rules/readme.md
@@ -0,0 +1,39 @@
+## j-Rules (BETA)
+
+The component can create a simple rules which you can transform to some engines like Total.js Flow. The component uses `j-Directory` component.
+
+__Configuration__:
+
+- `dirsearch` {String} A placeholder for search input in `j-Directory`, default: `search`
+
+__Data declaration__:
+
+```javascript
+var form = [];
+form.push({ enabled: true, label: 'CPU', name: 'cpu', type: 'number', comparer: '==', value: 0, note: 'Value in mega bytes' });
+form.push({ enabled: false, label: 'Memory', name: 'memory', type: 'number', comparer: '>', value: 0 });
+form.push({ enabled: true, label: 'Error', name: 'error', type: 'string', comparer: '==', items: [{ name: 'Error', id: 'error' }, { name: 'Obsolete', id: 'obsolete' }] });
+form.push({ enabled: false, label: 'Offline', name: 'offline', type: 'boolean', comparer: '==', value: 0 });
+```
+
+__Data properties in data declaration__:
+
+- `name` {String} an item identifier (required)
+- `label` {String} a label (required)
+- `note` {String} a small note under control
+- `type` {String} a type, lower-case (required)
+- `items` {Object Array} items for `string` type only `{ id: String, name: String }`
+- `enabled` {Boolean} enables item
+- `comparer` {String} can be `==`, `!=`, `>`, `>=`, `<=`, `<` (required)
+- `value` {Number/Boolean/String} a value
+
+__Allowed types in data__:
+
+- `string`
+- `boolean`
+- `number`
+
+### Author
+
+- Peter Širka <petersirka@gmail.com>
+- [License](https://www.totaljs.com/license/)
