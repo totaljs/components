23d634c577d7551a2bd096a56f9197112fd78bd2
Peter Sirka <petersirka@gmail.com>
2020-12-01 22:59:27 +0100

    Added new component `j-Columns`.

---
+++ j-Columns/component.js
@@ -0,0 +1,102 @@
+COMPONENT('columns', 'parent:window;margin:0', function(self, config, cls) {
+
+	var columns;
+	var cache;
+
+	self.readonly();
+	self.nocompile();
+
+	self.make = function() {
+		self.aclass(cls + (config.noborder ? (' ' + cls + '-noborder') : ''));
+		columns = self.find('> section').aclass(cls + '-col');
+		self.rclass('hidden');
+		self.resize();
+		self.on('resize2', self.resize);
+	};
+
+	self.resize = function() {
+		setTimeout2(self.ID, self.resizeforce, 200);
+	};
+
+	self.resizeforce = function() {
+		var parent = self.parent(config.parent);
+		var wh = parent.height();
+		var w = self.element.width();
+		var key = w + 'x' + wh;
+		if (cache === key)
+			return;
+		cache = key;
+		var notdefined = [];
+		var total = 0;
+		var css = {};
+
+		var d = WIDTH();
+		var m = config['margin' + d] || 0;
+
+		if (config.margin)
+			wh -= config.margin;
+
+		if (m)
+			wh -= m;
+
+		var index = 0;
+		switch (d) {
+			case 'md':
+				index = 1;
+				break;
+			case 'sm':
+				index = 2;
+				break;
+			case 'xs':
+				index = 3;
+				break;
+		}
+
+		for (var i = 0; i < columns.length; i++) {
+			var el = $(columns[i]);
+			var size = el.attrd('size');
+
+			if (size) {
+				size = size.split(',');
+				var s = size[index];
+				if (s == null) {
+					while (index > -1 && !s) {
+						index--;
+						s = size[index];
+					}
+				}
+
+				if (s === '0')
+					s = 0;
+
+				if (s) {
+					size = +s;
+					total += size;
+					css.width = size;
+					css.height = wh;
+					el.css(css).rclass('hidden');
+				} else
+					el.aclass('hidden');
+			} else
+				notdefined.push(el);
+		}
+
+		var sum = ((w - total) / notdefined.length).floor(3);
+
+		for (var i = 0; i < notdefined.length; i++) {
+			var el = notdefined[i];
+			css.width = sum;
+			css.height = wh;
+			el.css(css);
+		}
+
+		self.css('height', wh);
+		self.rclass('invisible');
+		self.element.SETTER('*/resize');
+	};
+
+	self.setter = function() {
+		self.resize();
+	};
+
+});
---
+++ j-Columns/readme.md
@@ -0,0 +1,17 @@
+## j-Columns
+
+The component `Columns` is a very simple alternative to `j-Layout` and `j-Layout2`. With this component you can set columns with 100% height by your needs.
+
+- `parent` {String} can contain `parent`, `window` or jQuery selector for closest method (default: `window`)
+- `margin` {Number} add margin for `height` of this component (default: `0`)
+- `marginlg` {Number} adds margin for `height` of this component in `LG` display (default: `margin`)
+- `marginmd` {Number} adds margin for `height` of this component in `MD` display (default: `margin`)
+- `marginsm` {Number} adds margin for `height` of this component in `SM` display (default: `margin`)
+- `marginxs` {Number} adds margin for `height` of this component in `XS` display (default: `margin`)
+- `noborder` {Boolean} disables border (default: `false`)
+
+
+### Author
+
+- Peter Širka <petersirka@gmail.com>
+- [License](https://www.totaljs.com/license/)
