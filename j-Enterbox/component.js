7667a5a7a80cfff61f9fce13911d0a562c350fd9
Peter Sirka <petersirka@gmail.com>
2020-12-19 10:30:18 +0100

    Improved code.

--- j-Enterbox/component.js
+++ j-Enterbox/component.js
@@ -8,7 +8,7 @@ COMPONENT('enterbox', function(self, config, cls) {
 			return;
 		switch (key) {
 			case 'icon':
-				self.find('i').rclass().aclass((value.indexOf(' ') === -1 ? 'fa fa-' : '') + value);
+				self.find('i').rclass().aclass(self.faicon(value));
 				break;
 			case 'placeholder':
 				self.find('input').prop('placeholder', value);
@@ -20,13 +20,8 @@ COMPONENT('enterbox', function(self, config, cls) {
 	};
 
 	self.make = function() {
-
-		var icon = config.icon;
-		if (icon)
-			icon = (icon.indexOf(' ') === -1 ? 'fa fa-' : '') + icon;
-
 		self.aclass(cls);
-		self.append('<div class="{3}-button"><button><i class="{0}"></i></button></div><div class="{3}-input"><input type="text" placeholder="{1}" maxlength="{2}" /></div>'.format(icon || 'fa fa-keyboard-o', config.placeholder, config.maxlength || 50, cls));
+		self.append('<div class="{3}-button"><button><i class="{0}"></i></button></div><div class="{3}-input"><input type="text" placeholder="{1}" maxlength="{2}" /></div>'.format(self.faicon(config.icon) || 'fa fa-keyboard-o', config.placeholder, config.maxlength || 50, cls));
 		self.event('click', 'button', self.submit);
 		self.event('keyup', 'input', function(e) {
 			e.which === 13 && self.submit();
