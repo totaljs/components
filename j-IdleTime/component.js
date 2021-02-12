9358da2edfb9f016edd906352187500a11403f3b
Peter Širka <petersirka@gmail.com>
2021-02-12 10:47:50 +0100

    Improved `j-IdleTime`.

--- j-IdleTime/component.js
+++ j-IdleTime/component.js
@@ -2,8 +2,11 @@ COMPONENT('idletime', 'count:300', function(self, config) {
 
 	var is = false;
 	var count = 0;
+	var countfocus = 0;
 	var interval;
 	var interval_rebind;
+	var ticks;
+	var rebinded = false;
 	var $W = $(W);
 
 	self.singleton();
@@ -11,8 +14,9 @@ COMPONENT('idletime', 'count:300', function(self, config) {
 	self.readonly();
 
 	function rebind() {
-		is && EMIT('idletime', false, 0);
+		is && EMIT('idletime', false);
 		is = false;
+		countfocus = 0;
 		count = 0;
 		unbind();
 		interval_rebind && clearTimeout(interval_rebind);
@@ -20,11 +24,17 @@ COMPONENT('idletime', 'count:300', function(self, config) {
 	}
 
 	function rebind2() {
-		$W.on('mousemove click keyup touchstart focus', rebind);
+		if (!rebinded) {
+			$W.on('mousemove mousewheel click keyup touchstart focus scroll pageshow', rebind);
+			rebinded = true;
+		}
 	}
 
 	function unbind() {
-		$W.off('mousemove click keyup touchstart focus', rebind);
+		if (rebinded) {
+			$W.off('mousemove mousewheel click keyup touchstart focus scroll pageshow', rebind);
+			rebinded = false;
+		}
 	}
 
 	self.destroy = function() {
@@ -35,13 +45,37 @@ COMPONENT('idletime', 'count:300', function(self, config) {
 	};
 
 	self.make = function() {
+
+		$(document).on('visibilitychange', function() {
+			var now = Date.now();
+			if (document.hidden) {
+				ticks = now;
+			} else {
+				var diff = Math.ceil((now - ticks) / 1000);
+				if (diff >= config.count) {
+					rebind();
+					setTimeout(function() {
+						EMIT('reload');
+					}, 500);
+				}
+				ticks = now;
+			}
+		});
+
 		rebind2();
 		interval = setInterval(function() {
-			if (count > config.count && !is) {
+
+			if (document.hasFocus())
+				countfocus = 0;
+			else
+				countfocus++;
+
+			if ((count > config.count || countfocus > config.count) && !is) {
 				is = true;
-				EMIT('idletime', true, count);
+				EMIT('idletime', true);
 			} else
 				count++;
+
 		}, 1000);
 	};
 });
--- j-IdleTime/readme.md
+++ j-IdleTime/readme.md
@@ -1,10 +1,15 @@
 ## j-IdleTime
 
-This component emits event `ON('idletime', function(is, count) {})` when the website is in idle. It reacts on mouse, keyboard, touch events or focusing of window.
+This component emits event `ON('idletime', function(is) {})` when the website is in idle. It reacts on mouse, keyboard, touch events or focusing of window.
 
 __Configuration__:
 
 - `count {Number}` in seconds, default: `300` 5 minutes)
+
+__Events__:
+
+- `ON('idletime', function(is))` is emitted if the document is idle (`is=true`) / active (`is=false`)
+- `ON('reload', function() {})` is emitted if the document is transformed from invisible to visible state within `config.count` interval
 
 ### Author
 
