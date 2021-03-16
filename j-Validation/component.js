fff50f1a192798307cfef33a3d91562891155d60
Peter Širka <petersirka@gmail.com>
2021-03-16 20:26:23 +0100

    Fixed removing of `-modified` class in `j-Validation.`

--- j-Validation/component.js
+++ j-Validation/component.js
@@ -74,8 +74,10 @@ COMPONENT('validation', 'delay:100;flags:visible', function(self, config, cls) {
 	};
 
 	self.state = function(type, what) {
-		if (type === 3 || what === 3)
+		if (type === 3 || what === 3) {
+			self.rclass(cls + '-modified');
 			tracked = 0;
+		}
 		setTimeout2(self.ID, check, config.delay);
 	};
 
