33c974cc5e51876c9b67753af3d08f958bd35b85
Peter Širka <petersirka@gmail.com>
2021-04-06 10:52:33 +0200

    Fixed `Thelpers.counter`.

--- Tangular-Counter/component.js
+++ Tangular-Counter/component.js
@@ -6,6 +6,6 @@ Thelpers.counter = function(value, decimals) {
 	if (value > 999999)
 		return (value / 1000000).format(decimals) + ' M';
 	if (value > 9999)
-		return (value / 10000).format(decimals) + ' K';
+		return (value / 1000).format(decimals) + ' K';
 	return value.format(decimals);
 };
