ccb05b5aecdecbdaee911968fe674fcb73c3a7e6
Peter Širka <petersirka@gmail.com>
2021-09-28 15:50:00 +0200

    Added `delay` option to the `j-Enter` component.

--- j-Enter/component.js
+++ j-Enter/component.js
@@ -1,19 +1,31 @@
-COMPONENT('enter', 'validate:1;trigger:button[name="submit"];flags:visible;timeout:1500', function(self, config) {
+COMPONENT('enter', 'validate:1;trigger:button[name="submit"];flags:visible;timeout:1500;delay:500', function(self, config) {
 
 	var flags;
+	var delay = null;
+	var submit = function(btn) {
+		delay = null;
+		if (btn)
+			btn.trigger('click');
+		else
+			self.EXEC(config.exec, self);
+	};
 
 	self.readonly();
 	self.make = function() {
 		self.event('keydown', 'input', function(e) {
 			if (e.which === 13 && (!config.validate || CAN(self.path, flags))) {
 				if (config.exec) {
-					if (!BLOCKED(self.ID, config.timeout))
-						EXEC(self.makepath(config.exec), self);
+					if (!BLOCKED(self.ID, config.timeout)) {
+						delay && clearTimeout(delay);
+						delay && setTimeout(submit, config.delay);
+					}
 				} else {
 					var btn = self.find(config.trigger);
 					if (!btn.prop('disabled')) {
-						if (!BLOCKED(self.ID, config.timeout))
-							btn.trigger('click');
+						if (!BLOCKED(self.ID, config.timeout)) {
+							delay && clearTimeout(delay);
+							delay = setTimeout(submit, config.delay, btn);
+						}
 					}
 				}
 			}
--- j-Enter/readme.md
+++ j-Enter/readme.md
@@ -3,7 +3,8 @@
 This component evaluates some method if the user press `enter`. Important: component finds all `input` elements and performs `CAN()` for the path.
 
 __Configuration__:
-- `exec` {String} a method name for execution
-- `validate` {Boolean} enables validate (default: `true`)
-- `trigger` {String} can contain a selector for triggering a real `click` event (default: `button[name="submit"]`)
-- __NEW__ `timeout` {Number} a timeout as a prevention for multi-press of `enter` key (default: `1500`)
+- `exec {String}` a method name for execution
+- `validate {Boolean}` enables validate (default: `true`)
+- `trigger {String}` can contain a selector for triggering a real `click` event (default: `button[name="submit"]`)
+- `timeout {Number}` a timeout as a prevention for multi-press of `enter` key (default: `1500`)
+- __NEW__ `delay {Number}` a delay for submitting data (default: `500` ms)
