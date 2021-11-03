b77fb18c1782f100cc20a0411a39a0fe2fe5fbbf
Peter Širka <petersirka@gmail.com>
2021-11-03 14:57:03 +0100

    Fixed `groups` with zoom in `j-Flow`.

--- j-Flow/component.js
+++ j-Flow/component.js
@@ -2347,7 +2347,7 @@ EXTENSION('flow:groups', function(self, config, cls) {
 				var h = item.el.height();
 				var node = item.el;
 				var pos = node.position();
-				var rect2 = { x: pos.left, y: pos.top, width: w, height: h };
+				var rect2 = { x: zoom(pos.left), y: zoom(pos.top), width: zoom(w), height: zoom(h) };
 				if (rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x && rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y) {
 					if (!item.actions || item.actions.move !== false)
 						drag.selected.push({ id: node.attrd('id'), node: node, pos: pos, output: node.find('.output'), input: node.find('.input') });
