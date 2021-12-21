95131b343eaa525d13482c637ef1f46cf20c5d7b
Peter Širka <petersirka@gmail.com>
2021-12-21 17:53:47 +0100

    Added new component `j-Display`.

---
+++ j-Display/component.js
@@ -0,0 +1,32 @@
+COMPONENT('display', 'delay:100', function(self, config, cls) {
+
+	var initialized = false;
+	var prev;
+
+	self.readonly();
+
+	self.make = function() {
+		self.aclass(cls);
+		self.resizeforce();
+		self.on('resize', self.resize);
+	};
+
+	self.resizeforce = function() {
+		var size = WIDTH(self.element);
+		if (!prev || prev.substring(3) !== size) {
+			prev && self.rclass(prev);
+			prev = 'jc-' + size;
+			self.aclass(prev);
+			self.path && self.set(size);
+			if (!initialized) {
+				initialized = true;
+				self.rclass('invisible hidden');
+			}
+		}
+	};
+
+	self.resize = function() {
+		setTimeout2(self.ID, self.resizeforce, config.delay);
+	};
+
+});
---
+++ j-Display/readme.md
@@ -0,0 +1,15 @@
+The component measures a display size according to the current element. It uses `WIDTH()` method and appends a CSS class in the form `jc-DISPLAY_SIZE`:
+
+- `jc-xs` extra small display (mobile phone)
+- `jc-sm` small display (tablet)
+- `jc-md` medium display (laptop)
+- `jc-lg` large display (desktop computer)
+
+__Configuration__:
+
+- `delay {Number}` a delay for obtaining of size (default: `100` ms)
+
+### Author
+
+- Peter Širka <petersirka@gmail.com>
+- [License](https://www.totaljs.com/license/)
