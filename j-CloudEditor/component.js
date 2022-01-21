9793409006842840bbc67b34dd0f70c9292d773c
Peter Širka <petersirka@gmail.com>
2022-01-21 20:36:03 +0100

    Added `j-CloudEditor`.

---
+++ j-CloudEditor/component.js
@@ -0,0 +1,184 @@
+COMPONENT('cloudeditor', 'parent:auto;autosave:1', function(self, config) {
+
+	var iframe;
+	var savetimeout;
+	var callbacks = {};
+	var callbackid = 1;
+	var skip = false;
+	var init = false;
+
+	var save = function() {
+		savetimeout = null;
+		send({ TYPE: 'body' }, function(msg) {
+			skip = true;
+			self.set(msg.value);
+		});
+	};
+
+	var send = function(msg, callback) {
+
+		if (callback) {
+			var id = (callbackid++) + '';
+			msg.callbackid = id;
+			callbacks[id] = callback;
+		}
+
+		iframe[0].contentWindow.postMessage(STRINGIFY(msg), '*');
+	};
+
+	var onmessage = function(e) {
+
+		var msg = e.originalEvent ? e.originalEvent.data : '';
+
+		if (typeof(msg) === 'string') {
+			if (msg.indexOf('"totaleditor"') === -1 || msg.indexOf('"id":"' + self.ID +'"') === -1)
+				return;
+			msg = PARSE(msg);
+		} else if (!msg)
+			return;
+
+		if (msg.totaleditor !== 1 || msg.id !== self.ID)
+			return;
+
+		if (msg.callbackid) {
+			var fn = callbacks[msg.callbackid];
+			if (fn) {
+				delete callbacks[msg.callbackid];
+				fn(msg);
+			}
+			return;
+		}
+
+		switch (msg.TYPE) {
+			case 'click':
+			case 'shortcut':
+			case 'errors':
+				config.event && self.SEEX(config.event, msg.TYPE, msg.value);
+				break;
+			case 'menu':
+			case 'contextmenu':
+				var offset = self.element.offset();
+				msg.x += offset.left;
+				msg.y += offset.top;
+				config.contextmenu && self.SEEX(config.contextmenu, msg);
+				break;
+			case 'cursor':
+				config.event && self.SEEX(config.event, msg.TYPE, msg.value);
+				if (config.autosave) {
+					savetimeout && clearTimeout(savetimeout);
+					savetimeout = setTimeout(save, 500);
+				}
+				break;
+			case 'init':
+				init = true;
+				break;
+		}
+	};
+
+	self.make = function() {
+		self.append('<iframe src="1.html?id={0}" frameborder="0" scrolling="no" allowtransparency="true" allow="geolocation *; microphone *; camera *; midi *; encrypted-media *" style="width:100%"></iframe>'.format(self.ID));
+		iframe = self.find('iframe');
+		self.resize();
+		$(W).on('message', onmessage);
+	};
+
+	self.destroy = function() {
+		$(W).off('message', onmessage);
+	};
+
+	self.replace = function(text, to) {
+		send({ TYPE: 'replace', value: text, to: to });
+	};
+
+	self.search = function(text, callback) {
+		send({ TYPE: 'search', value: text }, callback);
+	};
+
+	self.select = function(from, to) {
+		send({ TYPE: 'select', from: from, to: to });
+	};
+
+	self.goto = function(pos) {
+		send({ TYPE: 'goto', value: pos });
+	};
+
+	self.clear = function() {
+		send({ TYPE: 'clear' });
+	};
+
+	self.replaceselections = function(val) {
+		send({ TYPE: 'replaceselections', value: val });
+	};
+
+	self.replaceselection = function(val) {
+		send({ TYPE: 'replaceselection', value: val });
+	};
+
+	self.replacerange = function(val, from, to) {
+		send({ TYPE: 'replacerange', value: val, from: from, to: to });
+	};
+
+	self.focus = function() {
+		iframe.focus();
+		send({ TYPE: 'focus' });
+	};
+
+	self.insert = function(value) {
+		send({ TYPE: 'insert', value: value });
+	};
+
+	self.save = function(callback) {
+		send({ TYPE: 'body' }, function(msg) {
+			skip = true;
+			send({ TYPE: 'clear' });
+			if (callback) {
+				callback(msg.value);
+			} else {
+				self.get().body = msg.value;
+				self.update(true);
+			}
+		});
+	};
+
+	self.send = send;
+
+	self.resize = function() {
+		var parent = self.parent(config.parent);
+		var w = parent.width();
+		var h = parent.height();
+		iframe.css({ width: w, height: h });
+	};
+
+	self.markeradd = function(from, to, name, color) {
+		send({ TYPE: 'markeradd', from: from, to: to, value: name, color: color });
+	};
+
+	self.markerrem = function(name) {
+		send({ TYPE: 'markerrem', value: name });
+	};
+
+	self.darkmode = function() {
+		send({ TYPE: 'darkmode', value: $('body').hclass('ui-dark') });
+	};
+
+	var settertimeout;
+
+	self.setter = function() {
+
+		if (skip) {
+			skip = false;
+			return;
+		}
+
+		if (init) {
+			settertimeout = null;
+			var model = self.get();
+			model && send({ TYPE: 'init', mode: model.type || 'clientside', value: model.body, darkmode: $('body').hclass('ui-dark') });
+		} else {
+			settertimeout && clearTimeout(settertimeout);
+			settertimeout = setTimeout(self.setter, 100);
+		}
+
+	};
+
+});
---
+++ j-CloudEditor/readme.md
@@ -0,0 +1,39 @@
+## j-CloudEditor (BETA)
+
+- supports multiple cursors (`shift + cmd + arrow-down/up`)
+- supports finding selected text (`cmd + d`)
+- supports custom scrollbars
+- source-code: <https://github.com/totaljs/editor>
+- it uses editor: [CodeMirror](https://codemirror.net/)
+
+__Configuration__:
+
+- `parent {String}` a selector for the main container (default: `auto`)
+- `autosave {Boolean}` auto rebinds value from the editor to the component's path (default: `true`)
+- `event {String}` a path to the `function(name, value)`
+- `contextmenu {String}` a path to the `function(opt)`
+
+__Method__:
+
+- `component.insert('some text')`
+- `component.replace('text', 'newvalue')`
+- `component.search('text', callback(output))`
+- `component.select(from, [to])`
+- `component.goto(pos)`
+- `component.focus()`
+- `component.save(callback(value))`
+- `component.markeradd(from, to, name, [color])`
+- `component.markerrem(name)`
+- `component.clear()` clears all changes
+- `component.replaceselections(selections)`
+- `component.replaceselection(selection)`
+- `component.replacerange(value, from, [to])`
+
+__IMPORTANT__:
+
+`from`, `to`, `pos` arguments must be in the form: `{ line: Number, ch: Number }`.
+
+### Author
+
+- Peter Širka <petersirka@gmail.com>
+- [License](https://www.totaljs.com/license/)
