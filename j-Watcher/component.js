98817baa8358028e10940d89393351a4b302ef6b
Peter Širka <petersirka@gmail.com>
2022-01-21 15:28:19 +0100

    Added new component j-Watcher.

---
+++ j-Watcher/component.js
@@ -0,0 +1,34 @@
+COMPONENT('watcher', 'delay:0', function(self, config) {
+
+	var fn = NOOP;
+	var delay = null;
+
+	self.readonly();
+
+	self.make = function() {
+		var scr = self.find('scri' + 'pt').eq(0);
+		var js = scr.html();
+		if (js) {
+			fn = new Function('value', 'path', 'type', 'element', js);
+			scr.remove();
+		}
+	};
+
+	self.destroy = function() {
+		fn = null;
+	};
+
+	var exec = function(value, path, type) {
+		delay = null;
+		fn.call(this, value, path, type, self.element);
+	};
+
+	self.setter = function(value, path, type) {
+		if (config.delay) {
+			delay && clearTimeout(delay);
+			delay = setTimeout(exec, config.delay, value, path, type);
+		} else
+			fn.call(this, value, path, type, self.element);
+	};
+
+});
---
+++ j-Watcher/readme.md
@@ -0,0 +1,21 @@
+## j-Watcher
+
+This component executes the code defined in `<script type="text/js"></script>` tag.
+
+```html
+<script type="text/js">
+
+	// value {Object} a value binded according to the path
+	// path {String} a path where the value was binded
+	// type {Number}
+	// element {jQuery element} a current element
+
+</script>
+```
+
+- `delay {Number}` a delay for evaluating code (default: `0`)
+
+### Author
+
+- Peter Širka <petersirka@gmail.com>
+- [License](https://www.totaljs.com/license/)
