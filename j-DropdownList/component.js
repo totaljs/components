d9878e2842de87f0d87798ca21a8862c2df6b9ef
Peter Širka <petersirka@gmail.com>
2022-02-21 18:28:49 +0100

    Added new component: `j-DropDownList`.

---
+++ j-DropdownList/component.js
@@ -0,0 +1,145 @@
+COMPONENT('dropdownlist', 'limit:3;check:true', function(self, config, cls) {
+
+	var cls2 = '.' + cls;
+	var items;
+	var nocheck = false;
+
+	self.template = Tangular.compile('<div class="{0}-item" data-id="{{ id }}"><span class="{0}-control {0}-remove"><i class="fa fa-times"></i></span><div class="name">{{ if icon }}<i class="{{ icon }}"></i>{{ fi }}{{ name }}</div></div>'.format(cls));
+	self.getter = null;
+
+	self.validate = function(value) {
+		return (!config.required || config.disabled) ? true : (value && value.length > 0);
+	};
+
+	self.uncheck = function() {
+		var items = self.get();
+		nocheck = false;
+		self.tclass(cls + '-limited', items && config.limit ? items.length >= config.limit : false);
+	};
+
+	self.make = function() {
+
+		var label = self.element.text() || config.label;
+		self.aclass(cls + (config.required ? (' ' + cls + '-required') : ''));
+		self.html('<label>{2}:</label><div class="{0}-values"><div class="{0}-add"><span class="{0}-control"><i class="fa fa-angle-down"></i></span>{1}</div><div class="{0}-items"></div></div>'.format(cls, config.placeholder, label));
+		items = self.find(cls2 + '-items');
+		self.event('click', cls2 + '-add', function() {
+
+			if (self.hclass(cls + '-limited') || config.disabled)
+				return;
+
+			var items = self.get();
+			var datasource = typeof(config.datasource) === 'string' ? GET(config.datasource) : config.datasource;
+			var opt = {};
+
+			opt.element = $(this);
+
+			if (config.dirplaceholder)
+				opt.placeholder = config.dirplaceholder;
+
+			if (config.dirsearch)
+				opt.search = config.dirsearch;
+
+			opt.offsetX = -1;
+			opt.offsetY = -1;
+			opt.offsetWidth = 12;
+			opt.items = datasource.slice(0);
+
+			if ((items && items.length)) {
+				opt.items = opt.items.remove(function(item) {
+					return items && items.length && items.findIndex('id', item.id) !== -1;
+				});
+			}
+
+			opt.callback = function(value) {
+
+				value = CLONE(value);
+
+				delete value.selected;
+				delete value.checked;
+				delete value.template;
+
+				var items = self.get() || [];
+
+				if (items.findItem('id', value.id))
+					return;
+
+				items.unshift(value);
+
+				if (config.limit && items.length > config.limit)
+					items.splice(config.limit);
+
+				nocheck = true;
+				self.set(items);
+				self.change(true);
+				setTimeout(self.uncheck, 100);
+			};
+
+			SETTER('directory/show', opt);
+		});
+
+		self.event('click', cls2 + '-remove', function() {
+
+			if (config.disabled)
+				return;
+
+			var el = $(this).closest(cls2 + '-item');
+			var id = ATTRD(el);
+			var arr = self.get();
+			var index = arr.findIndex('id', id);
+			arr.splice(index, 1);
+			nocheck = true;
+			self.update(true);
+			self.change(true);
+			setTimeout(self.uncheck, 100);
+		});
+	};
+
+	self.configure = function(key, value) {
+		if (key === 'disabled') {
+			if (config.datasource.indexOf(',') !== -1)
+				config.datasource = self.parsesource(config.datasource);
+			self.tclass('ui-disabled', value);
+		}
+	};
+
+	self.check = function() {
+		var items = self.get() || [];
+		var check = typeof(config.datasource) === 'string' ? GET(config.datasource) : config.datasource;
+		items.wait(function(item, next) {
+			var m = check.findItem('id', item.id);
+			if (m) {
+				item.name = m.name;
+				item.icon = m.icon;
+				item.unit = m.unit;
+			} else
+				item.id = '';
+			next();
+		}, function() {
+			nocheck = true;
+			self.set(items.remove('id', ''));
+			self.change(true);
+			setTimeout(self.uncheck, 100);
+		});
+	};
+
+	self.redraw = function() {
+
+		var value = self.get() || [];
+		var builder = [];
+
+		for (var item of value)
+			builder.push(self.template(item));
+
+		items.html(builder.join(''));
+		setTimeout(self.uncheck, 100);
+	};
+
+	self.setter = function() {
+		if (nocheck || !config.check)
+			self.redraw();
+		else
+			self.check();
+	};
+
+});
---
+++ j-DropdownList/readme.md
@@ -0,0 +1,18 @@
+## j-DropDownList
+
+- supports __dark mode__
+
+__Configuration__:
+
+- `required {Boolean}` enables validation (default: `false`)
+- `datasource {String}`  a link to the `path {Array}` or inline defined items `key1|value1,key2 with value,key3|value`
+- `limit {Number}` a maximum count of selected items (default: `0` disabled)
+- `disabled {Boolean}` disables component
+- `dirplaceholder {String}` placeholder for `j-Directory`
+- `dirsearch {Boolean}` disables searching in `j-Directory` (default: `false`)
+- `check {Boolean}` compares selected values with datasource (default: `false`)
+
+### Author
+
+- Peter Širka <petersirka@gmail.com>
+- [License](https://www.totaljs.com/license/)
