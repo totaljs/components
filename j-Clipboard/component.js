3912b94324b9f91a4cc51c3e62cd31bdd58ee179
Peter Širka <petersirka@gmail.com>
2022-04-01 08:55:02 +0200

    Improved code in `j-Clipboard` again.

--- j-Clipboard/component.js
+++ j-Clipboard/component.js
@@ -7,15 +7,14 @@ COMPONENT('clipboard', function(self, config, cls) {
 	self.nocompile();
 
 	self.make = function() {
-		var id = 'clipboard' + self.id;
-		$(document.body).append('<textarea id="{0}" class="{1}"></textarea>'.format(id, cls));
-		container = $('#' + id);
-
 		if (navigator.clipboard && W.isSecureContext) {
 			self.copy = function(value) {
 				navigator.clipboard.writeText(value).catch(err => console.error(err));
 			};
 		} else {
+			var id = 'clipboard' + self.id;
+			$(document.body).append('<textarea id="{0}" class="{1}"></textarea>'.format(id, cls));
+			container = $('#' + id);
 			self.copy = function(value) {
 				container.val(value);
 				container.focus();
