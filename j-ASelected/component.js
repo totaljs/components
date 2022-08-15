5683c24519e0c42327e15a1becb3392455314356
Peter Širka <petersirka@gmail.com>
2022-08-15 14:27:18 +0200

    Fixed filtering of links in `j-ASelected`.

--- j-ASelected/component.js
+++ j-ASelected/component.js
@@ -16,7 +16,7 @@ COMPONENT('aselected', 'selector:a;attr:href;class:selected;delay:300', function
 		var url = NAV.url;
 		for (var i = 0; i < arr.length; i++) {
 			var el = $(arr[i]);
-			var href = el.attr(config.attr);
+			var href = el.attr(config.attr) || '';
 			var selected = config.strict ? url === href : url.length === 1 || href.length === 1 ? href === url : url.indexOf(href) === 0;
 			el.tclass(config.class, selected);
 		}
