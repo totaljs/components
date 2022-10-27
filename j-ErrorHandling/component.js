0e53bbc68235fb6c2a3f9c1d3b3225988709bbc3
Peter Širka <petersirka@gmail.com>
2022-10-27 08:45:47 +0200

    Added new component `j-ErrorHandling`.

---
+++ j-ErrorHandling/component.js
@@ -0,0 +1,50 @@
+COMPONENT('errorhandling', 'keywords:401=login', function(self, config) {
+
+	self.readonly();
+	self.singleton();
+	self.nocompile();
+	self.blind();
+
+	self.make = function() {
+
+		var keywords = config.keywords ? config.keywords.split(',') : EMPTYARRAY;
+		for (var i = 0; i < keywords.length; i++)
+			keywords[i] = keywords[i].split('=');
+
+		self.on('ERROR', function(err) {
+
+			if (err instanceof Error)
+				err = [{ error: err.message }];
+			else if (!(err instanceof Array))
+				err = [{ error: err.error || err.message || err.err }];
+
+			var arr = [];
+			var response = {};
+
+			for (var m of err) {
+
+				if (!m.error)
+					continue;
+
+				for (var k of keywords) {
+					if ((m.name && m.name.indexOf(k[0]) !== -1) || m.error.indexOf(k[0]) !== -1)
+						response[k[1]] = true;
+				}
+
+				arr.push(m.error);
+			}
+
+			response.items = arr;
+			if (config.exec) {
+				self.SEEX(config.exec, arr);
+			} else {
+				var name = M.$components.message ? 'message' : M.$components.snackbar ? 'snackbar' : M.$components.notifybar ? 'notifybar' : M.$components.notify ? 'notify' : '';
+				if (name)
+					SETTER(name + '/warning', response.items.join('<br />'));
+				else if (W.console)
+					console.error('ERROR():', response.items.join('<br />'));
+			}
+		});
+	};
+
+});
---
+++ j-ErrorHandling/readme.md
@@ -0,0 +1,21 @@
+## j-ErrorHandling
+
+This component captures the `ON('ERROR', function(err` event trigger from the `ERROR()` method and tries to display form more comfortably.
+
+- easy usage
+- singleton
+
+__Configuration__:
+
+- `keywords {String}` which will be searched in the error messages (default value: `401=login`)
+	- must be in the form `search1=target_property,search2=target_property`
+- `exec {String}` a link to the `function(response)`, output:
+	- `response.items {String Array}`
+	- `response.keyword_target_property {Boolean}` optional, according to the `keywords` settings
+
+The component tries using `j-Message`, `j-Snackbar`, `j-NotifyBar` or `j-Notify` for displaying of errors if you don't have defined `exec` configuration.
+
+### Author
+
+- Peter Širka <petersirka@gmail.com>
+- [License](https://www.totaljs.com/license/)
