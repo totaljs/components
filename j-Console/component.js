c83fd52f85ac04eeaa725d16498a36d12c6cd0e5
Peter Širka <petersirka@gmail.com>
2023-01-12 14:13:12 +0100

    Fixed method name in the `j-Console`.

--- j-Console/component.js
+++ j-Console/component.js
@@ -47,7 +47,7 @@ COMPONENT('console', function(self, config, cls) {
 			if (!current)
 				current = keys[i];
 
-			var icon = self.tiicon(item.icon);
+			var icon = self.icon(item.icon);
 			builder.push(('<span title="{1}" data-id="{2}" class="' + cls + '-tab{3}"><i class="{0}"></i>{1}</span>').format(icon + (item.name ? '' : '" style="margin-right:0'), item.name, keys[i], current === keys[i] ? (' ' + cls + '-selected') : ''));
 		}
 
