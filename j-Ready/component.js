022f41e3602b32d403ab16d03146b7ec15ed8f94
Peter Širka <petersirka@gmail.com>
2023-01-14 20:53:09 +0100

    Added remove `invisible` automatically to the `j-Ready`.

--- j-Ready/component.js
+++ j-Ready/component.js
@@ -6,8 +6,9 @@ COMPONENT('ready', 'delay:800', function(self, config) {
 	self.make = function() {
 		config.rclass && self.rclass(config.rclass, config.adelay || config.delay);
 		config.aclass && self.aclass(config.aclass, config.rdelay || config.delay);
+		self.rclass('invisible', config.adelay || config.delay);
 		config.focus && setTimeout(function() {
-			self.find(config.focus === true || config.focus === 1 ? 'input[type="text"],textarea,select' : config.focus).focus();
+			self.autofocus(config.focus === true || config.focus === 1 ? null : config.focus).focus();
 		}, config.fdelay || (config.delay + 5));
 	};
 
