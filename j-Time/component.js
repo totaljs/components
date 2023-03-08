e3d5607f17a3ba5404be40a0075e9635732f6b22
Peter Širka <petersirka@gmail.com>
2023-03-08 21:10:21 +0100

    Fixed time format in `j-Time`.

--- j-Time/component.js
+++ j-Time/component.js
@@ -9,11 +9,17 @@ COMPONENT('time', 'icon:ti ti-clock', function(self, config, cls) {
 		self.append((config.icon ? '<i class="{0}"></i>'.format(config.icon) : '') + '<span></span>');
 
 		var span = self.find('span');
+		var format = (config.format || DEF.timeformat || 'HH:mm');
+		var t12 = format.indexOf('a') !== -1;
+
+		if (t12)
+			format = format.replace(/a/).trim();
+
+		format += ':ss' + (t12 ? ' a' : '');
 
 		fn = function() {
 			if (is) {
 				NOW = new Date();
-				var format = (config.format || DEF.timeformat || 'HH:mm') + ':ss';
 				if (NOW.getSeconds() % 2 === 0)
 					format = format.replace(/:/g, ' ');
 				span.html(NOW.format(format));
