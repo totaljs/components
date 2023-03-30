b4cdbc90934012e38fc7d705e244ce6d39951c57
Peter Širka <petersirka@gmail.com>
2023-03-30 20:29:21 +0200

    Added `j-Printer`.

---
+++ j-Printer/component.js
@@ -0,0 +1,40 @@
+COMPONENT('printer', 'delay:500', function(self, config, cls) {
+
+	self.nocompile();
+	self.readonly();
+	self.singleton();
+
+	self.make = function() {
+		self.aclass(cls);
+	};
+
+	self.print = function(title, html) {
+
+		if (html == null) {
+			html = title;
+			title = '';
+		}
+
+		var iframe = $('<ifra' + 'me src="about:blank" frameborder="0" scrolling="no"></ifr' + 'ame>')[0];
+		self.dom.appendChild(iframe);
+
+		var win = iframe.contentWindow;
+
+		if (html.indexOf('<body>') === -1)
+			html = '<html><head><title>{0}</title><meta charset="utf-8" /></head><body style="font-family:Arial;font-size:14px;margin:0;padding:0;color:#000">{1}</body></html>'.format(title, html);
+
+		setTimeout(function() {
+
+			var doc = win.document;
+			doc.open();
+			doc.write(html);
+			doc.close();
+			win.print();
+
+			setTimeout(() => self.dom.removeChild(iframe), config.delay);
+
+		}, config.delay);
+
+	};
+
+});
