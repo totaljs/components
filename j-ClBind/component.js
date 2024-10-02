392c88b8e997f749b184f6ba59a1757eecf37e2e
petersirka <petersirka@gmail.com>
2024-10-02 20:19:32 +0200

    Improved functionalinty `j-ClBind`.

--- j-ClBind/component.js
+++ j-ClBind/component.js
@@ -3,19 +3,15 @@ COMPONENT('clbind', 'nullable:1;key:id', function(self, config, cls) {
 	var datasource = [];
 	var template = null;
 	var curr = null;
-	var prev = null;
 
 	var bind = function() {
-		if (prev !== curr) {
-			prev = curr;
-			var item = datasource.findItem(config.key, curr);
-			if (template && (config.nullable || item))
-				self.html(template({ value: item }));
-			if (!config.nullable)
-				self.tclass('hidden', !item);
-			config.bind && self.SEEX(config.bind, item, self.element);
-			config.exec && self.SEEX(config.exec, item, self.element);
-		}
+		var item = datasource.findItem(config.key, curr);
+		if (template && (config.nullable || item))
+			self.html(template({ value: item }));
+		if (!config.nullable)
+			self.tclass('hidden', !item);
+		config.bind && self.SEEX(config.bind, item, self.element);
+		config.exec && self.SEEX(config.exec, item, self.element);
 	};
 
 	self.make = function() {
@@ -29,14 +25,14 @@ COMPONENT('clbind', 'nullable:1;key:id', function(self, config, cls) {
 		if (key === 'dirsource' || key === 'datasource' || key === 'source') {
 			self.datasource(value, function(path, value) {
 				datasource = (M.is20 ? path : value) || [];
-				bind();
+				setTimeout2(self.ID, bind, 10);
 			});
 		}
 	};
 
 	self.setter = function(value) {
 		curr = value;
-		bind();
+		setTimeout2(self.ID, bind, 10);
 	};
 
 });
