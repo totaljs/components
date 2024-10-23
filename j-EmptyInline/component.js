d132c323322205580fe15e17fe462bdec7bca615
petersirka <petersirka@gmail.com>
2024-10-23 13:12:17 +0200

    Fixed icon in `j-EmptyInline`.

--- j-EmptyInline/component.js
+++ j-EmptyInline/component.js
@@ -2,7 +2,7 @@ COMPONENT('emptyinline', 'icon:ti ti-ban', function(self, config, cls) {
 
 	self.make = function() {
 		self.aclass(cls + ' hidden');
-		config.icon && self.element.prepend('<i class="{icon}"></i>');
+		config.icon && self.element.prepend('<i class="{icon}"></i>'.args(config));
 	};
 
 	self.setter = function(value) {
