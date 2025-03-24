9278c458f10ecc04d30b6f83de04719de1bfa9fa
petersirka <petersirka@gmail.com>
2025-03-24 10:21:37 +0100

    Fixed code.

--- Tangular-Badge/component.js
+++ Tangular-Badge/component.js
@@ -14,7 +14,7 @@ Thelpers.badge = function(value, size) {
 		let num = 0;
 		for (let i = 0; i < m.length; i++)
 			num += m.charCodeAt(i);
-		output += '<span class="badge badge-{2} mr5" style="background-color:{0}">{1}</span>'.format(colors[num % colors.length], value, size || 'medium');
+		output += '<span class="badge badge-{2} mr5" style="background-color:{0}">{1}</span>'.format(colors[num % colors.length], m, size || 'medium');
 	}
 
 	return output;
