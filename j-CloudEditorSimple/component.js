b8603ad26ecb38e3c309b8337ef908da2152ebc1
petersirka <petersirka@gmail.com>
2026-01-28 09:37:07 +0100

    Updated links to the cloud code editor.

--- j-CloudEditorSimple/component.js
+++ j-CloudEditorSimple/component.js
@@ -83,7 +83,7 @@ COMPONENT('cloudeditorsimple', 'parent:auto;autosave:500;margin:0;linenumbers:1;
 
 	self.make = function() {
 		if (!config.url)
-			config.url = (DEF.cdn || 'https://cdn.componentator.com') + '/editor/1.min.html';
+			config.url = (DEF.cdn || 'https://cdn.componentator.com') + '/editor.min@1.html';
 		self.append('<iframe src="{1}?id={0}" frameborder="0" scrolling="no" allowtransparency="true" allow="geolocation *; microphone *; camera *; midi *; encrypted-media *" style="width:100%;overflow:hidden;display:block"></iframe>'.format(self.ID, config.url));
 		iframe = self.find('iframe');
 		self.resize();
